47e5c7349c50aebd24063c59e26fd2bd489fad9c
andreamari <andreamari84@gmail.com>
2020-02-07 11:31:04 +0100

    updates test

--- mitiq/tests/test_folding_pyquil.py
+++ mitiq/tests/test_folding_pyquil.py
@@ -4,7 +4,6 @@ from pyquil import Program
 from pyquil.gates import X, Y, Z
 from mitiq.folding_pyquil import local_folding, unitary_folding
 
-
 def random_circuit(depth):
     """Returns a single-qubit random circuit based on Pauli gates."""
     prog = Program()
@@ -12,10 +11,8 @@ def random_circuit(depth):
         prog += random.choice([X(0), Y(0), Z(0)])
     return prog
 
-
 strech_vals = [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.5, 4.5, 5.0]
 
-# Test unitary folding
 print("Testing unitary folding...")
 for c in strech_vals:
     circ = random_circuit(10)
@@ -23,18 +20,20 @@ for c in strech_vals:
     actual_c = len(out) / len(circ)
     print("Input stretch: {:}    Real stretch: {}".format(c, actual_c))
     assert np.isclose(c, actual_c, atol=1.0e-1)
-    # Uncomment to print input and output circuits
-    # print("input \n", circ)
-    # print("output \n", out)
 
-# Test local_folding
-print("Testing local folding...")
+print("Testing local folding (no sampling)...")
 for c in strech_vals:
     circ = random_circuit(10)
-    out = local_folding(circ, c)
+    out = local_folding(circ, c, sampling=False)
     actual_c = len(out) / len(circ)
     print("Input stretch: {:}    Real stretch: {}".format(c, actual_c))
     assert np.isclose(c, actual_c, atol=1.0e-1)
-    # Uncomment to print input and output circuits
-    # print("input \n", circ)
-    # print("output \n", out)
+
+print("Testing local folding (random sampling)...")
+for c in strech_vals:
+    circ = random_circuit(10)
+    out = local_folding(circ, c, sampling=True)
+    actual_c = len(out) / len(circ)
+    print("Input stretch: {:}    Real stretch: {}".format(c, actual_c))
+    assert np.isclose(c, actual_c, atol=1.0e-1)
+
