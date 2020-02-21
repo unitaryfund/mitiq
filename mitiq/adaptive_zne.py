7c4d047f4b3268159cd02b44fd9a47d31bcd0ad8
Will Zeng <zeng.will@gmail.com>
2020-02-21 11:41:20 -0500

    rename Generator to Factory

--- mitiq/adaptive_zne.py
+++ mitiq/adaptive_zne.py
@@ -6,9 +6,9 @@ import mitiq.qiskit.qiskit_utils as qs_utils
 from mitiq import QPROGRAM
 
 
-class Generator(object):
+class Factory(object):
     """
-    This object adaptively generates new noise scaling parameters based on a historical stack of previous noise
+    This object adaptively produces new noise scaling parameters based on a historical stack of previous noise
     scale parameters and previously calculated expectation values. Noise scaling parameters are stored on the `in_stack`
     and the running list of expectation values are stored on the `out_stack`.
     """
@@ -27,7 +27,7 @@ class Generator(object):
         return expt
 
 
-class BatchedGenerator(Generator):
+class BatchedFactory(Factory):
     def __init__(self, scalars: List[float], instack: List[float] = None, outstack: List[float] = None) -> None:
         """
         Runs a series of scalar noise parameters serially.
@@ -46,7 +46,7 @@ class BatchedGenerator(Generator):
         try:
             next_param = self.scalars[len(outstack)]
         except IndexError:
-            raise IndexError(f"BatchedGenerator cannot take another step. Number of batched scalars ({len(instack)}) "
+            raise IndexError(f"BatchedFactory cannot take another step. Number of batched scalars ({len(instack)}) "
                              f"exceeded.")
         self.instack.append(next_param)
         return next_param
@@ -55,46 +55,46 @@ class BatchedGenerator(Generator):
         return len(instack) == len(self.scalars)
 
 
-def mitigate(pq: Program, gen: Generator, scale_noise: Callable, run_program: Callable) -> \
+def mitigate(pq: Program, fac: Factory, scale_noise: Callable, run_program: Callable) -> \
         Tuple[List[float], List[float]]:
     """
-    Runs the generator until convergence and return the full list of noise values and expectations that have been
+    Runs the factory until convergence and return the full list of noise values and expectations that have been
     calculated.
     :param pq: Program to mitigate.
     """
-    if not gen.is_converged(gen.instack, gen.outstack):
-        next_param = gen.step(gen.instack, gen.outstack)
+    if not fac.is_converged(fac.instack, fac.outstack):
+        next_param = fac.step(fac.instack, fac.outstack)
 
         scaled_pq = scale_noise(pq, next_param)
 
         next_result = run_program(scaled_pq)
-        gen.outstack.append(next_result)
+        fac.outstack.append(next_result)
 
-        return mitigate(pq, gen, scale_noise, run_program)
+        return mitigate(pq, fac, scale_noise, run_program)
 
-    return gen.instack, gen.outstack
+    return fac.instack, fac.outstack
 
 
-def zne(run_program: Callable[[QPROGRAM, int], float], gen: Generator = None,
+def zne(run_program: Callable[[QPROGRAM, int], float], fac: Factory = None,
         scale_noise: Callable[[QPROGRAM, float], QPROGRAM] = None) -> Callable[[QPROGRAM], float]:
     if scale_noise is None:
         # TODO this assumes is qiskit
         scale_noise = qs_utils.scale_noise
 
-    if gen is None:
-        gen = BatchedGenerator([1.0, 2.0, 3.0])
+    if fac is None:
+        fac = BatchedFactory([1.0, 2.0, 3.0])
 
     def zne_run(pq: QPROGRAM) -> float:
-        params, expects = mitigate(pq, gen, scale_noise, run_program)
-        return gen.reduce(expects)
+        params, expects = mitigate(pq, fac, scale_noise, run_program)
+        return fac.reduce(expects)
 
     return zne_run
 
 
-def zne_factory(gen: Generator = None, scale_noise: Callable[[QPROGRAM, float], QPROGRAM] = None) -> Callable:
+def zne_factory(fac: Factory = None, scale_noise: Callable[[QPROGRAM, float], QPROGRAM] = None) -> Callable:
     # more precisely, this is a wrap function which is necessary to pass parameters to the decorator
     # formally, the function below is the actual decorator
     def decorator(fun):
-        return zne(fun, gen, scale_noise)
+        return zne(fun, fac, scale_noise)
 
     return decorator
--- mitiq/qiskit/tests/test_adaptive_zne.py
+++ mitiq/qiskit/tests/test_adaptive_zne.py
@@ -1,7 +1,7 @@
 # test_adaptive_zne.py
 import numpy as np
 
-from mitiq.adaptive_zne import BatchedGenerator, mitigate
+from mitiq.adaptive_zne import BatchedFactory, mitigate
 import mitiq.qiskit.qiskit_utils as qs
 
 
@@ -9,8 +9,8 @@ def test_adaptive_zne_qiskit():
     rand_circ = qs.random_identity_circuit(depth=30)
     rand_circ.measure(0, 0)
 
-    gen = BatchedGenerator([1.0, 2.0, 3.0])
-    params, expects = mitigate(rand_circ, gen, qs.scale_noise, qs.run_program)
-    xx = gen.reduce(expects)
+    fac = BatchedFactory([1.0, 2.0, 3.0])
+    params, expects = mitigate(rand_circ, fac, qs.scale_noise, qs.run_program)
+    xx = fac.reduce(expects)
 
     assert np.isclose(xx, 1.0, atol=1.e-1)
