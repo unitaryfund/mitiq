390fb4be11ad87e6f8152b57cb32b631ff136121
andreamari <andreamari84@gmail.com>
2020-02-25 17:37:38 +0100

    major refactoring of pyquil folding and tests

--- mitiq/tests/test_folding_pyquil.py
+++ mitiq/tests/test_folding_pyquil.py
@@ -4,21 +4,20 @@ from pyquil import Program
 from pyquil.gates import X, Y, Z, CNOT
 from pyquil.unitary_tools import program_unitary
 from mitiq.folding_pyquil import (
-    local_folding,
+    fold_local,
     unitary_folding,
-    sampling_stretcher,
-    left_stretcher,
+    fold_gates_at_random,
+    fold_gates_from_left,
 )
 from copy import deepcopy
 
 
+# arbitrary constants used for testing
 STRETCH_VALS = [1.0, 1.3, 2.0, 2.6, 3.0, 3.5, 4.0]
 DEPTH = 50
-
-# NUM_SHOTS must be 1 in order to pass the next tests.
-# This is due to the fact that num_shots are not retained by
-# some methods of the Program class (bug?)
-NUM_SHOTS = 1
+NUM_SHOTS = 7
+GATE = [[0, 1], [1, 0]]
+GATE_NAME = "X_test"
 
 
 def random_circuit(depth: int):
@@ -27,86 +26,74 @@ def random_circuit(depth: int):
     gate_set = [X(0), Y(0), Z(0), X(1), Y(1), Z(1), CNOT(0, 1), CNOT(1, 0)]
     for _ in range(depth):
         prog += random.choice(gate_set)
-    prog.num_shots = NUM_SHOTS
+
+    # we add arbitrary metadata to the program since we want to test that
+    # properties are correctly retained by the output of folding functions.
+    prog.wrap_in_numshots_loop(NUM_SHOTS)
+    prog.defgate(GATE_NAME, GATE)
     return prog
 
+
+def apply_folding_tests(circ: Program, circ_copy: Program, out: Program, stretch: float):
+    """Performs a sequece of tests associated to a generic folding function.
+
+    Args:
+        circ: Input circuit of the generic folding function.
+        circ_copy: Deep copy of the input circuit performed before folding.
+        out: Output circuit returned by the generic folding function.
+        stretch: Stretch factor used by the generic folding function.
+    """
+    actual_c = len(out) / len(circ)
+    # test length scaling
+    assert np.isclose(stretch, actual_c, atol=1.0e-1)
+    # test unitaries are equal
+    u_in = program_unitary(circ, 2)
+    u_out = program_unitary(out, 2)
+    assert (u_in == u_out).all
+    # test input is not mutated up to "_synthesized_instructions"
+    vars_a = vars(circ_copy)
+    vars_b = vars(circ)
+    vars_a.pop("_synthesized_instructions")
+    vars_b.pop("_synthesized_instructions")
+    assert vars_a == vars_b
+    # test input properties are passed to output
+    # up to "_synthesized_instructions" and "_instructions"
+    vars_a.pop("_instructions")
+    vars_c = vars(out)
+    vars_c.pop("_synthesized_instructions")
+    vars_c.pop("_instructions")
+    assert vars_a == vars_c
+    
 
 def test_unitary_folding():
     for c in STRETCH_VALS:
         circ = random_circuit(DEPTH)
         circ_copy = deepcopy(circ)
         out = unitary_folding(circ, c)
-        actual_c = len(out) / len(circ)
-        # test length scaling
-        assert np.isclose(c, actual_c, atol=1.0e-1)
-        # test unitaries are equal
-        u_in = program_unitary(circ, 2)
-        u_out = program_unitary(out, 2)
-        assert (u_in == u_out).all
-        # test input is not mutated up to "_synthesized_instructions"
-        vars_a = vars(circ_copy)
-        vars_b = vars(circ)
-        vars_a.pop("_synthesized_instructions")
-        vars_b.pop("_synthesized_instructions")
-        assert vars_a == vars_b
-        # test input properties are passed to output
-        # up to "_synthesized_instructions" and "_instructions"
-        vars_a.pop("_instructions")
-        vars_b = vars(out)
-        vars_b.pop("_synthesized_instructions")
-        vars_b.pop("_instructions")
-        assert vars_a == vars_b
+        apply_folding_tests(circ, circ_copy, out, c)
 
 
-def test_local_folding_nosamp():
+def test_fold_local_from_left():
     for c in STRETCH_VALS:
         circ = random_circuit(DEPTH)
         circ_copy = deepcopy(circ)
-        out = local_folding(circ, c, stretcher=sampling_stretcher)
-        actual_c = len(out) / len(circ)
-        # test length scaling
-        assert np.isclose(c, actual_c, atol=1.0e-1)
-        # test unitaries are equal
-        u_in = program_unitary(circ, 2)
-        u_out = program_unitary(out, 2)
-        assert (u_in == u_out).all
-        # test input is not mutated up to "_synthesized_instructions"
-        vars_a = vars(circ_copy)
-        vars_b = vars(circ)
-        vars_a.pop("_synthesized_instructions")
-        vars_b.pop("_synthesized_instructions")
-        assert vars_a == vars_b
-        # test input properties are passed to output
-        # up to "_synthesized_instructions" and "_instructions"
-        vars_a.pop("_instructions")
-        vars_b = vars(out)
-        vars_b.pop("_synthesized_instructions")
-        vars_b.pop("_instructions")
-        assert vars_a == vars_b
+        out = fold_local(circ, c, fold_gates_from_left)
+        apply_folding_tests(circ, circ_copy, out, c)
 
 
-def test_local_folding_withsamp():
+def test_fold_local_at_random_no_seed():
     for c in STRETCH_VALS:
         circ = random_circuit(DEPTH)
         circ_copy = deepcopy(circ)
-        out = local_folding(circ, c, stretcher=left_stretcher)
-        actual_c = len(out) / len(circ)
-        # test lenght scaling
-        assert np.isclose(c, actual_c, atol=1.0e-1)
-        # test unitaries are equal
-        u_in = program_unitary(circ, 2)
-        u_out = program_unitary(out, 2)
-        assert (u_in == u_out).all
-        # test input is not mutated up to "_synthesized_instructions"
-        vars_a = vars(circ_copy)
-        vars_b = vars(circ)
-        vars_a.pop("_synthesized_instructions")
-        vars_b.pop("_synthesized_instructions")
-        assert vars_a == vars_b
-        # test input properties are passed to output
-        # up to "_synthesized_instructions" and "_instructions"
-        vars_a.pop("_instructions")
-        vars_b = vars(out)
-        vars_b.pop("_synthesized_instructions")
-        vars_b.pop("_instructions")
-        assert vars_a == vars_b
+        # test random folding without seed
+        out = fold_local(circ, c, fold_gates_at_random)
+        apply_folding_tests(circ, circ_copy, out, c)
+        
+
+def test_fold_local_at_random_with_seed():
+    for c in STRETCH_VALS:
+        circ = random_circuit(DEPTH)
+        circ_copy = deepcopy(circ)
+        # test random folding with seed
+        out = fold_local(circ, c, fold_gates_at_random, (42,))
+        apply_folding_tests(circ, circ_copy, out, c)
