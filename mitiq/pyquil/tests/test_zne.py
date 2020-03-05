685e38e01796d5bcfc26010ad81961316447df8b
andreamari <andreamari84@gmail.com>
2020-03-05 14:07:50 +0100

    black test_zne.py

--- mitiq/pyquil/tests/test_zne.py
+++ mitiq/pyquil/tests/test_zne.py
@@ -1,11 +1,20 @@
-# test_adaptive_zne.py
+"""Tests for test.zne.py."""
+
 import numpy as np
 
 from mitiq.factories import LinearFactory
 from mitiq.zne import qrun_factory, execute_with_zne, zne_decorator
-from mitiq.pyquil.pyquil_utils import random_identity_circuit, measure, run_program, scale_noise, Program, QVM
+from mitiq.pyquil.pyquil_utils import (
+    random_identity_circuit,
+    measure,
+    run_program,
+    scale_noise,
+    Program,
+    QVM,
+)
 
 TEST_DEPTH = 30
+
 
 def test_qrun_factory():
     rand_circ = random_identity_circuit(depth=TEST_DEPTH)
@@ -13,13 +22,14 @@ def test_qrun_factory():
     fac = LinearFactory([1.0, 2.0, 3.0])
     qrun_factory(fac, qp, run_program, scale_noise)
     result = fac.reduce()
-    assert np.isclose(result, 1.0, atol=1.e-1)
+    assert np.isclose(result, 1.0, atol=1.0e-1)
+
 
 def test_execute_with_zne():
     rand_circ = random_identity_circuit(depth=TEST_DEPTH)
     qp = measure(rand_circ, qid=0)
     result = execute_with_zne(qp, run_program, None, scale_noise)
-    assert np.isclose(result, 1.0, atol=1.e-1)
+    assert np.isclose(result, 1.0, atol=1.0e-1)
 
 
 def basic_executor(qp: Program, shots: int = 100) -> float:
@@ -27,6 +37,7 @@ def basic_executor(qp: Program, shots: int = 100) -> float:
     results = QVM.run(qp)
     expval = (results == [0]).sum() / shots
     return expval
+
 
 @zne_decorator()
 def decorated_executor(qp: Program, shots: int = 100) -> float:
@@ -39,7 +50,5 @@ def test_zne_decorator():
     # bar_result is computed with native noise (scale = 1)
     bad_result = basic_executor(scale_noise(qp, 1))
     good_result = decorated_executor(qp)
-    assert not np.isclose(bad_result, 1.0, atol=1.e-2)
-    assert np.isclose(good_result, 1.0, atol=1.e-2)
-
-
+    assert not np.isclose(bad_result, 1.0, atol=1.0e-2)
+    assert np.isclose(good_result, 1.0, atol=1.0e-2)
