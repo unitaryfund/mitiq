ba7d74b01f9749918f078c722f485ab999deb727
Will Zeng <willzeng@users.noreply.github.com>
2020-03-04 21:32:16 -0500

    Two different files for factories and (adaptive) zne. Adds simple fit algorithms. (#20)
    * initial adaptive framework
    
    * move scale noise in-place
    
    * refactor folders to seperate tests
    
    * fix default to qiskit for better CI
    
    * first draft
    
    * test added for first algorithm
    
    * linear extrapolator and test
    
    * PolyExtr and related test
    
    * minor
    
    * conflicts solved
    
    * Generators->Factories in algorithms.py
    
    * super(BatchedFactory.. and tests
    
    * Refactoring of (adaptive) zne and factories.
    New factories.py file with simple algorithms.
    Deleted old zne.py and old test_zne.py.
    Renamed adaptive_zne.py -> zne.py
    Changed tests consistently.
    
    * reduce made static and minor changes
    
    * lin and richardson from polyFactory
    
    * fix tests
    
    * scalars: Iterable[float]
    
    * minor
    
    * x, y renamed instack,outstack, apart static_reduce
    
    * typo fixed
    
    Co-authored-by: Will Zeng <willzeng@users.noreply.github.com>

---
+++ mitiq/factories.py
@@ -0,0 +1,109 @@
+from typing import List, Iterable
+import numpy as np
+
+
+class Factory(object):
+    """
+    This object adaptively produces new noise scaling parameters based on a historical stack of previous noise
+    scale parameters and previously calculated expectation values. Noise scaling parameters are stored on the `in_stack`
+    and the running list of expectation values are stored on the `out_stack`.
+    """
+    def __init__(self, instack: List[float], outstack: List[float]) -> None:
+        self.instack = instack
+        self.outstack = outstack
+
+    def step(self, instack: List[float], outstack: List[float]) -> None:
+        raise NotImplementedError
+
+    def is_converged(self, instack: List[float], outstack: List[float]) -> None:
+        raise NotImplementedError
+
+    def reduce(self) -> float:
+        raise NotImplementedError
+
+
+class BatchedFactory(Factory):
+    """
+    Runs a series of scalar noise parameters serially.
+    :param scalars: List of scalar noise values to be executed.
+    :param instack: Running stack of noise values run so far.
+    :param outstack: Expectations calculated thus far.
+    """
+    def __init__(self, scalars: Iterable[float], instack: List[float] = None, outstack: List[float] = None) -> None:
+        if instack is None:
+            instack = []
+        if outstack is None:
+            outstack = []
+        super(BatchedFactory, self).__init__(instack, outstack)
+        self.scalars = scalars
+
+    def step(self, instack: List[float], outstack: List[float]) -> float:
+        try:
+            next_param = self.scalars[len(outstack)]
+        except IndexError:
+            raise IndexError(f"BatchedFactory cannot take another step. Number of batched scalars ({len(instack)}) "
+                             f"exceeded.")
+        self.instack.append(next_param)
+        return next_param
+
+    def is_converged(self, instack: List[float], outstack: List[float]) -> bool:
+        return len(instack) == len(self.scalars)
+
+
+class RichardsonFactory(BatchedFactory):
+    """Factory object implementing Richardson's extrapolation."""
+    
+    def reduce(self, instack: List[float], outstack: List[float]) -> float:
+        """Returns the Richardson's extrapolation to the zero-noise limit."""
+        # Richardson's extrapolation is a particular case of a polynomial fit
+        # with order equal to the number of data points minus 1.
+        order = len(instack) - 1
+        return  PolyFactory.static_reduce(instack, outstack, order=order)
+
+class LinearFactory(BatchedFactory):
+    """Factory object implementing a zero-noise extrapolation algotrithm based on a linear fit."""
+    
+    def reduce(self, instack: List[float], outstack: List[float]) -> float:
+        """
+        Determines, with a least squared method, the line of best fit
+        associated to the data points. The intercept is returned.
+        """
+        # Richardson's extrapolation is a particular case of a polynomial fit
+        # with order equal to 1.
+        return PolyFactory.static_reduce(instack, outstack, order=1)
+
+
+class PolyFactory(BatchedFactory):
+    """
+    Factory object implementing a zero-noise extrapolation algotrithm based on a polynomial fit.     
+    Note: RichardsonFactory and LinearFactory are special cases of PolyFactory.
+    """
+    @staticmethod
+    def static_reduce(x: List[float], y: List[float], order: int) -> float:
+        """
+        Static method equivalent to the reduce instance method of PolyFactory.
+        This method is also called by other factories, e.g., LinearFactory and RichardsonFactory.
+        """
+
+        # check arguments
+        error_str = "Data is not enough: at least two data points are necessary."
+        if (x is None) or (y is None):
+            raise ValueError(error_str) 
+        if (len(x) != len(y)) or (len(x)<2):
+            raise ValueError(error_str) 
+        if order > len(x) - 1:
+            raise ValueError(
+                "Extrapolation order is too high. The order cannot exceed the number of data points minus 1."
+            )
+        
+        # get coefficients c_j of p(x)= c_0 + c_1*x + c_2*x**2... which best fits the data
+        coefficients = np.polyfit(x, y, deg=order)
+        # c_0, i.e., p(x=0), is returned
+        return coefficients[-1]
+
+    def reduce(self, instack: List[float], outstack: List[float], order: int) -> float:
+        """
+        Determines with a least squared method, the polynomial of degree equal to 'order' 
+        which optimally fits the input data. The zero-noise limit is returned.
+        """
+        return PolyFactory.static_reduce(instack, outstack, order)
---
+++ mitiq/tests/test_factories.py
@@ -0,0 +1,65 @@
+# test_algorithms.py
+
+from typing import Callable
+import numpy as np
+from mitiq.factories import Factory, RichardsonFactory, LinearFactory, PolyFactory
+
+
+# Constant parameters for test functions:
+A = 1.2
+B = 1.5
+C = 1.7
+D = 0.9
+X_VALS = [1, 1.4, 1.9]
+
+# two test functions (f_lin and f_non_lin):
+def f_lin(x: float) -> float:
+    """Linear function."""
+    return A + B * x
+
+
+def f_non_lin(x: float) -> float:
+    """Non-linear function."""
+    return A + B * x + C * x ** 2
+
+
+def apply_algorithm(algo_class: Factory, f: Callable[[float], float], order: float = None) -> float:
+    """Applies a generc algorithm for extrapolating a given (classical) test function f(x).
+    Returns an estimate of f(0).
+
+    Args:
+        algo_class: class of type Factory corresponding to a specific extrapolation method.
+        f: test function to be extrapolated.
+        order: (optional) extrapolation order.
+    """
+    y_vals = [f(x) for x in X_VALS]
+    algo_object = algo_class(X_VALS, X_VALS, y_vals)
+    if order is None:
+        return algo_object.reduce(X_VALS, y_vals)
+    return algo_object.reduce(X_VALS, y_vals, order)
+
+
+def test_richardson_extr():
+    """Test of the Richardson's extrapolator."""
+    for f in [f_lin, f_non_lin]:
+        f_of_zero = apply_algorithm(RichardsonFactory, f)
+        assert np.isclose(f_of_zero, f(0), atol=1.0e-7)
+
+
+def test_linear_extr():
+    """Test of linear extrapolator."""
+    f_of_zero = apply_algorithm(LinearFactory, f_lin)
+    assert np.isclose(f_of_zero, f_lin(0), atol=1.0e-7)
+
+
+def test_poly_extr():
+    """Test of polynomial extrapolator."""
+    # test linear extrapolation (order=1)
+    f_of_zero = apply_algorithm(PolyFactory, f_lin, 1)
+    assert np.isclose(f_of_zero, f_lin(0), atol=1.0e-7)
+    # test that, for some non-linear functions,
+    # order=1 is bad while ored=2 is better.
+    f_of_zero = apply_algorithm(PolyFactory, f_non_lin, 1)
+    assert not np.isclose(f_of_zero, f_non_lin(0), atol=1)
+    f_of_zero = apply_algorithm(PolyFactory, f_non_lin, 2)
+    assert np.isclose(f_of_zero, f_non_lin(0), atol=1.0e-7)
