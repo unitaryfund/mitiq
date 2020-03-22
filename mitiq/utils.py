e4b07f4f148283a092aa3e9d867d416a5d0097f1
Ryan LaRose <rlarose@umich.edu>
2020-03-21 18:26:30 -0700

    Unit tests + fixes for utils.py.

--- mitiq/utils.py
+++ mitiq/utils.py
@@ -1,12 +1,22 @@
 """Utility functions."""
 
+from copy import deepcopy
 import random
 
 import cirq
 
 
 def random_circuit(depth: int, **kwargs) -> cirq.Circuit:
-    """Returns a random single-qubit circuit with Pauli gates."""
+    """Returns a random single-qubit circuit with Pauli gates.
+
+    Parameters
+    ----------
+        depth: Number of gates in the circuit.
+
+    Keyword Args
+    ------------
+        seed: Sets seed for the random number generator to input `seed`.
+    """
     if "seed" in kwargs.keys():
         random.seed(kwargs.get("seed"))
 
@@ -17,11 +27,43 @@ def random_circuit(depth: int, **kwargs) -> cirq.Circuit:
     return circuit
 
 
-def _equal(circuit_one: cirq.Circuit, circuit_two: cirq.Circuit) -> bool:
-    """Returns True if circuits are equal.
+def _equal(
+        circuit_one: cirq.Circuit,
+        circuit_two: cirq.Circuit,
+        require_qubit_equality: bool = False
+) -> bool:
+    """Returns True if the circuits are equal, else False.
 
-    Args:
+    Parameters
+    ----------
         circuit_one: Input circuit to compare to circuit_two.
         circuit_two: Input circuit to compare to circuit_one.
+        require_qubit_equality: Requires that the qubits be equal in the two circuits.
+
+                                E.g., if set(circuit_one.all_qubits()) = {LineQubit(0)},
+                                then set(circuit_two_all_qubits()) must be {LineQubit(0)},
+                                else the two are not equal.
+
+                                If True, the qubits of both circuits must have a well-defined ordering.
     """
+    if circuit_one is circuit_two:
+        return True
+
+    if not require_qubit_equality:
+        print("Transforming qubits of circuit one...")
+        qubit_map = dict(zip(
+            sorted(circuit_one.all_qubits()), sorted(circuit_two.all_qubits())
+        ))
+        print("The qubit map is:")
+        print(qubit_map)
+        circuit_one = circuit_one.transform_qubits(lambda q: qubit_map[q])
+        print("New qubits of circuit one:")
+        print(circuit_one.all_qubits())
+        print("New qubits of circuit two:")
+        print(circuit_two.all_qubits())
+        print("Done")
+        print("Comparing equality between the circuits:")
+        print(circuit_one)
+        print(circuit_two)
+
     return cirq.CircuitDag.from_circuit(circuit_one) == cirq.CircuitDag.from_circuit(circuit_two)
