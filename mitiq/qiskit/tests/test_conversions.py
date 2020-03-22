0f4d7f624821c163109d5aebece63dd2bb527da6
Ryan LaRose <rlarose@umich.edu>
2020-03-21 18:38:08 -0700

    Adds (not that many) unit tests for circuit conversions.

--- mitiq/qiskit/tests/test_conversions.py
+++ mitiq/qiskit/tests/test_conversions.py
@@ -3,21 +3,48 @@
 import cirq
 import qiskit.extensions.standard as qiskit_ops
 
+from mitiq.utils import (_equal, random_circuit)
 from mitiq.qiskit.conversions import (_to_qasm, _to_qiskit, _from_qasm, _from_qiskit)
 
 
-def test_to_from_bell_state():
-    """Minimal test for converting a Bell state circuit."""
+def test_bell_state_to_from_circuits():
+    """Tests cirq.Circuit --> qiskit.QuantumCircuit --> cirq.Circuit with a Bell state circuit."""
     qreg = cirq.LineQubit.range(2)
     cirq_circuit = cirq.Circuit(
         [cirq.ops.H.on(qreg[0]), cirq.ops.CNOT.on(qreg[0], qreg[1])]
     )
-    qiskit_circuit = _to_qiskit(cirq_circuit)
-
-    circuit_cirq = _from_qiskit(qiskit_circuit)
-    print(circuit_cirq)
+    qiskit_circuit = _to_qiskit(cirq_circuit)  # Qiskit from Cirq
     ops = qiskit_circuit.data
     assert len(ops) == 2
     assert isinstance(ops[0], qiskit_ops.HGate)
     assert isinstance(ops[1], qiskit_ops.CnotGate)
-    assert False
+
+    circuit_cirq = _from_qiskit(qiskit_circuit)  # Cirq from Qiskit
+    assert _equal(cirq_circuit, circuit_cirq)
+
+
+def test_bell_state_to_from_qasm():
+    """Tests cirq.Circuit --> QASM string --> cirq.Circuit with a Bell state circuit."""
+    qreg = cirq.LineQubit.range(2)
+    cirq_circuit = cirq.Circuit(
+        [cirq.ops.H.on(qreg[0]), cirq.ops.CNOT.on(qreg[0], qreg[1])]
+    )
+    qasm = _to_qasm(cirq_circuit)  # Qasm from Cirq
+    circuit_cirq = _from_qasm(qasm)
+    assert _equal(cirq_circuit, circuit_cirq)
+
+
+def test_random_circuit_to_from_circuits():
+    """Tests cirq.Circuit --> qiskit.QuantumCircuit --> cirq.Circuit with a random one-qubit circuit."""
+    cirq_circuit = random_circuit(depth=20)
+    qiskit_circuit = _to_qiskit(cirq_circuit)
+    circuit_cirq = _from_qiskit(qiskit_circuit)
+    assert _equal(cirq_circuit, circuit_cirq)
+
+
+def test_random_circuit_to_from_qasm():
+    """Tests cirq.Circuit --> QASM string --> cirq.Circuit with a random one-qubit circuit."""
+    cirq_circuit = random_circuit(depth=20)
+    qasm = _to_qasm(cirq_circuit)
+    circuit_cirq = _from_qasm(qasm)
+    assert _equal(cirq_circuit, circuit_cirq)
