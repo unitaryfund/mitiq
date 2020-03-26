ccd3e80f9094d855bf5a4539a690e13e3d138db0
Nathan Shammah <nathan.shammah@gmail.com>
2020-03-26 21:39:33 +0100

    polish comments and docstrings

--- mitiq/matrices.py
+++ mitiq/matrices.py
@@ -1,18 +1,6 @@
 # matrices.py
 import numpy as np
 
-# def function_with_pep484_type_annotations(param1: int, param2: str) -> bool:
-#    """Example function with PEP 484 type annotations.
-#
-#    Args:
-#        param1: The first parameter.
-#        param2: The second parameter.
-#
-#    Returns:
-#        The return value. True for success, False otherwise.
-#
-#    """
-
 def npI() -> ndarray:
     """Returns the identity matrix in SU(2) algebra.
 
@@ -24,7 +12,7 @@ def npI() -> ndarray:
     return np_identity
 
 def npX() -> ndarray:
-    """Returns the sigma_x matrix in SU(2) algebra.
+    """Returns the sigma_x Pauli matrix in SU(2) algebra.
 
     Returns:
         The sigma_x matrix as a numpy.array.
@@ -34,7 +22,7 @@ def npX() -> ndarray:
     return sigma_x
 
 def npY() -> ndarray:
-    """Returns the sigma_y matrix in SU(2) algebra.
+    """Returns the sigma_y Pauli matrix in SU(2) algebra.
 
     Returns:
         The sigma_y matrix as a numpy.array.
@@ -44,7 +32,7 @@ def npY() -> ndarray:
     return sigma_y
 
 def npZ() -> ndarray:
-    """Returns the sigma_z matrix in SU(2) algebra.
+    """Returns the sigma_z Pauli matrix in SU(2) algebra.
 
     Returns:
         The sigma_z matrix as a numpy.array.
