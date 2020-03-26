b72a91b90e77a47712c4d7d674f2ba6859a337b7
Nathan Shammah <nathan.shammah@gmail.com>
2020-03-26 13:52:22 +0100

    line limit to 79 in test_utils

--- mitiq/tests/test_utils.py
+++ mitiq/tests/test_utils.py
@@ -4,7 +4,7 @@ import pytest
 
 import cirq
 
-from mitiq.utils import (_equal, random_circuit)
+from mitiq.utils import _equal, random_circuit
 
 
 def test_random_circuit_simple():
@@ -13,36 +13,24 @@ def test_random_circuit_simple():
     assert len(list(circuit.all_operations())) == n
 
 
-@pytest.mark.parametrize(
-    ["require_qubit_equality"],
-    [[True], [False]]
-)
+@pytest.mark.parametrize(["require_qubit_equality"], [[True], [False]])
 def test_circuit_equality_identical_qubits(require_qubit_equality):
     qreg = cirq.NamedQubit.range(5, prefix="q_")
-    circA = cirq.Circuit(
-        cirq.ops.H.on_each(*qreg)
-    )
-    circB = cirq.Circuit(
-        cirq.ops.H.on_each(*qreg)
-    )
+    circA = cirq.Circuit(cirq.ops.H.on_each(*qreg))
+    circB = cirq.Circuit(cirq.ops.H.on_each(*qreg))
     assert circA is not circB
     assert _equal(circA, circB, require_qubit_equality=require_qubit_equality)
 
 
-@pytest.mark.parametrize(
-    ["require_qubit_equality"],
-    [[True], [False]]
-)
-def test_circuit_equality_nonidentical_but_equal_qubits(require_qubit_equality):
+@pytest.mark.parametrize(["require_qubit_equality"], [[True], [False]])
+def test_circuit_equality_nonidentical_but_equal_qubits(
+    require_qubit_equality,
+):
     n = 5
     qregA = cirq.NamedQubit.range(n, prefix="q_")
     qregB = cirq.NamedQubit.range(n, prefix="q_")
-    circA = cirq.Circuit(
-        cirq.ops.H.on_each(*qregA)
-    )
-    circB = cirq.Circuit(
-        cirq.ops.H.on_each(*qregB)
-    )
+    circA = cirq.Circuit(cirq.ops.H.on_each(*qregA))
+    circB = cirq.Circuit(cirq.ops.H.on_each(*qregB))
     assert circA is not circB
     assert _equal(circA, circB, require_qubit_equality=require_qubit_equality)
 
@@ -51,12 +39,8 @@ def test_circuit_equality_linequbit_gridqubit_equal_indices():
     n = 10
     qregA = cirq.LineQubit.range(n)
     qregB = [cirq.GridQubit(x, 0) for x in range(n)]
-    circA = cirq.Circuit(
-        cirq.ops.H.on_each(*qregA)
-    )
-    circB = cirq.Circuit(
-        cirq.ops.H.on_each(*qregB)
-    )
+    circA = cirq.Circuit(cirq.ops.H.on_each(*qregA))
+    circB = cirq.Circuit(cirq.ops.H.on_each(*qregB))
     assert _equal(circA, circB, require_qubit_equality=False)
     assert not _equal(circA, circB, require_qubit_equality=True)
 
@@ -65,12 +49,8 @@ def test_circuit_equality_linequbit_gridqubit_unequal_indices():
     n = 10
     qregA = cirq.LineQubit.range(n)
     qregB = [cirq.GridQubit(x + 3, 0) for x in range(n)]
-    circA = cirq.Circuit(
-        cirq.ops.H.on_each(*qregA)
-    )
-    circB = cirq.Circuit(
-        cirq.ops.H.on_each(*qregB)
-    )
+    circA = cirq.Circuit(cirq.ops.H.on_each(*qregA))
+    circB = cirq.Circuit(cirq.ops.H.on_each(*qregB))
     assert _equal(circA, circB, require_qubit_equality=False)
     assert not _equal(circA, circB, require_qubit_equality=True)
 
@@ -79,12 +59,8 @@ def test_circuit_equality_linequbit_namedqubit_equal_indices():
     n = 8
     qregA = cirq.LineQubit.range(n)
     qregB = cirq.NamedQubit.range(n, prefix="q_")
-    circA = cirq.Circuit(
-        cirq.ops.H.on_each(*qregA)
-    )
-    circB = cirq.Circuit(
-        cirq.ops.H.on_each(*qregB)
-    )
+    circA = cirq.Circuit(cirq.ops.H.on_each(*qregA))
+    circB = cirq.Circuit(cirq.ops.H.on_each(*qregB))
     assert _equal(circA, circB, require_qubit_equality=False)
     assert not _equal(circA, circB, require_qubit_equality=True)
 
@@ -93,12 +69,8 @@ def test_circuit_equality_linequbit_namedqubit_unequal_indices():
     n = 11
     qregA = cirq.LineQubit.range(n)
     qregB = [cirq.NamedQubit(str(x + 10)) for x in range(n)]
-    circA = cirq.Circuit(
-        cirq.ops.H.on_each(*qregA)
-    )
-    circB = cirq.Circuit(
-        cirq.ops.H.on_each(*qregB)
-    )
+    circA = cirq.Circuit(cirq.ops.H.on_each(*qregA))
+    circB = cirq.Circuit(cirq.ops.H.on_each(*qregB))
     assert _equal(circA, circB, require_qubit_equality=False)
     assert not _equal(circA, circB, require_qubit_equality=True)
 
@@ -107,12 +79,8 @@ def test_circuit_equality_gridqubit_namedqubit_equal_indices():
     n = 8
     qregA = [cirq.GridQubit(0, x) for x in range(n)]
     qregB = cirq.NamedQubit.range(n, prefix="q_")
-    circA = cirq.Circuit(
-        cirq.ops.H.on_each(*qregA)
-    )
-    circB = cirq.Circuit(
-        cirq.ops.H.on_each(*qregB)
-    )
+    circA = cirq.Circuit(cirq.ops.H.on_each(*qregA))
+    circB = cirq.Circuit(cirq.ops.H.on_each(*qregB))
     assert _equal(circA, circB, require_qubit_equality=False)
     assert not _equal(circA, circB, require_qubit_equality=True)
 
@@ -121,11 +89,7 @@ def test_circuit_equality_gridqubit_namedqubit_unequal_indices():
     n = 5
     qregA = [cirq.GridQubit(x + 2, 0) for x in range(n)]
     qregB = [cirq.NamedQubit(str(x + 10)) for x in range(n)]
-    circA = cirq.Circuit(
-        cirq.ops.H.on_each(*qregA)
-    )
-    circB = cirq.Circuit(
-        cirq.ops.H.on_each(*qregB)
-    )
+    circA = cirq.Circuit(cirq.ops.H.on_each(*qregA))
+    circB = cirq.Circuit(cirq.ops.H.on_each(*qregB))
     assert _equal(circA, circB, require_qubit_equality=False)
     assert not _equal(circA, circB, require_qubit_equality=True)
