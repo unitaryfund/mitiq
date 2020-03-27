a2d339eb4265dc6ea1f8648c562a639f281d04f2
Nathan Shammah <nathan.shammah@gmail.com>
2020-03-27 17:01:14 +0100

    include docstring suggestions simplification in test of pauli matrices

--- mitiq/tests/test_matrices.py
+++ mitiq/tests/test_matrices.py
@@ -5,21 +5,21 @@ from mitiq.matrices import npI, npX, npY, npZ
 
 
 def test_npI() -> AssertionError:
-    """Test square of identity in npI is a projector"""
+    """Test square of npI is a projector (identity)."""
     assert_array_equal(npI.dot(npI), npI)
 
 def test_npX() -> AssertionError:
-    """Test square of sigma_x of npX"""
+    """Test square of npX."""
     assert_array_equal(npX.dot(npX), npI)
 
 def test_npY() -> AssertionError:
-    """Test square of sigma_y of npY"""
+    """Test square of npY."""
     assert_array_equal(npY.dot(npY), npI)
 
 def test_npZ() -> AssertionError:
-    """Test square of sigma_z of npZ"""
+    """Test square of npZ."""
     assert_array_equal(npZ.dot(npZ), npI)
 
 def test_matrices_algebra() -> AssertionError:
-    """Test SU(2) algebra with commutation relations"""
+    """Test commutation relations for Pauli matrices as in SU(2) algebra."""
     assert_array_equal(npX.dot(npY)-npY.dot(npX), 2*1j*npZ)
