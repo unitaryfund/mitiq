40d400b59936686be49f3dab73a2983793333efc
Nathan Shammah <nathan.shammah@gmail.com>
2020-03-27 15:26:04 +0100

    fix tests on matrices with numpy.testing assert array equal and annotations

--- mitiq/tests/test_matrices.py
+++ mitiq/tests/test_matrices.py
@@ -4,19 +4,22 @@ from numpy.testing import assert_array_equal
 from mitiq.matrices import npI, npX, npY, npZ
 
 
-def test_npX() -> None:
+def test_npI() -> AssertionError:
     """Test square of identity in npI is a projector"""
-    assert_array_equal(npI**2, npI)
+    assert_array_equal(npI().dot(npI()), npI())
 
-def test_npX() -> None:
+def test_npX() -> AssertionError:
     """Test square of sigma_y of npX"""
-    assert_array_equal(npX**2, npI)
+    assert_array_equal(npX().dot(npX()), npI())
 
-def test_npY() -> None:
+def test_npY() -> AssertionError:
     """Test square of sigma_y of npY"""
-    assert_array_equal(npY**2, npI)
+    assert_array_equal(npY().dot(npY()), npI())
 
-def test_npZ() -> None:
+def test_npZ() -> AssertionError:
     """Test square of sigma_z of npZ"""
-    assert_array_equal(npZ**2, npI)
+    assert_array_equal(npZ().dot(npZ()), npI())
 
+def test_matrices_algebra() -> AssertionError:
+    """Test SU(2) algebra with commutation relations"""
+    assert_array_equal(npI().dot(npI()), npI())
