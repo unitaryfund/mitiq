95ae49415770f0f59f618e891776340cba222cad
Nathan Shammah <nathan.shammah@gmail.com>
2020-03-27 16:52:07 +0100

    redefine pauli matrices as variables

--- mitiq/matrices.py
+++ mitiq/matrices.py
@@ -1,42 +1,14 @@
 # matrices.py
 import numpy as np
 
-def npI() -> np.ndarray:
-    """Returns the identity matrix in SU(2) algebra.
+npI = np.array([[1, 0], [0, 1]])
+"""Returns the identity matrix in SU(2) algebra as a (2,2) `np.array`."""
 
-    Returns:
-        The identity matrix as a numpy.array.
+npX = np.array([[0, 1], [1, 0]])
+"""Defines the sigma_x Pauli matrix in SU(2) algebra as a (2,2) `np.array`."""
 
-    """
-    np_identity = np.array([[1, 0], [0, 1]])
-    return np_identity
+npY = np.array([[0, -1j], [1j, 0]])
+"""Defines the sigma_y Pauli matrix in SU(2) algebra as a (2,2) `np.array`."""
 
-def npX() -> np.ndarray:
-    """Returns the sigma_x Pauli matrix in SU(2) algebra.
-
-    Returns:
-        The sigma_x matrix as a numpy.array.
-
-    """
-    sigma_x = np.array([[0, 1], [1, 0]])
-    return sigma_x
-
-def npY() -> np.ndarray:
-    """Returns the sigma_y Pauli matrix in SU(2) algebra.
-
-    Returns:
-        The sigma_y matrix as a numpy.array.
-
-    """
-    sigma_y = np.array([[0, -1j], [1j, 0]])
-    return sigma_y
-
-def npZ() -> np.ndarray:
-    """Returns the sigma_z Pauli matrix in SU(2) algebra.
-
-    Returns:
-        The sigma_z matrix as a numpy.array.
-
-    """
-    sigma_z = np.array([[1, 0], [0, -1]])
-    return sigma_z
+npZ = np.array([[1, 0], [0, -1]])
+"""Defines the sigma_z Pauli matrix in SU(2) algebra as a (2,2) `np.array`."""
