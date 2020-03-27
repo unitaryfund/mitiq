4c5dfa786a623d8e2c04477f5aa7ac1d3bb5bfc0
Nathan Shammah <nathan.shammah@gmail.com>
2020-03-27 11:19:24 +0100

    fix matrices tests using numpy.testing.assert_array_equal

--- mitiq/tests/test_matrices.py
+++ mitiq/tests/test_matrices.py
@@ -1,21 +1,22 @@
 """Testing of zero-noise extrapolation methods (factories) with classically generated data."""
 import numpy as np
+from numpy.testing import assert_array_equal
 from mitiq.matrices import npI, npX, npY, npZ
 
 
 def test_npX() -> None:
     """Test square of identity in npI is a projector"""
-    assert npI**2 == npI
+    assert_array_equal(npI**2, npI)
 
 def test_npX() -> None:
     """Test square of sigma_y of npX"""
-    assert npX**2 == npI
+    assert_array_equal(npX**2, npI)
 
 def test_npY() -> None:
     """Test square of sigma_y of npY"""
-    assert npY**2 == npI
+    assert_array_equal(npY**2, npI)
 
 def test_npZ() -> None:
     """Test square of sigma_z of npZ"""
-    assert npZ**2 == npI
+    assert_array_equal(npZ**2, npI)
 
