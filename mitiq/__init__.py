a4e7a449ed61e42fbef7c2a554eb629c8f8398bc
Will Zeng <zeng.will@gmail.com>
2020-04-02 17:35:40 -0400

    make cirq Circuits the default QPROGRAM type

--- mitiq/__init__.py
+++ mitiq/__init__.py
@@ -6,10 +6,12 @@ Mitiq can be directly imported.
 import os
 from typing import Union
 
+from cirq import Circuit
+
 
 # This is used to optionally import what program types should be allowed
 # by mitiq based on what packages are installed in the environment
-PROGRAM_TYPES = []
+PROGRAM_TYPES = [Circuit]
 try:
     from pyquil import Program
     PROGRAM_TYPES.append(Program)
@@ -22,11 +24,7 @@ try:
 except ImportError:
     pass
 
-if len(PROGRAM_TYPES) == 0:
-    class QPROGRAM():
-        pass
-else:
-    QPROGRAM = Union[tuple(PROGRAM_TYPES)]
+QPROGRAM = Union[tuple(PROGRAM_TYPES)]
 
 # this must be after QPROGRAM as the zne.py module imports QPROGRAM
 from mitiq.zne import execute_with_zne, mitigate_executor
