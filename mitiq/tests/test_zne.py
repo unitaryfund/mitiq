3bca647bf7860102a3298386552a149325aca454
Will Zeng <zeng.will@gmail.com>
2020-04-04 13:13:07 -0400

    Add methods for testing mitiq against random circuits of arbitrary size and different parameters.

--- mitiq/tests/test_zne.py
+++ mitiq/tests/test_zne.py
@@ -3,11 +3,15 @@
 
 import numpy as np
 import pytest
+from itertools import product
 
 from cirq import Circuit, depolarize, LineQubit, X, DensityMatrixSimulator
 
 from mitiq import execute_with_zne, mitigate_executor
-from mitiq.factories import LinearFactory
+from mitiq.factories import LinearFactory, RichardsonFactory, PolyFactory
+from mitiq.folding import fold_gates_at_random, fold_gates_from_left, \
+    fold_gates_from_right
+from mitiq.testing import rand_benchmark_zne
 
 SIMULATOR = DensityMatrixSimulator()
 
@@ -34,7 +38,8 @@ def noisy_simulation(circ: Circuit, shots=None) -> float:
     expectation = np.real(np.trace(rho @ obs))
     return expectation
 
-
+# This test is somewhat redundant with the random circuit tests but we keep in
+# because this tests the exact code used in the getting started.
 @pytest.mark.parametrize(["depth"], [[n] for n in range(10, 80, 20)])
 def test_cirq_zne(depth):
     # This test runs circuits with an even number of X gates at varying
@@ -61,3 +66,30 @@ def test_cirq_zne(depth):
     run_mitigated = mitigate_executor(noisy_simulation)
     e_mitigated = run_mitigated(circ)
     assert np.isclose(e_mitigated, mitigated)
+
+
+SCALE_FUNCTIONS = [
+    fold_gates_at_random,
+    fold_gates_from_left,
+    fold_gates_from_right
+]
+
+FACTORIES = [
+    RichardsonFactory([1.0, 1.4, 2.1]),
+    LinearFactory([1.0, 1.6]),
+    PolyFactory([1.0, 1.4, 2.1], order=2)
+]
+
+
+@pytest.mark.parametrize(["scale_noise", "fac"],
+                         product(SCALE_FUNCTIONS, FACTORIES))
+def test_random_benchmarks(scale_noise, fac):
+    unmit_err, mit_err = rand_benchmark_zne(n_qubits=2,
+                                            depth=20,
+                                            trials=8,
+                                            op_density=0.99,
+                                            noise=0.003,
+                                            fac=fac,
+                                            scale_noise=scale_noise)
+
+    assert np.average(unmit_err) >= np.average(mit_err)
