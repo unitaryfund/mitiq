b06c01c03e8710080df3cad8b8335c8f1626d30d
Nathan Shammah <nathan.shammah@gmail.com>
2020-05-15 18:15:18 +0200

    add pytest-cov and aux files, docs

--- test_build.sh
+++ test_build.sh
@@ -2,9 +2,7 @@
 
 if [ "$1" != "-docs" ]; then
     echo "Running Tests...";
-    pytest mitiq/tests;
-    pytest mitiq/benchmarks/tests;
-    pytest mitiq/mitiq_qiskit/tests;
+    pytest --cov=mitiq mitiq/tests mitiq/benchmarks/tests mitiq/mitiq_qiskit/tests;
 elif [ "$1" != "-tests" ]; then
     echo "Building and Testing Docs...";
     cd docs && make html;
