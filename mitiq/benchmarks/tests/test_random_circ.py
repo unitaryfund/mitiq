884c61f06ecd667fb4d891fd804e4fe7129afbe8
Will Zeng <zeng.will@gmail.com>
2020-05-20 17:18:59 -0400

    update random circuit benchmark

--- mitiq/benchmarks/tests/test_random_circ.py
+++ mitiq/benchmarks/tests/test_random_circ.py
@@ -6,7 +6,7 @@ import numpy as np
 from mitiq.factories import LinearFactory, RichardsonFactory, PolyFactory
 from mitiq.folding import fold_gates_at_random, fold_gates_from_left, \
     fold_gates_from_right
-from mitiq.benchmarks.random_circ import rand_benchmark_zne, sample_projector
+from mitiq.benchmarks.random_circ import rand_circuit_zne, sample_projector
 
 # Fix a seed for this test file
 SEED = 808
@@ -33,15 +33,14 @@ FACTORIES = [
 @pytest.mark.parametrize(["scale_noise", "fac"],
                          product(SCALE_FUNCTIONS, FACTORIES))
 def test_random_benchmarks(scale_noise, fac):
-
-    exact, unmitigated, mitigated = rand_benchmark_zne(n_qubits=2,
-                                                       depth=20,
-                                                       trials=8,
-                                                       op_density=0.99,
-                                                       noise=0.003,
-                                                       fac=fac,
-                                                       scale_noise=scale_noise,
-                                                       seed=SEED)
+    exact, unmitigated, mitigated = rand_circuit_zne(n_qubits=2,
+                                                     depth=20,
+                                                     trials=8,
+                                                     op_density=0.99,
+                                                     noise=0.003,
+                                                     fac=fac,
+                                                     scale_noise=scale_noise,
+                                                     seed=SEED)
 
     unmit_err = np.abs(exact - unmitigated)
     mit_err = np.abs(exact - mitigated)
