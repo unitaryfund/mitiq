84deb7cf16ef2df210633a7a5fac16cf4cc6cfb1
Will Zeng <zeng.will@gmail.com>
2020-05-20 18:29:50 -0400

    add rb systematic tests

--- mitiq/benchmarks/rand_bench.py
+++ mitiq/benchmarks/rand_bench.py
@@ -3,7 +3,7 @@
 Contains methods used for testing mitiq's performance on randomized
 benchmarking circuits.
 """
-from typing import List, Tuple
+from typing import List
 import numpy as np
 
 from cirq.experiments.qubit_characterizations import _single_qubit_cliffords, \
@@ -11,9 +11,9 @@ from cirq.experiments.qubit_characterizations import _single_qubit_cliffords, \
     _two_qubit_clifford_matrices
 from cirq import NamedQubit, Circuit
 
-cliffords = _single_qubit_cliffords()
-c1 = cliffords.c1_in_xy
-cfd_mats = np.array([_gate_seq_to_mats(gates) for gates in c1])
+CLIFFORDS = _single_qubit_cliffords()
+C1 = CLIFFORDS.c1_in_xy
+CFD_MAT_1Q = np.array([_gate_seq_to_mats(gates) for gates in C1])
 
 
 def rb_circuits(n_qubits: int, num_cfds: List[int], trials: int) -> \
@@ -23,16 +23,16 @@ def rb_circuits(n_qubits: int, num_cfds: List[int], trials: int) -> \
         qubit1 = NamedQubit("0")
         if n_qubits == 1:
             rb_circuits = [
-                _random_single_q_clifford(qubit1, num, c1, cfd_mats)
+                _random_single_q_clifford(qubit1, num, C1, CFD_MAT_1Q)
                 for _ in range(trials)
             ]
         elif n_qubits == 2:
             qubit2 = NamedQubit("1")
             cfd_matrices = _two_qubit_clifford_matrices(qubit1, qubit2,
-                                                        cliffords)
+                                                        CLIFFORDS)
             rb_circuits = [
                 _random_two_q_clifford(qubit1, qubit2, num, cfd_matrices,
-                                       cliffords)
+                                       CLIFFORDS)
                 for _ in range(trials)
             ]
         else:
--- mitiq/benchmarks/tests/test_rand_bench.py
+++ mitiq/benchmarks/tests/test_rand_bench.py
@@ -1,7 +1,26 @@
 import pytest
+from itertools import product
 import numpy as np
 
 from mitiq.benchmarks.rand_bench import rb_circuits
+from mitiq.factories import LinearFactory, RichardsonFactory, PolyFactory
+from mitiq.folding import fold_gates_at_random, fold_gates_from_left, \
+    fold_gates_from_right, fold_global
+from mitiq.benchmarks.utils import noisy_simulation
+from mitiq.zne import mitigate_executor
+
+SCALE_FUNCTIONS = [
+    fold_gates_at_random,
+    fold_gates_from_left,
+    fold_gates_from_right,
+    fold_global
+]
+
+FACTORIES = [
+    RichardsonFactory([1.0, 1.4, 2.1]),
+    LinearFactory([1.0, 1.6]),
+    PolyFactory([1.0, 1.4, 2.1], order=2),
+]
 
 
 def test_rb_circuits():
@@ -13,7 +32,7 @@ def test_rb_circuits():
         for qc in circuits:
             # we check the ground state population to ignore any global phase
             wvf = qc.final_wavefunction()
-            zero_prob = abs(wvf[0]**2)
+            zero_prob = abs(wvf[0] ** 2)
             assert np.isclose(zero_prob, 1)
 
     # test two qubit RB
@@ -22,5 +41,29 @@ def test_rb_circuits():
         for qc in circuits:
             # we check the ground state population to ignore any global phase
             wvf = qc.final_wavefunction()
-            zero_prob = abs(wvf[0]**2)
+            zero_prob = abs(wvf[0] ** 2)
             assert np.isclose(zero_prob, 1)
+
+
+@pytest.mark.parametrize(["scale_noise", "fac"],
+                         product(SCALE_FUNCTIONS, FACTORIES))
+def test_random_benchmarks(scale_noise, fac):
+    depths = [2, 4]
+    trials = 3
+    circuits = rb_circuits(n_qubits=2, num_cfds=depths, trials=trials)
+    noise = 0.01
+    obs = np.diag([1, 0, 0, 0])
+
+    executor = lambda qc: noisy_simulation(qc, noise=noise, obs=obs)
+    mit_executor = mitigate_executor(executor, fac, scale_noise)
+
+    unmitigated = []
+    mitigated = []
+    for qc in circuits:
+        unmitigated.append(executor(qc))
+        mitigated.append(mit_executor(qc))
+
+    unmit_err = np.abs(1.0 - np.asarray(unmitigated))
+    mit_err = np.abs(1.0 - np.asarray(mitigated))
+
+    assert np.average(unmit_err) >= np.average(mit_err)
