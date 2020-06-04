63c88607cdbf3a4ee84d317559142692fbcb723f
Nathan Shammah <nathan.shammah@gmail.com>
2020-06-04 15:02:09 +0200

    add comment in test build

--- test_build.sh
+++ test_build.sh
@@ -10,3 +10,13 @@ elif [ "$1" != "-tests" ]; then
     cd docs && make html;
     make doctest;
 fi
+
+
+# if [ "$1" != "-docs" ]; then
+#     echo "Running Tests...";
+#     pytest --cov=mitiq mitiq/tests mitiq/benchmarks/tests mitiq/mitiq_qiskit/tests;
+# elif [ "$1" != "-tests" ]; then
+#     echo "Building and Testing Docs...";
+#     cd docs && make html;
+#     make doctest;
+# fi
