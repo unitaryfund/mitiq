45048275c2a87d67a8f5bd8d91ac9797259d1f95
Ryan LaRose <rlarose@umich.edu>
2020-07-02 09:19:38 -0700

    Imports in mitiq_qiskit. Removes unused seed for random_identity_circuit + renames function. Moves `measure` from utils to the test file. (#227)

---
+++ mitiq/mitiq_qiskit/__init__.py
@@ -0,0 +1,10 @@
+from mitiq.mitiq_qiskit.conversions import (
+    from_qasm,
+    from_qiskit,
+    to_qasm,
+    to_qiskit
+)
+from mitiq.mitiq_qiskit.qiskit_utils import (
+    random_one_qubit_identity_circuit,
+    run_with_noise
+)
