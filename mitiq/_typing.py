b8485a6b0ee5c634614aef4fc3568ee250ae2828
Peter Karalekas <peter@karalekas.com>
2020-07-17 10:37:36 -0400

    Put QPROGRAM stuff in a _typing.py module

---
+++ mitiq/_typing.py
@@ -0,0 +1,25 @@
+"""
+This file is used to optionally import what program types should be allowed
+by mitiq based on what packages are installed in the environment.
+"""
+
+from typing import Union
+
+SUPPORTED_PROGRAM_TYPES = {
+    "cirq": "Circuit",
+    "qiskit": "QuantumCircuit"
+}
+
+AVAILABLE_PROGRAM_TYPES = {}
+
+for (module, program_type) in SUPPORTED_PROGRAM_TYPES.items():
+    try:
+        exec(f"from {module} import {program_type}")
+        AVAILABLE_PROGRAM_TYPES.update({module: program_type})
+    except ImportError:
+        pass
+
+QPROGRAM = Union[
+    tuple(f"{package}.{circuit}"
+     for package, circuit in AVAILABLE_PROGRAM_TYPES.items())
+]
