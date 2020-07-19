d1f1e83ba27c8b06b96ff541d95964582daea0ec
Peter Karalekas <peter@karalekas.com>
2020-07-19 12:17:32 -0400

    Export QPROGRAM as part of top-level API

--- mitiq/__init__.py
+++ mitiq/__init__.py
@@ -5,5 +5,6 @@ Mitiq can be directly imported.
 """
 
 from mitiq._about import about
+from mitiq._typing import QPROGRAM
 from mitiq._version import __version__
 from mitiq.zne import execute_with_zne, mitigate_executor
