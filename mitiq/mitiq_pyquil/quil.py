06cbfbc23e5364ef3c602eb05f56635ea8c661ff
Peter Karalekas <peter@karalekas.com>
2020-07-24 14:41:08 -0400

    First attempt at Quil program to Cirq circuit conversion

---
+++ mitiq/mitiq_pyquil/quil.py
@@ -0,0 +1,147 @@
+import warnings
+from functools import singledispatch
+from typing import Callable, Mapping, Union
+
+import numpy as np
+from pyquil.parser import parse
+from pyquil.quilbase import (Declare,
+                             Gate as PyQuilGate,
+                             Measurement as PyQuilMeasurement,
+                             Pragma,
+                             Reset,
+                             ResetQubit)
+
+from cirq import Circuit, LineQubit
+from cirq.ops import (CCNOT, CNOT, CSWAP, CZ, CZPowGate, Gate, H, I, ISWAP,
+                      ISwapPowGate, MeasurementGate, S, SWAP, T, X, Y, Z,
+                      ZPowGate, rx, ry, rz)
+
+
+class UnsupportedQuilGate(Exception):
+    pass
+
+
+def cphase(param: float) -> CZPowGate:
+    """
+    PyQuil's CPHASE and Cirq's CZPowGate are the same up to a factor of pi.
+    """
+    return CZPowGate(exponent=param / np.pi)
+
+
+def phase(param: float) -> ZPowGate:
+    """
+    PyQuil's PHASE and Cirq's ZPowGate are the same up to a factor of pi.
+    """
+    return ZPowGate(exponent=param / np.pi)
+
+
+def xy(param: float) -> ISwapPowGate:
+    """
+    PyQuil's XY and Cirq's ISwapPowGate are the same up to a factor of pi.
+    """
+    return ISwapPowGate(exponent=param / np.pi)
+
+
+# parameterized gates map to functions that produce Gate constructors
+SUPPORTED_GATES: Mapping[str, Union[Gate, Callable[..., Gate]]] = {
+    "CCNOT": CCNOT,
+    "CNOT": CNOT,
+    "CSWAP": CSWAP,
+    "CPHASE": cphase,
+    "CZ": CZ,
+    "PHASE": phase,
+    "H": H,
+    "I": I,
+    "ISWAP": ISWAP,
+    "RX": rx,
+    "RY": ry,
+    "RZ": rz,
+    "S": S,
+    "SWAP": SWAP,
+    "T": T,
+    "X": X,
+    "Y": Y,
+    "Z": Z,
+    "XY": xy,
+}
+
+
+@singledispatch
+def op_from_inst(inst: object) -> None:
+    """
+    Generic function for converting pyQuil instructions to Cirq operations.
+    """
+    raise TypeError(f"Quil instruction {inst} of type {type(inst)} not currently supported in Cirq.")
+
+
+@op_from_inst.register
+def _(inst: PyQuilGate) -> Gate:
+    """
+    Convert pyQuil gates to Cirq operations.
+    """
+    quil_gate_name = inst.name
+    quil_gate_params = inst.params
+    line_qubits = list(LineQubit(q.index) for q in inst.qubits)
+
+    if quil_gate_name in SUPPORTED_GATES:
+        cirq_gate_fn = SUPPORTED_GATES[quil_gate_name]
+        if quil_gate_params:
+            return cirq_gate_fn(*quil_gate_params)(*line_qubits)
+        return cirq_gate_fn(*line_qubits)
+    raise UnsupportedQuilGate(f"Quil gate {quil_gate_name} not supported in Cirq.")
+
+
+@op_from_inst.register
+def _(inst: PyQuilMeasurement) -> MeasurementGate:
+    """
+    Convert pyQuil MEASURE operations to Cirq MeasurementGate objects.
+    """
+    line_qubit = LineQubit(inst.qubit.index)
+    quil_memory_reference = inst.classical_reg.out()
+    return MeasurementGate(1, key=quil_memory_reference)(line_qubit)
+
+
+@op_from_inst.register
+def _(inst: Declare) -> Circuit:
+    """
+    Pass when encountering a DECLARE.
+    """
+    return Circuit()
+
+
+@op_from_inst.register
+def _(inst: Pragma) -> Circuit:
+    """
+    Pass and warn when encountering a PRAGMA.
+    """
+    warnings.warn(f"Ignoring unsupported operation {inst}")
+    return Circuit()
+
+
+@op_from_inst.register
+def _(inst: Reset) -> Circuit:
+    """
+    Pass and warn when encountering a RESET.
+    """
+    warnings.warn(f"Ignoring unsupported operation {inst}")
+    return Circuit()
+
+
+@op_from_inst.register
+def _(inst: ResetQubit) -> Circuit:
+    """
+    Pass and warn when encountering a RESET q.
+    """
+    warnings.warn(f"Ignoring unsupported operation {inst}")
+    return Circuit()
+
+
+def circuit_from_quil(quil: str) -> Circuit:
+    """
+    Convert a Quil program to a Cirq Circuit.
+    """
+    circuit = Circuit()
+    instructions = parse(quil)
+    for inst in instructions:
+        circuit += op_from_inst(inst)
+    return circuit
---
+++ mitiq/mitiq_pyquil/tests/test_quil.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+from cirq import Circuit, LineQubit
+from cirq.ops import (CCNOT, CNOT, CSWAP, CZ, H, I, ISWAP,
+                      MeasurementGate, S, SWAP, T, X, Y, Z,
+                      rx, ry, rz)
+
+from mitiq.mitiq_pyquil.quil import circuit_from_quil, cphase, xy
+
+
+QUIL_PROGRAM = """
+DECLARE ro BIT[3]
+I 0
+I 1
+I 2
+X 0
+Y 1
+Z 2
+H 0
+S 1
+T 2
+PHASE(pi/8) 0
+PHASE(pi/8) 1
+PHASE(pi/8) 2
+RX(pi/2) 0
+RY(pi/2) 1
+RZ(pi/2) 2
+CZ 0 1
+CNOT 1 2
+CPHASE(pi/2) 0 1
+SWAP 1 2
+ISWAP 0 1
+XY(pi/2) 1 2
+CCNOT 0 1 2
+CSWAP 0 1 2
+MEASURE 0 ro[0]
+MEASURE 1 ro[1]
+MEASURE 2 ro[2]
+"""
+
+
+def test_circuit_from_quil():
+    q0, q1, q2 = LineQubit.range(3)
+    cirq_circuit = Circuit([I(q0),
+                            I(q1),
+                            I(q2),
+                            X(q0),
+                            Y(q1),
+                            Z(q2),
+                            H(q0),
+                            S(q1),
+                            T(q2),
+                            Z(q0)**(1/8),
+                            Z(q1)**(1/8),
+                            Z(q2)**(1/8),
+                            rx(np.pi/2)(q0),
+                            ry(np.pi/2)(q1),
+                            rz(np.pi/2)(q2),
+                            CZ(q0, q1),
+                            CNOT(q1, q2),
+                            cphase(np.pi/2)(q0, q1),
+                            SWAP(q1, q2),
+                            ISWAP(q0, q1),
+                            xy(np.pi/2)(q1, q2),
+                            CCNOT(q0, q1, q2),
+                            CSWAP(q0, q1, q2),
+                            MeasurementGate(1, key="ro[0]")(q0),
+                            MeasurementGate(1, key="ro[1]")(q1),
+                            MeasurementGate(1, key="ro[2]")(q2)])
+
+    quil_circuit = circuit_from_quil(QUIL_PROGRAM)
+    assert cirq_circuit == quil_circuit
