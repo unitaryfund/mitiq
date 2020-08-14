7c0f7b238a64d2252b0327dcefe5ca4abc792189
Nathan Shammah <nathan.shammah@gmail.com>
2020-08-14 12:35:00 +0200

    comment import of modules as aliases

--- mitiq/__init__.py
+++ mitiq/__init__.py
@@ -1,4 +1,4 @@
-# Licensed under ...
+# Licensed under GNI GPL v.3.0
 """
 This is the top level module from which functions and classes of
 Mitiq can be directly imported.
@@ -8,3 +8,7 @@ from mitiq._about import about
 from mitiq._typing import QPROGRAM
 from mitiq._version import __version__
 from mitiq.zne.zne import execute_with_zne, mitigate_executor
+
+#import mitiq.zne.inference as mitiq.inference
+#import mitiq.zne.scaling as mitiq.scaling
+#import mitiq.zne.zne as mitiq.zne
