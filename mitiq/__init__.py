0fec917642d76fd36dc4900a91e3b97af6152cc2
Nathan Shammah <nathan.shammah@gmail.com>
2020-08-14 18:04:55 +0200

    remove comments in init

--- mitiq/__init__.py
+++ mitiq/__init__.py
@@ -8,7 +8,3 @@ from mitiq._about import about
 from mitiq._typing import QPROGRAM
 from mitiq._version import __version__
 from mitiq.zne.zne import execute_with_zne, mitigate_executor
-
-#import mitiq.zne.inference as mitiq.inference
-#import mitiq.zne.scaling as mitiq.scaling
-#import mitiq.zne.zne as mitiq.zne
--- mitiq/zne/__init__.py
+++ mitiq/zne/__init__.py
@@ -3,7 +3,3 @@
 This is the mitiq.zne subpackage from which functions and classes of
 Mitiq can be directly imported.
 """
-
-#import mitiq.zne.inference as mitiq.inference
-#import mitiq.zne.scaling as mitiq.scaling
-#import mitiq.zne.zne as mitiq.zne
