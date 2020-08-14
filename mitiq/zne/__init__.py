4920ae84645068d7ca91fa7872a77898d979d1e0
Nathan Shammah <nathan.shammah@gmail.com>
2020-08-14 19:15:38 +0200

    Update mitiq/zne/__init__.py
    Co-authored-by: Ryan LaRose <rlarose@umich.edu>

--- mitiq/zne/__init__.py
+++ mitiq/zne/__init__.py
@@ -3,3 +3,4 @@
 This is the mitiq.zne subpackage from which functions and classes of
 Mitiq can be directly imported.
 """
+from mitiq.zne.zne import execute_with_zne, mitigate_executor, zne_decorator
