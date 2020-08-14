6d7572181392b90707bfd9097d5670abdcf4fc66
Ryan LaRose <rlarose@umich.edu>
2020-08-14 13:15:59 -0400

    Adds license.

--- mitiq/benchmarks/maxcut.py
+++ mitiq/benchmarks/maxcut.py
@@ -1,7 +1,20 @@
-# /benchmarks/maxcut.py
-"""
-This module contains methods for benchmarking mitiq error extrapolation against
-a standard QAOA for MAXCUT.
+# Copyright (C) 2020 Unitary Fund
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""This module contains methods for benchmarking mitiq error extrapolation
+against a standard QAOA for MAXCUT.
 """
 from typing import List, Tuple, Callable
 import numpy as np
--- mitiq/zne/tests/test_inference.py
+++ mitiq/zne/tests/test_inference.py
@@ -1,6 +1,20 @@
-"""
-Testing of zero-noise inference and extrapolation methods
-(factories) with classically generated data.
+# Copyright (C) 2020 Unitary Fund
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Testing of zero-noise inference and extrapolation methods (factories) with
+classically generated data.
 """
 from copy import copy
 from typing import Callable
