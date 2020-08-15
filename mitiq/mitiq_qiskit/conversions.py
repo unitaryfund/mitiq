f14caebe328d13aee1c14d87b2a13eff66d48ed2
Ryan LaRose <rlarose@umich.edu>
2020-08-15 18:25:30 -0400

    Adds license. (#304)
    * Adds license.
    
    * Fix tests.

--- mitiq/mitiq_qiskit/conversions.py
+++ mitiq/mitiq_qiskit/conversions.py
@@ -1,7 +1,21 @@
-"""Functions to convert between Mitiq's internal circuit representation
-and Qiskit's circuit representation.
-"""
+# Copyright (C) 2020 Unitary Fund
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+"""Functions to convert between Mitiq's internal circuit representation and
+Qiskit's circuit representation.
+"""
 import cirq
 from cirq.contrib.qasm_import import circuit_from_qasm
 from qiskit import QuantumCircuit
