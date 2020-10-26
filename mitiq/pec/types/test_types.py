15b73b5dc6825af2022ebde60ce14ac12f8463a0
Ryan LaRose <rlarose@umich.edu>
2020-10-26 15:12:42 -0400

    Add NoisyOperation.

---
+++ mitiq/pec/types/test_types.py
@@ -0,0 +1,249 @@
+# Copyright (C) 2020 Unitary Fund
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import numpy as np
+import pytest
+
+import cirq
+
+from mitiq.utils import _equal
+from mitiq.pec.types import NoisyOperation
+
+
+def test_init_with_gate():
+    ideal_gate = cirq.Z
+    real = np.array([[0.94, 0.05], [0.13, -0.98]])
+    noisy_op = NoisyOperation(ideal_gate, real)
+    assert isinstance(noisy_op.ideal_circuit, cirq.Circuit)
+    assert _equal(
+        noisy_op.ideal_circuit,
+        cirq.Circuit(ideal_gate.on(cirq.LineQubit(0))),
+        require_qubit_equality=False,
+    )
+    assert noisy_op.qubits == (cirq.LineQubit(0),)
+    assert np.allclose(noisy_op.ideal_matrix, cirq.unitary(cirq.Z))
+    assert np.allclose(noisy_op.real_matrix, real)
+    assert noisy_op.real_matrix is not real
+
+
+@pytest.mark.parametrize(
+    "qubit",
+    (cirq.LineQubit(0), cirq.GridQubit(1, 2), cirq.NamedQubit("Qubit")),
+)
+def test_init_with_operation(qubit):
+    ideal_op = cirq.H.on(qubit)
+    real = np.array([[0.51, 0.49], [0.46, -0.45]]) / np.sqrt(2)
+    noisy_op = NoisyOperation(ideal_op, real)
+
+    assert isinstance(noisy_op.ideal_circuit, cirq.Circuit)
+    assert _equal(
+        noisy_op.ideal_circuit,
+        cirq.Circuit(ideal_op),
+        require_qubit_equality=True,
+    )
+    assert noisy_op.qubits == (qubit,)
+    assert np.allclose(noisy_op.ideal_matrix, cirq.unitary(ideal_op))
+    assert np.allclose(noisy_op.real_matrix, real)
+    assert noisy_op.real_matrix is not real
+
+
+def test_init_with_op_tree():
+    qreg = cirq.LineQubit.range(2)
+    ideal_ops = [cirq.H.on(qreg[0]), cirq.CNOT.on(*qreg)]
+    real = np.empty(shape=(4, 4))
+    noisy_op = NoisyOperation(ideal_ops, real)
+
+    assert isinstance(noisy_op.ideal_circuit, cirq.Circuit)
+    assert _equal(
+        noisy_op.ideal_circuit,
+        cirq.Circuit(ideal_ops),
+        require_qubit_equality=True,
+    )
+    assert set(noisy_op.qubits) == set(qreg)
+    assert np.allclose(
+        noisy_op.ideal_matrix, cirq.unitary(cirq.Circuit(ideal_ops))
+    )
+    assert np.allclose(noisy_op.real_matrix, real)
+    assert noisy_op.real_matrix is not real
+
+
+def test_init_with_circuit():
+    qreg = cirq.LineQubit.range(2)
+    circ = cirq.Circuit(cirq.H.on(qreg[0]), cirq.CNOT.on(*qreg))
+    real = np.empty(shape=(4, 4))
+    noisy_op = NoisyOperation(circ, real)
+
+    assert isinstance(noisy_op.ideal_circuit, cirq.Circuit)
+    assert _equal(noisy_op.ideal_circuit, circ, require_qubit_equality=True)
+    assert set(noisy_op.qubits) == set(qreg)
+    assert np.allclose(noisy_op.ideal_matrix, cirq.unitary(circ))
+    assert np.allclose(noisy_op.real_matrix, real)
+    assert noisy_op.real_matrix is not real
+
+
+def test_init_with_bad_types():
+    ideal_ops = [cirq.H, cirq.CNOT]
+    real = np.empty(shape=(4, 4))
+    with pytest.raises(ValueError, match="must be cirq.CIRCUIT_LIKE"):
+        NoisyOperation(ideal_ops, real)
+
+
+def test_init_dimenension_mismatch_error():
+    ideal = cirq.H
+    real = np.empty(shape=(3, 3))
+    with pytest.raises(ValueError, match="has shape"):
+        NoisyOperation(ideal, real)
+
+
+def test_add_simple():
+    ideal = cirq.Circuit([cirq.X.on(cirq.NamedQubit("Q"))])
+    real = np.array([[0.01, 0.99], [0.98, 0.02]])
+
+    noisy_op1 = NoisyOperation(ideal, real)
+    noisy_op2 = NoisyOperation(ideal, real)
+
+    noisy_op = noisy_op1 + noisy_op2
+
+    correct = cirq.Circuit([cirq.X.on(cirq.NamedQubit("Q"))] * 2)
+
+    assert _equal(noisy_op.ideal_circuit, correct, require_qubit_equality=True)
+    assert np.allclose(noisy_op.ideal_matrix, np.identity(2))
+    assert np.allclose(noisy_op.real_matrix, real @ real)
+
+
+@pytest.mark.parametrize(
+    "qreg",
+    (
+        cirq.LineQubit.range(5),
+        cirq.GridQubit.square(5),
+        [cirq.NamedQubit(str(i)) for i in range(5)],
+    ),
+)
+def test_on_each_single_qubit(qreg):
+    real = np.array([[0.01, 0.99], [0.98, 0.02]])
+    noisy_ops = NoisyOperation.on_each(cirq.X, real, qubits=qreg)
+
+    assert len(noisy_ops) == len(qreg)
+
+    for i, op in enumerate(noisy_ops):
+        assert np.allclose(op.real_matrix, real)
+        assert op.num_qubits == 1
+        assert list(op.ideal_circuit.all_qubits())[0] == qreg[i]
+
+
+@pytest.mark.parametrize(
+    "qubits",
+    (
+        [cirq.LineQubit.range(2), cirq.LineQubit.range(2, 4)],
+        [cirq.LineQubit.range(5, 7), cirq.LineQubit.range(10, 12)],
+        [cirq.GridQubit.rect(1, 2), cirq.GridQubit.rect(2, 1)],
+    ),
+)
+def test_on_each_multiple_qubits(qubits):
+    real_cnot = np.array(
+        [
+            [0.99, 0, 0.01, 0.01],
+            [0.01, 0.97, 0, 0.02],
+            [0.01, 0, 0.03, 0.98],
+            [0.02, 0.01, 0.95, 0],
+        ]
+    )
+    noisy_ops = NoisyOperation.on_each(cirq.CNOT, real_cnot, qubits=qubits)
+    assert len(noisy_ops) == 2
+
+    for i, op in enumerate(noisy_ops):
+        assert np.allclose(op.real_matrix, real_cnot)
+        assert op.num_qubits == 2
+        assert set(op.qubits) == set(qubits[i])
+
+
+def test_on_each_multiple_qubits_bad_qubits_shape():
+    real_cnot = np.array(
+        [
+            [0.99, 0, 0.01, 0.01],
+            [0.01, 0.97, 0, 0.02],
+            [0.01, 0, 0.03, 0.98],
+            [0.02, 0.01, 0.95, 0],
+        ]
+    )
+    qubits = [cirq.LineQubit.range(3)]
+    with pytest.raises(
+        ValueError, match="Number of qubits in each register should be"
+    ):
+        NoisyOperation.on_each(cirq.CNOT, real_cnot, qubits=qubits)
+
+
+@pytest.mark.parametrize(
+    "qubit", (cirq.NamedQubit("New qubit"), cirq.GridQubit(2, 3))
+)
+def test_transform_qubits_single_qubit(qubit):
+    real = np.empty(shape=(2, 2))
+    gate = cirq.H
+    noisy_op = NoisyOperation(gate, real)
+
+    assert set(noisy_op.qubits) != {qubit}
+    noisy_op.transform_qubits(qubit)
+    assert set(noisy_op.qubits) == {qubit}
+
+
+@pytest.mark.parametrize(
+    "qubits", (cirq.LineQubit.range(4, 6), cirq.GridQubit.rect(1, 2))
+)
+def test_transform_qubits_multiple_qubits(qubits):
+    real = np.empty(shape=(4, 4))
+    qreg = [cirq.NamedQubit("Dummy 1"), cirq.NamedQubit("Dummy 2")]
+    ideal = cirq.Circuit(cirq.ops.H.on(qreg[0]), cirq.ops.CNOT.on(*qreg))
+    noisy_op = NoisyOperation(ideal, real)
+
+    assert set(noisy_op.qubits) != set(qubits)
+    assert np.allclose(noisy_op.real_matrix, real)
+
+    noisy_op.transform_qubits(qubits)
+    assert set(noisy_op.qubits) == set(qubits)
+    assert np.allclose(noisy_op.real_matrix, real)
+
+
+def test_with_qubits():
+    real = np.empty(shape=(4, 4))
+    qreg = [cirq.NamedQubit("Dummy 1"), cirq.NamedQubit("Dummy 2")]
+    ideal = cirq.Circuit(cirq.ops.H.on(qreg[0]), cirq.ops.CNOT.on(*qreg))
+    noisy_op = NoisyOperation(ideal, real)
+
+    assert set(noisy_op.qubits) == set(qreg)
+    assert np.allclose(noisy_op.real_matrix, real)
+
+    qubits = cirq.LineQubit.range(2)
+    new_noisy_op = noisy_op.with_qubits(qubits)
+    assert set(new_noisy_op.qubits) == set(qubits)
+    assert np.allclose(new_noisy_op.real_matrix, real)
+
+
+def test_extend_to_single_qubit():
+    qbit, qreg = cirq.LineQubit(0), cirq.LineQubit.range(1, 10)
+    ideal = cirq.Z.on(qbit)
+    real = np.array([[0.94, 0.05], [0.13, -0.98]])
+    noisy_op_on_one_qubit = NoisyOperation(ideal, real)
+
+    noisy_ops_on_all_qubits = noisy_op_on_one_qubit.extend_to(qreg)
+
+    assert isinstance(noisy_ops_on_all_qubits, list)
+    assert len(noisy_ops_on_all_qubits) == 10
+
+    for op in noisy_ops_on_all_qubits:
+        assert _equal(
+            op.ideal_circuit, cirq.Circuit(ideal), require_qubit_equality=False
+        )
+        assert np.allclose(op.ideal_matrix, cirq.unitary(ideal))
+        assert np.allclose(op.real_matrix, real)
---
+++ mitiq/pec/types/types.py
@@ -0,0 +1,231 @@
+# Copyright (C) 2020 Unitary Fund
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Types used in probabilistic error cancellation."""
+from copy import deepcopy
+from typing import Any, List, Sequence, Tuple, Union
+
+import numpy as np
+
+import cirq
+
+
+class NoisyOperation:
+    """An operation (or sequence of operations) which a quantum computer can
+     actually implement.
+     """
+
+    def __init__(self, ideal: cirq.CIRCUIT_LIKE, real: np.ndarray) -> None:
+        """Initializes a noisy operation.
+
+        Args:
+            ideal: An ideal (noiseless) gate, operation, sequence of
+                operations, or circuit.
+            real: The actual matrix of `ideal`, not necessarily unitary,
+                performed when implemented on a quantum processor.
+
+        Raises:
+            ValueError:
+                * If `ideal` is not cirq.CIRCUIT_LIKE.
+                * If the shape of `real` does not match the expected shape
+                    from `ideal`.
+        """
+        if isinstance(ideal, cirq.Gate):
+            self._qubits = tuple(cirq.LineQubit.range(ideal.num_qubits()))
+            self._ideal = cirq.Circuit(ideal.on(*self._qubits))
+
+        elif isinstance(ideal, cirq.Operation):
+            self._qubits = ideal.qubits
+            self._ideal = cirq.Circuit(ideal)
+
+        elif isinstance(ideal, cirq.Circuit):
+            self._ideal = deepcopy(ideal)
+            self._qubits = tuple(self._ideal.all_qubits())
+
+        else:
+            try:
+                self._ideal = cirq.Circuit(ideal)
+                self._qubits = tuple(self._ideal.all_qubits())
+            except Exception:
+                raise ValueError(
+                    f"Arg `ideal` must be cirq.CIRCUIT_LIKE "
+                    f"but was {type(ideal)}."
+                )
+
+        self._num_qubits = len(self._qubits)
+        self._dimension = 2 ** self._num_qubits
+
+        if real.shape != (self._dimension, self._dimension):
+            raise ValueError(
+                f"Arg `real` has shape {real.shape} but `ideal` has shape"
+                f" {self._dimension, self._dimension}."
+            )
+
+        self._ideal = deepcopy(self._ideal)
+        self._real = deepcopy(real)
+
+    @staticmethod
+    def on_each(
+        ideal: cirq.CIRCUIT_LIKE,
+        real: np.ndarray,
+        qubits: Sequence[List[cirq.Qid]],
+    ) -> List["NoisyOperation"]:
+        """Returns a NoisyOperation(ideal, real) on each qubit in qubits.
+
+        Args:
+            ideal: An ideal (noiseless) gate, operation, sequence of
+                operations, or circuit.
+            real: The actual matrix of `ideal`, not necessarily unitary,
+                performed when implemented on a quantum processor.
+            qubits: The qubits to implement `ideal` on.
+
+        Raises:
+            TypeError:
+                * If `qubits` is not iterable.
+                * If `qubits` is not an iterable of cirq.Qid's or
+                  a sequence of lists of cirq.Qid's of the same length.
+        """
+        try:
+            qubits = list(iter(qubits))
+        except TypeError:
+            raise TypeError("Argument `qubits` must be iterable.")
+
+        try:
+            num_qubits_needed = cirq.num_qubits(ideal)
+        except TypeError:
+            raise ValueError(
+                "Could not deduce number of qubits needed by `ideal`."
+            )
+
+        if all(isinstance(q, cirq.Qid) for q in qubits):
+            qubits = [[q] for q in qubits]
+
+        if not all(len(qreg) == num_qubits_needed for qreg in qubits):
+            raise ValueError(
+                f"Number of qubits in each register should be"
+                f" {num_qubits_needed}."
+            )
+
+        noisy_ops = []  # type: List[NoisyOperation]
+        base_circuit = NoisyOperation(ideal, real).ideal_circuit
+        base_qubits = list(base_circuit.all_qubits())
+
+        for new_qubits in qubits:
+            try:
+                new_qubits = list(iter(new_qubits))
+            except TypeError:
+                new_qubits = list(new_qubits)
+
+            qubit_map = dict(zip(base_qubits, new_qubits))
+            new_circuit = base_circuit.transform_qubits(lambda q: qubit_map[q])
+
+            noisy_ops.append(NoisyOperation(new_circuit, real))
+
+        return noisy_ops
+
+    def extend_to(
+        self, qubits: Sequence[List[cirq.Qid]]
+    ) -> Sequence["NoisyOperation"]:
+        return [self] + NoisyOperation.on_each(self._ideal, self._real, qubits)
+
+    @staticmethod
+    def from_noise_model(
+        ideal: cirq.CIRCUIT_LIKE, noise_model
+    ) -> "NoisyOperation":
+        raise NotImplementedError
+
+    @property
+    def ideal_circuit(self) -> cirq.Circuit:
+        return self._ideal
+
+    @property
+    def qubits(self) -> Tuple[cirq.Qid]:
+        return self._qubits
+
+    @property
+    def num_qubits(self) -> int:
+        return len(self.qubits)
+
+    @property
+    def ideal_matrix(self) -> np.ndarray:
+        return cirq.unitary(self._ideal)
+
+    @property
+    def real_matrix(self) -> np.ndarray:
+        return deepcopy(self._real)
+
+    def transform_qubits(
+        self, qubits: Union[cirq.Qid, Sequence[cirq.Qid]]
+    ) -> None:
+        """Changes the qubit(s) that the noisy operation acts on.
+
+        Args:
+            qubits: Qubit(s) that the noisy operation will act on.
+
+        Raises:
+            ValueError: If the number of qubits does not match that
+                of the noisy operation.
+        """
+        try:
+            qubits = list(iter(qubits))
+        except TypeError:
+            qubits = [qubits]
+
+        if len(qubits) != self._num_qubits:
+            raise ValueError(
+                f"Expected {self._num_qubits} qubits but received"
+                f" {len(qubits)} qubits."
+            )
+
+        qubit_map = dict(zip(self._qubits, qubits))
+        self._ideal = self._ideal.transform_qubits(lambda q: qubit_map[q])
+        self._qubits = tuple(qubits)
+
+    def with_qubits(self, qubits: Sequence[cirq.Qid]) -> "NoisyOperation":
+        """Returns the noisy operation acting on the input qubits.
+
+        Args:
+            qubits: Qubits that the returned noisy operation will act on.
+
+        Raises:
+            ValueError: If the number of qubits does not match that
+                of the noisy operation.
+        """
+        copy = self.copy()
+        copy.transform_qubits(qubits)
+        return copy
+
+    def copy(self) -> "NoisyOperation":
+        """Returns a copy of the noisy operation."""
+        return NoisyOperation(self._ideal, self._real)
+
+    def __add__(self, other: Any) -> "NoisyOperation":
+        if not isinstance(other, NoisyOperation):
+            raise ValueError(
+                f"Arg `other` must be a NoisyOperation but was {type(other)}."
+            )
+
+        if self.qubits != other.qubits:
+            raise NotImplementedError
+
+        return NoisyOperation(
+            self._ideal + other._ideal, self._real @ other._real
+        )
+
+    def __repr__(self) -> str:
+        return self._ideal.__repr__() + "\n:\n" + self._real.__repr__()
+
+    def __str__(self) -> str:
+        return self._ideal.__str__()
