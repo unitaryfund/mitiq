78b216faaec4ce2c54d41132f0d255af901f476d
Ryan LaRose <rlarose@umich.edu>
2020-12-17 16:46:01 -0500

    Docstring.

--- mitiq/tests/test_collector.py
+++ mitiq/tests/test_collector.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-"""Unit tests for Collector and CircuitCollection."""
+"""Unit tests for Collector."""
 import pytest
 from typing import List
 
