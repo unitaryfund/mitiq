8ec5a88fa0e82e391f88ec947685b1956e9299d7
purva-thakre <purva.athakre@gmail.com>
2021-03-18 13:37:04 -0500

    add functions from unitary folding

--- mitiq/zne/scaling/identity_insertion.py
+++ mitiq/zne/scaling/identity_insertion.py
@@ -14,3 +14,44 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """Functions for identity scaling on supported circuits."""
+
+from copy import deepcopy
+from typing import (
+    Any,
+    Callable,
+    Collection,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Tuple,
+    Union,
+)
+
+import numpy as np
+from cirq import Circuit, InsertStrategy, inverse, ops, has_unitary
+
+# Import functions from different parts of return_mitiq
+
+# add comment about what QPORGRAM does
+from mitiq._typing import QPROGRAM
+
+# add comment about what converter does
+from mitiq.conversions import converter
+
+# import function defined for unitary folding
+from mitiq.zne.scaling.folding import (
+    _is_measurement,
+    _pop_measurements,
+    _append_measurements,
+    _cirq_gates_to_string_keys,
+)
+
+# Define empty class when identity scaling cannot be performed on  a gate
+class UnscalableGateError(Exception):
+    pass
+
+
+# Define empty class when identity scaling cannot be performed on  a circuit
+class UnscalableCircuitError(Exception):
+    pass
