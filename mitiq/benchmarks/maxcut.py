45f9077a61725ccecbda22e6a04c2c9e9509a690
TripleR47 <rajatavdutta.pro@gmail.com>
2021-05-28 04:12:25 +0530

    Update type hints in benchmarks/maxcut.py

--- mitiq/benchmarks/maxcut.py
+++ mitiq/benchmarks/maxcut.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """Methods for benchmarking zero-noise extrapolation on MaxCut-QAOA."""
-from typing import Callable, Iterable, List, Optional, Tuple
+from typing import Callable, Iterable, List, Optional, Tuple, Union, cast
 
 import numpy as np
 from scipy.optimize import minimize
@@ -37,7 +37,7 @@ SIMULATOR = DensityMatrixSimulator()
 
 
 def _make_noisy_backend(
-    noise: float, obs: np.ndarray
+    noise: float, obs: Union[np.ndarray, float]
 ) -> Callable[[Circuit], float]:
     """Returns a (noisy) execute function for Cirq circuits.
 
@@ -47,7 +47,7 @@ def _make_noisy_backend(
     """
 
     def noisy_backend(circ: Circuit) -> float:
-        return noisy_simulation(circ, noise, obs)
+        return noisy_simulation(circ, noise, cast(np.ndarray, obs))
 
     return noisy_backend
 
@@ -58,7 +58,8 @@ def make_maxcut(
     scale_noise: Optional[Callable[[Circuit, float], Circuit]] = None,
     factory: Optional[Factory] = None,
 ) -> Tuple[
-    Callable[[np.ndarray], float], Callable[[np.ndarray], Circuit], np.ndarray
+    Callable[[np.ndarray], Optional[float]],
+    Callable[[np.ndarray], Circuit], float
 ]:
     """Makes an executor that evaluates the QAOA ansatz at a given beta
     and gamma parameters.
@@ -113,7 +114,7 @@ def make_maxcut(
     noisy_backend = _make_noisy_backend(noise, cost_mat)
 
     # must have this function signature to work with scipy minimize
-    def qaoa_cost(params: np.ndarray) -> float:
+    def qaoa_cost(params: np.ndarray) -> Optional[float]:
         qaoa_prog = qaoa_ansatz(params)
         if scale_noise is None and factory is None:
             return noisy_backend(qaoa_prog)
@@ -137,7 +138,7 @@ def run_maxcut(
     factory: Optional[Factory] = None,
     optimizer: str = "Nelder-Mead",
     verbose: bool = False,
-) -> Tuple[float, np.ndarray, List[float]]:
+) -> Tuple[float, np.ndarray, List[Optional[float]]]:
     """Optimizes MaxCut cost function on the given graph using QAOA.
 
     Args:
