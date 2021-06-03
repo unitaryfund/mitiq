65f2d61670b86f83bf086888078105e6a7cb2176
Andrea Mari <46054446+andreamari@users.noreply.github.com>
2021-06-03 14:50:34 +0200

    date 2021
    Co-authored-by: Ryan LaRose <rlarose@umich.edu>

--- mitiq/pec/representations/damping.py
+++ mitiq/pec/representations/damping.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 Unitary Fund
+# Copyright (C) 2021 Unitary Fund
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
