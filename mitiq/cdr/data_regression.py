cccbd4961c05cc65d0004c0c134073bce616c72a
L-P-B <32333736+L-P-B@users.noreply.github.com>
2021-06-07 22:43:52 +0200

    CDR part two - Clifford data regression functions to fit training data (#677)
    * first draft of data regression code written
    
    * changed Q defn in calulcate_observable function
    
    * fixed typos
    
    * added cdr_execution file and a test_cdr_execution where an example of how it would be used is included
    
    * moved circuit execution and regression sections to execute_with_CDR function, added more formal testing.
    
    * updated tests
    
    * code style fixed
    
    * Simplify training set construction + execution.
    
    * merged ryans changes and acted on andreas comments
    
    * capitalized some words
    
    * Remove scale_noise.
    
    * Housekeeping.
    
    * Factor out execution from regression.
    
    * Execute code.
    
    * Add _testing.py to de-dupe.
    
    * Fewer lines of code.
    
    * Simplify.
    
    * Docstrings.
    
    * Test calculate observable.
    
    * De-dupe.
    
    * Clearer arg name.
    
    * Improvements.
    
    * Move linear_fit_no_intercept to data_regression. Add test for dict_to_probs.
    
    * One-liner.
    
    * Remove one-liner helper function.
    
    * De-dupe.
    
    * Simplify.
    
    * Remove extraneous code.
    
    * Simplify execute, change behavior of normalize_measurements to not include a bunch of zeros.
    
    * Add tests.
    
    * Function name.
    
    * Format.
    
    * Flake.
    
    * Test directory.
    
    * Default values.
    
    * Rename to cdr.py to match zne.py and pec.py.
    
    * Finish refactor.
    
    * changed variable name ansatz and added rest of Andreas comments
    
    * renamed ansatz and changed order of outputs
    
    * fixed conflicts and added andreas suggestions.
    
    * fixed style issue
    
    Co-authored-by: Ryan LaRose <rlarose@umich.edu>

---
+++ mitiq/cdr/data_regression.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2021 Unitary Fund
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""The data regression portion of Clifford data regression."""
+from typing import Sequence
+
+import numpy as np
+
+
+def linear_fit_function(x_data: np.ndarray, params: Sequence[float]) -> float:
+    """Returns y(x) = a_1 x_1 + ... + a_n x_n + b.
+
+    Args:
+        x_data: The independent variables x_1, ..., x_n. In CDR, these are
+            nominally the noisy expectation values to perform regression on.
+        params: Parameters a_1, ..., a_n, b of the linear fit. Note the b
+            parameter is the intercept of the fit.
+    """
+    return sum(a * x for a, x in zip(params, x_data)) + params[-1]
+
+
+def linear_fit_function_no_intercept(
+    x_data: np.ndarray, params: Sequence[float]
+) -> float:
+    """Returns y(x) = a_1 x_1 + ... + a_n x_n.
+
+    Args:
+        x_data: The independent variables x_1, ..., x_n. In CDR, these are
+            nominally the noisy expectation values to perform regression on.
+        params: Parameters a_1, ..., a_n of the linear fit.
+    """
+    return sum(a * x for a, x in zip(params, x_data))
