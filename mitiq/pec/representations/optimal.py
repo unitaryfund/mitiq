97b684d2b882988f6732d67aa7de1389f159f403
andreamari <andreamari84@gmail.com>
2021-06-22 11:27:26 +0200

    try using M

--- mitiq/pec/representations/optimal.py
+++ mitiq/pec/representations/optimal.py
@@ -40,12 +40,10 @@ def minimize_one_norm(
     the following representation of the input ``ideal_matrix`` holds:
 
     .. math::
-        :nowrap:
+        M = x_0 A_0 + x_1 A_1 + ...,
 
-        \text{ideal_matrix} = x_0 A_0 + x_1 A_1 + ...,
-
-    where :math:`\{A_j\}` are the basis matrices, i.e., the elements of
-    the input ``basis_matrices``.
+    where :math:`\{A_j\}` are the basis matrices (i.e., the elements of the
+    input ``basis_matrices``), while :math:`M` is the input ``ideal_matrix``.
 
     This function can be used to compute the optimal representation
     of an ideal superoperator (or Choi state) as a linear
