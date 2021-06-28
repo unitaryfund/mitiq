c1a14919a276ea3972542e807fefc576e8954817
Ryan LaRose <rlarose@umich.edu>
2021-06-28 16:30:15 -0400

    README encoding.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     extras_require={"development": set(dev_requirements), },
     packages=find_packages(),
     include_package_data=True,
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Unitary Fund",
     classifiers=[
