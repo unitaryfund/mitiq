a9850f91fce211dfe8900bc17c18786850323642
Ryan LaRose <rlarose@umich.edu>
2021-07-30 20:41:04 -0400

    CP.

--- mitiq/interface/mitiq_pennylane/conversions.py
+++ mitiq/interface/mitiq_pennylane/conversions.py
@@ -16,14 +16,42 @@
 """Functions to convert between Mitiq's internal circuit representation and
 Pennylane's circuit representation.
 """
+
+from typing import Optional, List
+
 from cirq import Circuit
-from cirq.contrib.qasm_import import circuit_from_qasm
-from pennylane_qiskit import AerDevice, load_qasm
+from mitiq.interface.mitiq_qiskit import from_qasm as cirq_from_qasm, to_qasm
+from pennylane.wires import Wires
+from pennylane.measure import MeasurementProcess
+from pennylane.tape import QuantumTape
+
+from pennylane import from_qasm as pennylane_from_qasm
 
 
-def from_pennylane(device: AerDevice) -> Circuit:
-    return circuit_from_qasm(device._circuit.qasm())
+def from_pennylane(circuit: QuantumTape) -> Circuit:
+    """Returns a Cirq circuit equivalent to the input QuantumTape.
+
+    Args:
+        circuit: Pennylane QuantumTape to convert to a Cirq circuit.
+    """
+    return cirq_from_qasm(circuit.to_openqasm(rotations=False))
 
 
-def to_pennylane(circuit: Circuit):
-    return load_qasm(circuit.to_qasm())
+def to_pennylane(
+    circuit: Circuit,
+    target_wires: Optional[Wires] = None,
+) -> QuantumTape:
+    """Returns a QuantumTape equivalent to the input Cirq circuit.
+
+    Args:
+        circuit: Cirq circuit to convert to a Pennylane QuantumTape.
+    """
+    if target_wires is None:
+        target_wires = Wires(range(len(circuit.all_qubits())))
+
+    qfunc = pennylane_from_qasm(to_qasm(circuit))
+
+    with QuantumTape() as tape:
+        qfunc(wires=target_wires)
+
+    return tape
