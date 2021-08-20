70e6ea0380f384f6e43c8062d99cac8fb4eaa72e
Danny Samuel <dannywinstonsamuel@gmail.com>
2021-08-19 21:25:03 -0400

    Fixed formatting errors
    Fixed formatting errors

--- mitiq/benchmarks/mirror_circuits.py
+++ mitiq/benchmarks/mirror_circuits.py
@@ -14,9 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import typing
-from typing import Optional
-import numpy as np
+from typing import Optional, List
 from numpy import random
 import networkx as nx
 import cirq
@@ -26,21 +24,30 @@ single_q_cliffords = _single_qubit_cliffords()
 cliffords = single_q_cliffords.c1_in_xy
 paulis = [cirq.X, cirq.Y, cirq.Z, cirq.I]
 
+
 def random_paulis(nqubits: int) -> cirq.Circuit:
     """Returns a circuit with a random pauli gate applied to each qubit.
 
     Args:
         nqubits: The number of qubits in the circuit.
     """
-    return cirq.Circuit(paulis[random.randint(4)](cirq.LineQubit(x)) for x in range(nqubits))
+    return cirq.Circuit(
+        paulis[random.randint(4)](cirq.LineQubit(x)) for x in range(nqubits)
+    )
 
-def edge_grab(two_qubit_gate_prob: float, connectivity_graph_: nx.Graph) -> nx.Graph:
-    """Returns a set of edges for which two qubit gates are to be applied given a two qubit gate density, xi,
+
+def edge_grab(
+    two_qubit_gate_prob: float, connectivity_graph_: nx.Graph
+) -> nx.Graph:
+    """Returns a set of edges for which two qubit gates
+    are to be applied given a two qubit gate density, xi,
     and the connectivity graph that must be satisfied.
 
     Args:
-        two_qubit_gate_prob: Probability of an edge being chosen from the set of candidate edges.
-        connectivity_graph_: The connectivity graph for the backend on which the circuit will be run.
+        two_qubit_gate_prob: Probability of an edge being chosen
+            from the set of candidate edges.
+            connectivity_graph_: The connectivity graph for the backend
+            on which the circuit will be run.
     """
     connectivity_graph = connectivity_graph_.copy()
     candidate_edges = nx.Graph()
@@ -51,7 +58,7 @@ def edge_grab(two_qubit_gate_prob: float, connectivity_graph_: nx.Graph) -> nx.G
         curr_edge = edges[num]
         candidate_edges.add_edge(*curr_edge)
         connectivity_graph.remove_nodes_from(curr_edge)
-        
+
     prob = two_qubit_gate_prob
     final_edges = nx.Graph()
     final_edges.add_nodes_from(connectivity_graph_)
@@ -60,15 +67,20 @@ def edge_grab(two_qubit_gate_prob: float, connectivity_graph_: nx.Graph) -> nx.G
             final_edges.add_edge(*edge)
     return final_edges
 
+
 def random_cliffords(edges: nx.Graph) -> cirq.Circuit:
-    """Returns a circuit with a two-qubit Clifford gate applied to each edge in edges, and a random single-qubit
+    """Returns a circuit with a two-qubit Clifford gate applied
+    to each edge in edges, and a random single-qubit
     Clifford gate applied to every other qubit.
 
     Args:
-        edges: A graph with the edges for which the two-qubit Clifford gate is to be applied.
+        edges: A graph with the edges for which the
+            two-qubit Clifford gate is to be applied.
     """
     gates = []
-    gates.append(cirq.CNOT(cirq.LineQubit(a), cirq.LineQubit(b)) for a,b in edges.edges)
+    gates.append(
+        cirq.CNOT(cirq.LineQubit(a), cirq.LineQubit(b)) for a, b in edges.edges
+    )
     circuit = cirq.Circuit(gates)
     qubits = nx.Graph()
     qubits.add_nodes_from(nx.isolates(edges))
@@ -77,38 +89,51 @@ def random_cliffords(edges: nx.Graph) -> cirq.Circuit:
 
 
 def random_single_cliffords(qubits: nx.Graph) -> cirq.Circuit:
-    """Returns a circuit with a random single-qubit Clifford gate applied on each given qubit.
-    
+    """Returns a circuit with a random single-qubit Clifford gate
+    applied on each given qubit.
+
     Args:
-        qubits: A graph with each node representing a qubit for which a random single-qubit Clifford gate is to be applied.
+        qubits: A graph with each node representing a qubit for
+            which a random single-qubit Clifford gate is to be applied.
 
     """
-    gates = []
+    gates: List[cirq.Operation] = []
     for qubit in qubits.nodes:
         num = random.randint(len(cliffords))
-        gates.extend(clifford.on(cirq.LineQubit(qubit)) for clifford in cliffords[num])
+        gates.extend(
+            clifford.on(cirq.LineQubit(qubit)) for clifford in cliffords[num]
+        )
     return cirq.Circuit(gates)
 
-def generate_mirror_circuit(nlayers: int, two_qubit_gate_prob: float, connectivity_graph: nx.Graph) -> cirq.Circuit:
+
+def generate_mirror_circuit(
+    nlayers: int,
+    two_qubit_gate_prob: float,
+    connectivity_graph: nx.Graph,
+    seed: Optional[int] = None,
+) -> cirq.Circuit:
     """Returns a randomized mirror circuit.
-    
+
     Args:
         nlayers: The number of random Clifford layers to be generated.
-        two_qubit_gate_prob: Probability of a two-qubit gate being applied.
-        connectivity_graph: The connectivity graph of the backend on which the mirror circuit will be run. This is
-            used to make sure 2-qubit gates are only run on connected qubits.
-      """
+            two_qubit_gate_prob: Probability of a two-qubit gate being applied.
+            connectivity_graph: The connectivity graph of the backend
+            on which the mirror circuit will be run. This is used
+            to make sure 2-qubit gates are only run on connected qubits.
+    """
     if not 0 <= two_qubit_gate_prob <= 1:
         raise ValueError("two_qubit_gate_prob must be between 0 and 1")
-        
+
+    random.seed(seed)
+
     nqubits = connectivity_graph.number_of_nodes()
     single_qubit_cliffords = random_single_cliffords(connectivity_graph)
-    
+
     forward_circuit = cirq.Circuit()
-    
+
     quasi_inversion_circuit = cirq.Circuit()
     quasi_inverse_gates = []
-    
+
     for _ in range(nlayers):
         forward_circuit.append(random_paulis(nqubits))
 
@@ -118,17 +143,20 @@ def generate_mirror_circuit(nlayers: int, two_qubit_gate_prob: float, connectivi
         edge_graph.add_edges_from(selected_edges.edges)
         circ = random_cliffords(edge_graph)
         forward_circuit.append(circ)
-        
+
         quasi_inverse_gates.append(random_paulis(nqubits))
         quasi_inverse_gates.append(cirq.inverse(circ))
-    
-    
-    quasi_inversion_circuit.append(gate for gate in reversed(quasi_inverse_gates))
-        
+
+    quasi_inversion_circuit.append(
+        gate for gate in reversed(quasi_inverse_gates)
+    )
+
     rand_paulis = cirq.Circuit()
     rand_paulis.append(random_paulis(nqubits))
-    
-    circuit = single_qubit_cliffords + forward_circuit + rand_paulis + quasi_inversion_circuit + cirq.inverse(single_qubit_cliffords)
-    for x in range (nqubits):
+
+    circuit = single_qubit_cliffords + forward_circuit
+    +rand_paulis + quasi_inversion_circuit
+    +cirq.inverse(single_qubit_cliffords)
+    for x in range(nqubits):
         circuit.append(cirq.measure(cirq.LineQubit(x)))
     return circuit
--- mitiq/benchmarks/tests/test_mirror_circuits.py
+++ mitiq/benchmarks/tests/test_mirror_circuits.py
@@ -17,7 +17,6 @@
 from mitiq.benchmarks import mirror_circuits
 import networkx as nx
 import cirq
-import numpy as np
 import pytest
 from cirq.experiments.qubit_characterizations import _single_qubit_cliffords
 
@@ -32,6 +31,7 @@ cliffords.append(cirq.CNOT)
 all_gates = paulis + cliffords
 all_gates.append(cirq.measure)
 
+
 @pytest.mark.parametrize("n", (0, 5))
 def test_random_paulis(n):
     circuit = mirror_circuits.random_paulis(nqubits=n)
@@ -39,27 +39,38 @@ def test_random_paulis(n):
     assert len(circuit.all_qubits()) == n
     assert len(list(circuit.all_operations())) == n
     assert set(op.gate for op in circuit.all_operations()).issubset(paulis)
-    
+
+
 def test_edge_grab():
-    nqubits = [9,10,11]
+    nqubits = [9, 10, 11]
     graphs = []
     for qubits in nqubits:
         graphs.append(nx.complete_graph(qubits))
-    removed_edges = [[(1,2),(4,7),(3,5)],[(2,4),(5,6),(7,8)],[(3,4),(5,6),(8,9)]]
-                     
+    removed_edges = [
+        [(1, 2), (4, 7), (3, 5)],
+        [(2, 4), (5, 6), (7, 8)],
+        [(3, 4), (5, 6), (8, 9)],
+    ]
+
     for x in range(3):
         graph = graphs[x]
         for edge in removed_edges[x]:
             graph.remove_edge(*edge)
-        selected_edges = mirror_circuits.edge_grab(0.3,graph)
+        selected_edges = mirror_circuits.edge_grab(0.3, graph)
         assert set(selected_edges.edges()).issubset(graph.edges())
         assert selected_edges.number_of_nodes() == nqubits[x]
         for node in selected_edges.nodes:
-            assert len(list(selected_edges.neighbors(node))) == 1 or len(list(selected_edges.neighbors(node))) == 0
-            
+            neighbors = len(list(selected_edges.neighbors(node)))
+            assert neighbors == 1 or neighbors == 0
+
+
 def test_random_cliffords():
-    nqubits = [9,10,11]
-    edges = [[(1,2),(4,7),(3,5)],[(2,4),(5,6),(7,8)],[(3,4),(5,6),(8,9)]]
+    nqubits = [9, 10, 11]
+    edges = [
+        [(1, 2), (4, 7), (3, 5)],
+        [(2, 4), (5, 6), (7, 8)],
+        [(3, 4), (5, 6), (8, 9)],
+    ]
     for x in range(3):
         graph = nx.Graph()
         graph.add_nodes_from(range(nqubits[x]))
@@ -68,29 +79,46 @@ def test_random_cliffords():
         assert isinstance(circuit, cirq.Circuit)
         assert len(circuit.all_qubits()) == nqubits[x]
         two_q_gates = set()
-        for a,b in edges[x]:
-            two_q_gates.add(cirq.CNOT(cirq.LineQubit(a),cirq.LineQubit(b)))
+        for a, b in edges[x]:
+            two_q_gates.add(cirq.CNOT(cirq.LineQubit(a), cirq.LineQubit(b)))
         assert two_q_gates.issubset(circuit.all_operations())
-        assert set(op.gate for op in circuit.all_operations()).issubset(cliffords)
+        assert set(op.gate for op in circuit.all_operations()).issubset(
+            cliffords
+        )
+
 
 def test_random_single_cliffords():
-    qubits = [[0,1,2,3,4],[1,3,5,7],[1,2,4,6,7,8]]
+    qubits = [[0, 1, 2, 3, 4], [1, 3, 5, 7], [1, 2, 4, 6, 7, 8]]
     for x in range(3):
         graph = nx.Graph()
         graph.add_nodes_from(qubits[x])
         circuit = mirror_circuits.random_single_cliffords(graph)
         assert isinstance(circuit, cirq.Circuit)
-        assert set(circuit.all_qubits()).issubset([cirq.LineQubit(qubit) for qubit in qubits[x]])
-        assert set(op.gate for op in circuit.all_operations()).issubset(single_cliffords)
+        assert set(circuit.all_qubits()).issubset
+        ([cirq.LineQubit(qubit) for qubit in qubits[x]])
+        assert set(op.gate for op in circuit.all_operations()).issubset(
+            single_cliffords
+        )
+
 
 def test_mirror_circuit():
-    parameters = [(16,0.3,nx.complete_graph(3)), (20,0.4, nx.complete_graph(4)), (24,0.5, nx.complete_graph(5))]
-    for depth,xi,connectivity_graph in parameters:
+    parameters = [
+        (16, 0.3, nx.complete_graph(3)),
+        (20, 0.4, nx.complete_graph(4)),
+        (24, 0.5, nx.complete_graph(5)),
+    ]
+    for depth, xi, connectivity_graph in parameters:
         n = connectivity_graph.number_of_nodes()
-        circ = mirror_circuits.generate_mirror_circuit(depth,xi,connectivity_graph)
+        circ = mirror_circuits.generate_mirror_circuit(
+            depth, xi, connectivity_graph
+        )
         assert isinstance(circ, cirq.Circuit)
         assert len(circ.all_qubits()) == n
-        result = cirq.Simulator().run(circ, repetitions=1_000).multi_measurement_histogram(keys=circ.all_measurement_keys())
+        result = (
+            cirq.Simulator()
+            .run(circ, repetitions=1_000)
+            .multi_measurement_histogram(keys=circ.all_measurement_keys())
+        )
         assert len(result.keys()) == 1
         meas_circ = cirq.Circuit()
         for qubit in range(n):
@@ -98,7 +126,9 @@ def test_mirror_circuit():
         for moment in circ:
             temp_circ = cirq.Circuit()
             temp_circ.append(moment)
-            if(temp_circ != meas_circ):
-                assert set(op.gate for op in temp_circ.all_operations()).issubset(all_gates)
+            if temp_circ != meas_circ:
+                assert set(
+                    op.gate for op in temp_circ.all_operations()
+                ).issubset(all_gates)
         assert circ.has_measurements()
         assert circ.are_all_measurements_terminal()
