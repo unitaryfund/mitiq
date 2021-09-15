49203f005ccd5872487493be2c5ecf59234dcb41
andreamari <andreamari84@gmail.com>
2021-09-15 09:53:03 +0200

    fix style

--- mitiq/pec/types/types.py
+++ mitiq/pec/types/types.py
@@ -514,10 +514,8 @@ class OperationRepresentation:
         return noisy_op, int(self.sign_of(noisy_op)), self.coeff_of(noisy_op)
 
     def __str__(self) -> str:
-
         lhs = str(self._ideal) + " = "
         rhs = ""
-  
         for c, circ in zip(self.coeffs, self.noisy_operations):
             c_str = _format_coefficient(".3f", c)
             # Handle special cases as in cirq.value.linear_dict._format_term()
@@ -528,18 +526,17 @@ class OperationRepresentation:
                     c_str = "+" + c_str
                 if len(self._ideal.all_qubits()) == 1:
                     # Print single-qubit circuits horizontally
-                    rhs += f'{c_str}*{circ!s}'
+                    rhs += f"{c_str}*{circ!s}"
                 else:
                     # Print multi-qubit circuits vertically
                     rhs += "\n" + f"{c_str}*\n{circ!s}"
         # Handle special cases as in cirq.value.linear_dict._format_terms()
         if not rhs:
             rhs = f"{0:.3f}"
-        if rhs[0] == '+':
+        if rhs[0] == "+":
             rhs = rhs[1:]
-        if rhs[0:2] == '\n+':
+        if rhs[0:2] == "\n+":
             rhs = "\n" + rhs[2:]
-
         return lhs + rhs
 
     def __eq__(self, other: Any) -> bool:
