8fae2fe2e4982f1e35133fce1a071786abd6c7a1
Ryan LaRose <rlarose@umich.edu>
2021-09-27 16:42:10 -0400

    Update mitiq/zne/inference.py
    Co-authored-by: Andrea Mari <46054446+andreamari@users.noreply.github.com>

--- mitiq/zne/inference.py
+++ mitiq/zne/inference.py
@@ -311,7 +311,7 @@ class Factory(ABC):
             qp: Quantum circuit to scale noise in.
             executor: Function which inputs a (list of) quantum circuits and
                 outputs a (list of) expectation values.
-            observable: Observable to compute expectation value of. If None,
+            observable: Observable to compute the expectation value of. If None,
                 the `executor` must return an expectation value. Otherwise,
                 the `QuantumResult` returned by `executor` is used to compute
                 the expectation of the observable.
