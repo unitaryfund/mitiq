c9933e6f70a471cbd6c7d2be3e29dcd97fea070a
Ryan LaRose <rlarose@umich.edu>
2022-01-06 14:54:22 -0500

    Add asv benchmarking framework (#1047)
    * Add `mitiq.raw`.
    
    * Import raw.
    
    * Add asv zne benchmarks.
    
    * Comment.
    
    * Flake8.
    
    * Ignore benchmarks in coverage.
    
    * Coverage.
    
    * Add PEC benchmark.
    
    * Review comments.
    
    * Min parameters.
    
    * Run in CI.
    
    * Run `asv machine --yes`.
    
    * This should cause CI to fail.
    
    * Check if CI fails.
    
    * Debug.
    
    * Hacky fail check.
    
    * Display result message.
    
    * Remove fail check.
    
    * Print result message more clearly.
    
    * Remove old benchmarks in favor of new.
    
    * Axe stray ZNE test not in ZNE tests; docstrings for benchmark circuit modules.
    
    * Update PEC test.
    
    * Update apidoc.
    
    * Add mitiq.raw to apidoc.

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 Unitary Fund
+# Copyright (C) 2022 Unitary Fund
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -12,28 +12,3 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-"""Unit tests for MaxCut benchmark."""
-import numpy as np
-
-from mitiq.benchmarks.maxcut import run_maxcut
-
-
-def test_barbell():
-    graph = [(0, 1), (1, 0)]
-    opt_energy = -2.0
-
-    energy, *_ = run_maxcut(graph, x0=[0.0, 0.3])
-    assert np.isclose(energy, opt_energy)
-
-
-def test_triangle():
-    graph = [(0, 1), (1, 2), (2, 0)]
-    opt_energy = -2.0
-
-    x0 = [0.0, 0.5]
-    energy, *_ = run_maxcut(graph, x0)
-    assert np.isclose(energy, opt_energy)
-
-    energy_with_noise, *_ = run_maxcut(graph, x0, noise=0.4)
-    assert energy_with_noise > opt_energy
