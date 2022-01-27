d9b05d4a3e892d767b24a0563a8d647a4d912e42
vtomole <vtomole2@gmail.com>
2022-01-27 12:11:40 -0600

    Format

--- mitiq/interface/mitiq_pennylane/tests/test_conversions_pennylane.py
+++ mitiq/interface/mitiq_pennylane/tests/test_conversions_pennylane.py
@@ -98,8 +98,7 @@ def test_non_consecutive_wires_error():
     with qml.tape.QuantumTape() as tape:
         qml.CNOT(wires=[0, 2])
     with pytest.raises(
-        UnsupportedQuantumTapeError,
-        match="contiguously pack",
+        UnsupportedQuantumTapeError, match="contiguously pack",
     ):
         from_pennylane(tape)
 
