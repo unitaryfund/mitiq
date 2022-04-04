07e070f6e8e87c22670712334592805cb8a3682b
Aaron Robertson <aaronrobertson.dev@gmail.com>
2022-04-04 07:51:43 -0700

    include random rules and rule constructor

--- mitiq/ddd/rules/rules.py
+++ mitiq/ddd/rules/rules.py
@@ -17,7 +17,59 @@
 slack window.
 """
 
-from cirq import Circuit, X, Y, I, LineQubit  # , Z
+from cirq import Circuit, X, Y, I, LineQubit, Gate, Z
+from typing import List, Optional
+
+from itertools import cycle
+import numpy as np
+
+
+def construct_rule(
+    slack_length: int, num_repetitions: int, spacing: int, gates: List[Gate]
+) -> Circuit:
+    """Returns a digital dynamical decoupling sequence, based on inputs.
+
+    Args:
+        slack_length: Length of idle window to fill.
+        num_repetitions: How many repetitions of the ddd rule to apply within
+            the slack window. Default to 1.
+        spacing: How many identity spacing gates to apply between dynamical
+            decoupling gates, as a positive-value int. Defaults to evenly
+            spaced.
+        gates: A list of Cirq gates to build the rule. E.g. [X, X] is the xx
+            sequence, [X, Y, X, Y] is the xyxy sequence
+    Returns:
+        A digital dynamical decoupling sequence, as a cirq circuit
+    """
+    default_length = len(gates)
+    num_decoupling_gates = default_length * num_repetitions
+    slack_difference = slack_length - num_decoupling_gates
+    if spacing < 0:
+        spacing = slack_difference // (num_decoupling_gates + 1)
+    slack_remainder = slack_length - (
+        spacing * (num_decoupling_gates + 1) + num_decoupling_gates
+    )
+    if slack_remainder < 0:
+        raise ValueError(
+            "Rule too long for given slack window by {} moments.".format(
+                slack_remainder * -1
+            )
+        )
+    q = LineQubit(0)
+    slack_gates = [I(q) for _ in range(spacing)]
+    ddd_circuit = Circuit(
+        slack_gates,
+        [
+            (
+                gate.on(q),
+                slack_gates,
+            )
+            for (_, gate) in zip(range(num_decoupling_gates), cycle(gates))
+        ],
+    )
+    for i in range(slack_remainder):
+        ddd_circuit.append(I(q)) if i % 2 else ddd_circuit.insert(0, I(q))
+    return ddd_circuit
 
 
 def xx(
@@ -35,35 +87,13 @@ def xx(
     Returns:
         An XX digital dynamical decoupling sequence, as a cirq circuit
     """
-    default_length = 2
-    num_decoupling_gates = default_length * num_repetitions
-    slack_difference = slack_length - num_decoupling_gates
-    if spacing < 0:
-        spacing = slack_difference // (num_decoupling_gates + 1)
-    slack_remainder = slack_length - (
-        spacing * (num_decoupling_gates + 1) + num_decoupling_gates
+    xx_rule = construct_rule(
+        slack_length=slack_length,
+        num_repetitions=num_repetitions,
+        spacing=spacing,
+        gates=[X, X],
     )
-    if slack_remainder < 0:
-        raise ValueError(
-            "Rule too long for given slack window by {} moments.".format(
-                slack_remainder * -1
-            )
-        )
-    q = LineQubit(0)
-    slack_gates = [I(q) for _ in range(spacing)]
-    ddd_circuit = Circuit(
-        slack_gates,
-        [
-            (
-                X(q),
-                slack_gates,
-            )
-            for _ in range(num_decoupling_gates)
-        ],
-    )
-    for i in range(slack_remainder):
-        ddd_circuit.append(I(q)) if i % 2 else ddd_circuit.insert(0, I(q))
-    return ddd_circuit
+    return xx_rule
 
 
 def xyxy(
@@ -81,36 +111,13 @@ def xyxy(
     Returns:
         An XYXY digital dynamical decoupling sequence, as a cirq circuit
     """
-    default_length = 4
-    num_decoupling_gates = default_length * num_repetitions
-    slack_difference = slack_length - num_decoupling_gates
-    if spacing < 0:
-        spacing = slack_difference // (num_decoupling_gates + 1)
-    slack_remainder = slack_length - (
-        spacing * (num_decoupling_gates + 1) + num_decoupling_gates
+    xyxy_rule = construct_rule(
+        slack_length=slack_length,
+        num_repetitions=num_repetitions,
+        spacing=spacing,
+        gates=[X, Y, X, Y],
     )
-    if slack_remainder < 0:
-        raise ValueError(
-            "Rule too long for given slack window by {} moments.".format(
-                slack_remainder * -1
-            )
-        )
-    print(slack_remainder)
-    q = LineQubit(0)
-    slack_gates = [I(q) for _ in range(spacing)]
-    ddd_circuit = Circuit(
-        slack_gates,
-        [
-            (
-                Y(q) if i % 2 else X(q),
-                slack_gates,
-            )
-            for i in range(num_decoupling_gates)
-        ],
-    )
-    for i in range(slack_remainder):
-        ddd_circuit.append(I(q)) if i % 2 else ddd_circuit.insert(0, I(q))
-    return ddd_circuit
+    return xyxy_rule
 
 
 def yy(
@@ -128,32 +135,65 @@ def yy(
     Returns:
         An YY digital dynamical decoupling sequence, as a cirq circuit
     """
-    default_length = 2
-    num_decoupling_gates = default_length * num_repetitions
-    slack_difference = slack_length - num_decoupling_gates
-    if spacing < 0:
-        spacing = slack_difference // (num_decoupling_gates + 1)
-    slack_remainder = slack_length - (
-        spacing * (num_decoupling_gates + 1) + num_decoupling_gates
+    yy_rule = construct_rule(
+        slack_length=slack_length,
+        num_repetitions=num_repetitions,
+        spacing=spacing,
+        gates=[Y, Y],
     )
-    if slack_remainder < 0:
-        raise ValueError(
-            "Rule too long for given slack window by {} moments.".format(
-                slack_remainder * -1
-            )
-        )
-    q = LineQubit(0)
-    slack_gates = [I(q) for _ in range(spacing)]
-    ddd_circuit = Circuit(
-        slack_gates,
-        [
-            (
-                Y(q),
-                slack_gates,
-            )
-            for _ in range(num_decoupling_gates)
-        ],
+    return yy_rule
+
+
+def random(
+    slack_length: int,
+    sequence_length: int,
+    num_repetitions: int = 1,
+    spacing: int = -1,
+) -> Circuit:
+    """Returns a random digital dynamical decoupling sequence, based on inputs.
+
+    Args:
+        slack_length: Length of idle window to fill.
+        sequence_length: Length of the random sequence.
+        num_repetitions: How many repetitions of the ddd rule to apply within
+            the slack window. Default to 1.
+        spacing: How many identity spacing gates to apply between dynamical
+            decoupling gates, as a positive-value int. Defaults to evenly
+            spaced.
+    Returns:
+        A random digital dynamical decoupling sequence, as a cirq circuit
+    """
+    random_gates = _random_pauli_gates(
+        slack_length=slack_length, sequence_length=sequence_length
     )
-    for i in range(slack_remainder):
-        ddd_circuit.append(I(q)) if i % 2 else ddd_circuit.insert(0, I(q))
-    return ddd_circuit
+    random_rule = construct_rule(
+        slack_length=slack_length,
+        num_repetitions=num_repetitions,
+        spacing=spacing,
+        gates=random_gates,
+    )
+    return random_rule
+
+
+def _random_pauli_gates(
+    slack_length: int, sequence_length: int, seed: Optional[int] = None
+) -> List[Gate]:
+    """Helper function that returns a list of random Pauli gates based on the slack
+        length.
+
+    Args:
+        slack_length: Length of idle window to fill.
+        sequence_length: Length of the random sequence.
+        seed: Optional random seed
+    Returns:
+        A random digital dynamical decoupling sequence, as a cirq circuit
+    """
+    if seed is None:
+        random_state = np.random
+    else:
+        random_state = np.random.RandomState(seed)
+    random_paulis = random_state.choice(
+        [X, Y, Z], size=slack_length, replace=True
+    )
+    random_sequence = random_paulis[:sequence_length]
+    return random_sequence
