bae67c9167871e81229d8b6a40c394efd0189c33
Aaron Robertson <aaronrobertson.dev@gmail.com>
2022-04-28 07:57:49 -0700

    tests mostly working as expected

--- mitiq/ddd/tests/test_insertion.py
+++ mitiq/ddd/tests/test_insertion.py
@@ -15,22 +15,85 @@
 
 """Unit tests for DDD slack windows and DDD insertion tools."""
 
+from re import I
 import numpy as np
 import cirq
 from mitiq.ddd.insertion import (
     _get_circuit_mask,
     get_slack_matrix_from_circuit_mask,
+    insert_ddd_sequences
 )
 import pytest
+import qiskit
+from mitiq.ddd.rules import xx, xyxy
 
 circuit_cirq_one = cirq.Circuit(
     cirq.SWAP(q, q + 1) for q in cirq.LineQubit.range(7)
 )
+for i in (0,7):
+    circuit_cirq_one.append(cirq.ops.I.on(i),
+        cirq.ops.X.on(i),
+        cirq.ops.Y.on(i),
+        cirq.ops.X.on(i),
+        cirq.ops.Y.on(i),
+        cirq.ops.I.on(i),
+    )
+for i in (1,6):
+    circuit_cirq_one.append(cirq.ops.I.on(i),
+        cirq.ops.X.on(i),
+        cirq.ops.Y.on(i),
+        cirq.ops.X.on(i),
+        cirq.ops.Y.on(i),
+    )
+for i in (2, 5):
+    circuit_cirq_one.append(cirq.ops.X.on(i),
+        cirq.ops.Y.on(i),
+        cirq.ops.X.on(i),
+        cirq.ops.Y.on(i),
+    )
 
 qreg_cirq = cirq.GridQubit.rect(10, 1)
 circuit_cirq_two = cirq.Circuit(
-    cirq.ops.H.on_each(*qreg_cirq), cirq.ops.H.on(qreg_cirq[1])
+    cirq.ops.H.on_each(*qreg_cirq), cirq.ops.H.on(qreg_cirq[1]),
 )
+
+circuit_cirq_three = cirq.Circuit(
+    cirq.ops.H.on_each(*qreg_cirq), 5*[cirq.ops.H.on(qreg_cirq[1])],
+)
+
+circuit_cirq_three_validated = cirq.Circuit(
+    cirq.ops.H.on_each(*qreg_cirq), 5*[cirq.ops.H.on(qreg_cirq[1])],
+    cirq.ops.I.on_each(qreg_cirq[0], *qreg_cirq[2:]),
+    cirq.ops.X.on_each(qreg_cirq[0], *qreg_cirq[2:]),
+    cirq.ops.Y.on_each(qreg_cirq[0], *qreg_cirq[2:]),
+    cirq.ops.X.on_each(qreg_cirq[0], *qreg_cirq[2:]),
+    cirq.ops.Y.on_each(qreg_cirq[0], *qreg_cirq[2:])
+)
+
+qreg = qiskit.QuantumRegister(4)
+circuit_qiskit_one = qiskit.QuantumCircuit(qreg)
+for q in qreg:
+    circuit_qiskit_one.x(q)
+    circuit_qiskit_one.x(3)
+circuit_qiskit_one.cx(0, 3)
+
+qiskit.QuantumRegister(4)
+circuit_qiskit_validate = qiskit.QuantumCircuit(qreg)
+for i in range(4):
+    circuit_qiskit_validate.x(i)
+    circuit_qiskit_validate.x(3)
+    if i != 3 and i != 0:
+        circuit_qiskit_validate.i(i)
+        circuit_qiskit_validate.x(i)
+        circuit_qiskit_validate.i(i)
+        circuit_qiskit_validate.x(i)
+        circuit_qiskit_validate.i(i)
+    elif i == 0:
+        circuit_qiskit_validate.i(i)
+        circuit_qiskit_validate.x(i)
+        circuit_qiskit_validate.x(i)
+        circuit_qiskit_validate.i(i)
+circuit_qiskit_validate.cx(0, 3)
 
 # Define test mask matrices
 test_mask_one = np.array(
@@ -163,3 +226,19 @@ def test_get_slack_matrix_from_circuit__bad_input_errors():
         get_slack_matrix_from_circuit_mask(np.array([[1, 0], [1, 1.7]]))
     with pytest.raises(ValueError, match="elements must be 0 or 1"):
         get_slack_matrix_from_circuit_mask(np.array([[2, 0], [0, 0]]))
+
+
+@pytest.mark.parametrize(
+    ("circuit", "result", "rule"),
+    [
+        (circuit_cirq_one, test_mask_one, xyxy),
+        (circuit_cirq_two, circuit_cirq_two, xx),
+        (circuit_cirq_three, circuit_cirq_three_validated, xyxy),
+        (circuit_qiskit_one, circuit_qiskit_validate, xx)
+    ],
+
+)
+def test_insert_sequences(circuit, result, rule):
+    circuit_with_sequences = insert_ddd_sequences(circuit, rule)
+    assert circuit_with_sequences == result
+
