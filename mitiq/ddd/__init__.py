3ce0e82fbd76c604bba0a4fdd1d251c715e7ee85
andreamari <andreamari84@gmail.com>
2022-04-29 16:09:30 +0200

    import insert_ddd_sequences in ddd.__init___

--- mitiq/ddd/__init__.py
+++ mitiq/ddd/__init__.py
@@ -17,4 +17,11 @@
 
 from mitiq.ddd import rules
 
+from mitiq.ddd import insertion
+
+from mitiq.ddd.insertion import (
+    get_slack_matrix_from_circuit_mask,
+    insert_ddd_sequences,
+)
+
 from mitiq.ddd.ddd import execute_with_ddd, mitigate_executor, ddd_decorator
