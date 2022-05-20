4e18f1dd63d9175c6fe7afc39c30b4c267d657ba
Nick Gardner <gardner.nicholas.d@gmail.com>
2022-05-20 14:47:29 -0700

    Add tests for random seed parameter

--- mitiq/benchmarks/tests/test_volume_circuits.py
+++ mitiq/benchmarks/tests/test_volume_circuits.py
@@ -17,7 +17,8 @@
 are tested here:
 cirq-core/cirq/contrib/quantum_volume/quantum_volume_test.py
 
-Tests below check that generate_volume_circuit() works for mitiq's interface.
+Tests below check that generate_volume_circuit() works as a wrapper and
+fits with mitiq's interface.
 """
 
 import pytest
@@ -28,15 +29,31 @@ import cirq
 from mitiq.benchmarks.volume_circuits import generate_volume_circuit 
 from mitiq._typing import SUPPORTED_PROGRAM_TYPES
 
-def test_generate_model_circuit():
+def test_generate_model_circuit_no_seed():
     """Test that random circuit of the right length is generated."""
     circuit, _ = generate_volume_circuit(3, 3)
     assert len(circuit) == 3
 
 
+def test_generate_model_circuit_with_seed():
+    """Test that a model circuit is determined by its seed."""
+    circuit_1, _ = generate_volume_circuit(
+        3, 3, seed=1
+    )
+    circuit_2, _ = generate_volume_circuit(
+        3, 3, seed=1
+    )
+    circuit_3, _ = generate_volume_circuit(
+        3, 3, seed=2
+    )
+
+    assert circuit_1 == circuit_2
+    assert circuit_2 != circuit_3
+
+
 @pytest.mark.parametrize("return_type", SUPPORTED_PROGRAM_TYPES.keys())
 def test_volume_conversion(return_type):
-    circuit, _ = generate_volume_circuit(3, 3, return_type)
+    circuit, _ = generate_volume_circuit(3, 3, return_type=return_type)
     assert return_type in circuit.__module__
 
 
