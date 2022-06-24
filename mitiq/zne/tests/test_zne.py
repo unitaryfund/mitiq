1e5aee58459089aea04b32bcd544d063ab1d5621
Misty Wahl <82074193+Misty-W@users.noreply.github.com>
2022-06-24 15:36:49 -0700

    Update test_zne.py

--- mitiq/zne/tests/test_zne.py
+++ mitiq/zne/tests/test_zne.py
@@ -304,6 +304,10 @@ def qiskit_decorated_executor(qp: QPROGRAM) -> float:
     return qiskit_executor(qp)
 
 
+def batched_qiskit_execute(circuits) -> List[float]:
+    return [qiskit_executor(circuit) for circuit in circuits]
+
+
 def test_qiskit_mitigate_executor():
     true_zne_value = 1.0
 
@@ -321,7 +325,11 @@ def test_qiskit_mitigate_executor():
     mitigated_executor = mitigate_executor(qiskit_executor)
     zne_value = mitigated_executor(circuit)
     assert abs(true_zne_value - zne_value) < abs(true_zne_value - base)
-
+    
+    batched_mitigated_executor = mitigate_executor(batched_qiskit_executor)
+    batched_zne_value = batched_mitigated_executor([circuit])
+    assert abs(true_zne_value - batched_zne_value) < abs(true_zne_value - base)
+    
 
 def test_qiskit_zne_decorator():
     true_zne_value = 1.0
