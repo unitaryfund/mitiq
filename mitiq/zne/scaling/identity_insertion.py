edbd54b5c206f0d7e27e72bbe65a57dcc47d1437
Purva Thakre <purva.athakre@gmail.com>
2022-08-17 15:10:03 -0500

    style, test whole factor

--- mitiq/zne/scaling/identity_insertion.py
+++ mitiq/zne/scaling/identity_insertion.py
@@ -12,14 +12,15 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""Functions for scaling supported circuits by inserting layers of identity gates."""
+"""Functions for scaling supported circuits by inserting layers of identity
+gates."""
 
 import numpy as np
+from typing import List
 from collections import Counter
-from cirq import Circuit, InsertStrategy, inverse, ops, has_unitary, Moment
+from cirq import Circuit, ops, Moment
 from mitiq.utils import (
     _append_measurements,
-    _is_measurement,
     _pop_measurements,
 )
 
@@ -27,9 +28,11 @@ from mitiq.utils import (
 class UnscalableCircuitError(Exception):
     pass
 
+
 # Helper functions for identity scaling
 def _check_scalable(input_circuit: Circuit) -> None:
-    """Raises an error if the input circuit cannot be scaled by inserting identity layers.
+    """Raises an error if the input circuit cannot be scaled by inserting
+    identity layers.
 
     Args:
         circuit: Checks whether this circuit can be scaled.
@@ -43,42 +46,51 @@ def _check_scalable(input_circuit: Circuit) -> None:
             "Circuit contains intermediate measurements and cannot be folded."
         )
 
-def _calculate_id_layers(input_circuit_depth: int, scale_factor: float) -> List[int]:
-    """Returns a list of integers that describes the number of identity layers to be
-    inserted after each layer of the input circuit. 
 
-    Args: 
-        input_circuit_depth : Number of moments in the input_circuit 
+def _calculate_id_layers(
+    input_circuit_depth: int, scale_factor: float
+) -> List[int]:
+    """Returns a list of integers that describes the number of identity layers
+    to be inserted after each layer of the input circuit.
+
+    Args:
+        input_circuit_depth : Number of moments in the input_circuit
         scale_factor : Intended noise scaling factor
-    
+
     Returns:
-        [num_uniform_layers, num_partial_layers] : A list of the number of uniform
-            identity layers to be inserted after each moment in the input_circuit and
-            a number of partial layers to be inserted after some random moments to be
-            able to achieve the intended scale factor.
+        [num_uniform_layers, num_partial_layers] : A list of the number of
+        uniform identity layers to be inserted after each moment in the
+        input_circuit and a number of partial layers to be inserted after
+        some random moments to be able to achieve the intended scale factor.
     """
     if not 1.0 <= scale_factor:
         raise ValueError(
-            f"Requires scale_factor >= 1 but scale_factor ={scale_factor}.")
-    
+            f"Requires scale_factor >= 1 but scale_factor ={scale_factor}."
+        )
+
     # find number of uniform layers
-    num_uniform_layers = int(scale_factor-1.0)
+    num_uniform_layers = int(scale_factor - 1.0)
     int_scale_factor = num_uniform_layers + 1
     if np.isclose(int_scale_factor, scale_factor):
         return [num_uniform_layers, 0]
     else:
-        # find partial layers by approximating closest to the desired scale_factor
-        num_partial_layers=int(input_circuit_depth*(scale_factor-1-num_uniform_layers))
+        # find partial layers by approximating closest to the desired
+        # scale_factor
+        num_partial_layers = int(
+            input_circuit_depth * (scale_factor - 1 - num_uniform_layers)
+        )
         return [num_uniform_layers, num_partial_layers]
 
+
 # identity insertion scaling function
-def insert_id_layers(input_circuit: Circuit, scale_factor: float)-> Circuit:
-    """Returns a scaled version of the input circuit by inserting layers of identities.
+def insert_id_layers(input_circuit: Circuit, scale_factor: float) -> Circuit:
+    """Returns a scaled version of the input circuit by inserting layers of
+    identities.
 
     Args:
         input_circuit : Cirq Circuit to be scaled
         scale_factor : Noise scaling factor as a float
-    
+
     Returns:
         scaled_circuit : Scaled quantum circuit via identity layer insertions
     """
@@ -96,29 +108,35 @@ def insert_id_layers(input_circuit: Circuit, scale_factor: float)-> Circuit:
     input_circuit_moment_list = []
     for i in range(input_circuit_depth):
         input_circuit_moment_list.append(input_circuit[i])
-    
+
     # find list of random moments for partial layers
     if num_partial_layers != 0:
-       random_moment_indices = np.random.randint(input_circuit_depth,size = num_partial_layers).tolist()
-       # figure out if the random list has any repeated indices
-       index_counter = Counter(random_moment_indices)
-    
+        random_moment_indices = np.random.randint(
+            input_circuit_depth, size=num_partial_layers
+        ).tolist()
+        # figure out if the random list has any repeated indices
+        index_counter = Counter(random_moment_indices)
+    else:
+        random_moment_indices = []
+        index_counter = dict()
+
     # create a layer of identity acting on every qubit in the circuit
     circuit_qubits = list(input_circuit.all_qubits())
-    id_layer = cirq.Moment(cirq.ops.I.on_each(circuit_qubits))
+    id_layer = Moment(ops.I.on_each(circuit_qubits))
 
     # create the scaled circuit
-    scaled_circuit = cirq.Circuit()
+    scaled_circuit = Circuit()
     for i in range(input_circuit_depth):
         scaled_circuit = scaled_circuit + input_circuit[i]
         # add partial layers if i is in random moment index list
         if i in random_moment_indices:
             num_partial_layers_random_moment = index_counter[i]
-            scaled_circuit = 
-                scaled_circuit + [id_layer]*num_partial_layers_random_moment
+            scaled_circuit = (
+                scaled_circuit + [id_layer] * num_partial_layers_random_moment
+            )
         # now insert uniform layers
-        scaled_circuit =  scaled_circuit + [id_layer]*num_uniform_layers
-    
-    # before returning scaled_circuit, terminal measurements need to be added 
+        scaled_circuit = scaled_circuit + [id_layer] * num_uniform_layers
+
+    # before returning scaled_circuit, terminal measurements need to be added
     _append_measurements(scaled_circuit, measurements)
     return scaled_circuit
--- mitiq/zne/scaling/tests/test_identity_insertion.py
+++ mitiq/zne/scaling/tests/test_identity_insertion.py
@@ -16,3 +16,35 @@
 """Unit tests for scaling noise by inserting identity layers."""
 import numpy as np
 import pytest
+from mitiq.zne.scaling.identity_insertion import (
+    UnscalableCircuitError,
+    _calculate_id_layers,
+    insert_id_layers,
+)
+from cirq import Circuit, ops, LineQubit
+from mitiq.utils import _equal
+
+
+@pytest.mark.parametrize("scale_factor", (1, 2, 3, 4, 5, 6))
+def test_id_layers_whole_scale_factor(scale_factor):
+    """Tests if n-1 identity layers are inserted uniformly when
+    the intended scale factor is n."""
+    qreg = LineQubit.range(3)
+    circ = Circuit(
+        [ops.H.on_each(*qreg)],
+        [ops.CNOT.on(qreg[0], qreg[1])],
+        [ops.X.on(qreg[2])],
+        [ops.TOFFOLI.on(*qreg)],
+    )
+    scaled = insert_id_layers(circ, scale_factor=scale_factor)
+    num_layers = scale_factor - 1
+    correct = Circuit(
+        [ops.H.on_each(*qreg)],
+        [ops.I.on_each(*qreg)] * num_layers,
+        [ops.CNOT.on(qreg[0], qreg[1])],
+        [ops.X.on(qreg[2])],
+        [ops.I.on_each(*qreg)] * num_layers,
+        [ops.TOFFOLI.on(*qreg)],
+        [ops.I.on_each(*qreg)] * num_layers,
+    )
+    assert _equal(scaled, correct)
