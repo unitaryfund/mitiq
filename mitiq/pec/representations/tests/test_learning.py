3a6957d662a3d8ff5fd2a0be045be763347f935c
Misty Wahl <82074193+Misty-W@users.noreply.github.com>
2022-09-23 11:30:20 -0700

    Learning function for biased noise (#1358)
    * Add learning function and test
    
    Add learning function and test back into `learning.py`
    
    * Update test_learning.py
    
    * Sync learning function with loss function
    
    * Update __init__.py
    
    * Update learning.py
    
    * Update test_learning.py
    
    * Update apidoc.rst
    
    * Make `biased_noise_loss_function` public
    
    Now that the loss function has been demonstrated with a frontend other than Cirq, i.e. Qiskit, we can make `biased_noise_loss_function` public
    
    * Use public `biased_noise_loss_function` everywhere
    
    * Add qiskit test, allow user to set `pec_kwargs`
    
    * Update test_learning.py
    
    Fix formatting
    
    * Add kwargs in top-level function, improve tests
    
    * Update learning.py
    
    Fix type errors
    
    * Apply suggestions from code review
    
    Co-authored-by: Andrea Mari <46054446+andreamari@users.noreply.github.com>
    
    * Update learning.py
    
    Fix the docstring in `learn_biased_noise_parameters`
    
    * Return optimization result as intended
    
    Return optimization result as intended
    Remove incompatible options for BFGS solver
    
    * Adding a new notebook for troubleshooting
    
    * Update new_test_learning.ipynb
    
    Strange things happening in the loss function plots
    
    * Merge branch '1440-make-suppression-of-no-representation-found-for-nnideal_operation-warnings-an-option' into learning-functions-for-biased-noise
    
    * Update new_test_learning.ipynb
    
    * Revert option for warning suppression
    
    * Remove changes to `pec.py`
    
    * Remove changes to `sampling.py`
    
    * Update sampling.py
    
    * Update loss function, add new notebook
    
    Fixed a bug in loss fn calculation (not the full fix)
    
    Added a new notebook, focused on troubleshooting PEC executions and representation generation within the loss fn
    
    * Found some settings that (kind of) work
    
    * Small offset on eta0, update notebook
    
    Add small offset on eta0 for eta = 0 case
    
    Update notebook with faster running combination, add some logic for learning R
    
    * Update test cases and notebook
    
    * Fixing minor bug in tests, adding notebook
    
    Adding another notebook, fixing a bug in tests (not the main cause of failure)
    
    * Update implementation to single param opt
    
    Update implementation to single parameter optimization
    
    Remove noise bias test cases
    
    Remove notebooks
    
    Temporarily remove Rx and Rz test cases
    
    * Update learning.py
    
    Fix type errors
    
    * Add tests for Rx and Rz ops
    
    Add new tests for Rx and Rz ops
    
    Adjust offset in CNOT test
    
    * Update test_learning.py
    
    * Faster executor
    
    Faster executor, one test for CNOT, Rx, and Rz
    
    Clean up `gitignore`
    
    * Update test_learning.py
    
    Remove separate rotation test
    
    * Using Nelder-Mead with fewer samples
    
    Using Nelder-Mead with fewer samples brought local execution time down to ~1 min.
    
    Also using a "minus" offset instead of a "plus" offset.
    
    Removed qiskit tests for now as they are still slow (probably due to mismatched noise model in executor)
    
    * Reduce PEC `num_samples` to 50
    
    * Correct test condition
    
    Apply corrected test condition
    Update learning function name
    Adjust samples and offset to run smoothly and efficiently:   `test_learn_depolarizing_noise_parameter` passed 10x in a row locally @ ~40s per run
    
    * Slightly slower (but safer) parameter choices
    
    Slightly slower (but safer) parameter choices - `num_samples` and `offset`
    Update docstring of `learn_depolarizing_noise_parameter`
    
    * Add `solver` argument, non-Cirq tests
    
    Add `solver` argument
    Add non-Cirq tests
    Improve docstrings
    
    * Address comments from community call
    
    * Update mitiq/pec/representations/learning.py
    
    Co-authored-by: Andrea Mari <andreamari84@gmail.com>
    
    Co-authored-by: Andrea Mari <46054446+andreamari@users.noreply.github.com>
    Co-authored-by: Andrea Mari <andreamari84@gmail.com>

--- mitiq/pec/representations/tests/test_learning.py
+++ mitiq/pec/representations/tests/test_learning.py
@@ -28,19 +28,28 @@ from cirq import (
     Circuit,
     ops,
     unitary,
+    InsertStrategy,
 )
+import qiskit
 from mitiq import Executor, Observable, PauliString
+from mitiq.interface.mitiq_qiskit import qiskit_utils
+from mitiq.interface.mitiq_qiskit.conversions import to_qiskit
 from mitiq.interface.mitiq_cirq import compute_density_matrix
 from mitiq.cdr import generate_training_circuits
 from mitiq.cdr._testing import random_x_z_cnot_circuit
-from mitiq.pec.representations.learning import _biased_noise_loss_function
+from mitiq.pec.representations.learning import (
+    biased_noise_loss_function,
+    learn_depolarizing_noise_parameter,
+)
 
+seed = 1
+rng = np.random.RandomState(seed)
 circuit = random_x_z_cnot_circuit(
-    LineQubit.range(2), n_moments=5, random_state=1
+    LineQubit.range(2), n_moments=5, random_state=rng
 )
 
 # Set number of samples used to calculate mitigated value in loss function
-pec_kwargs = {"num_samples": 100}
+pec_kwargs = {"num_samples": 50, "random_state": 1}
 
 observable = Observable(PauliString("XZ"), PauliString("YY"))
 
@@ -99,7 +108,7 @@ def test_biased_noise_loss_function(epsilon, eta, operations):
     noisy_values = np.array(
         [noisy_executor.evaluate(t, observable) for t in training_circuits]
     )
-    loss = _biased_noise_loss_function(
+    loss = biased_noise_loss_function(
         params=[epsilon, eta],
         operations_to_mitigate=operations,
         training_circuits=training_circuits,
@@ -125,7 +134,8 @@ def test_biased_noise_loss_compare_ideal(operations):
         return ideal_execute(noisy_circ)
 
     noisy_executor = Executor(noisy_execute)
-    loss = _biased_noise_loss_function(
+
+    loss = biased_noise_loss_function(
         params=[0, 0],
         operations_to_mitigate=operations,
         training_circuits=training_circuits,
@@ -135,3 +145,108 @@ def test_biased_noise_loss_compare_ideal(operations):
         observable=observable,
     )
     assert np.isclose(loss, 0)
+
+
+@pytest.mark.parametrize(
+    "operations",
+    [
+        [to_qiskit(Circuit(CNOT_ops[0][1]))],
+        [to_qiskit(Circuit(Rx_ops[0][1]))],
+        [to_qiskit(Circuit(Rz_ops[0][1]))],
+    ],
+)
+def test_biased_noise_loss_function_qiskit(operations):
+    """Test the learning function with initial noise strength and noise bias
+    with a small offset from the simulated noise model values"""
+    qiskit_circuit = to_qiskit(circuit)
+
+    qiskit_training_circuits = generate_training_circuits(
+        circuit=qiskit_circuit,
+        num_training_circuits=3,
+        fraction_non_clifford=0.2,
+        method_select="uniform",
+        method_replace="closest",
+        random_state=rng,
+    )
+    obs = Observable(PauliString("XY"), PauliString("ZZ"))
+
+    def ideal_execute_qiskit(circ: qiskit.QuantumCircuit) -> float:
+        return qiskit_utils.execute(circ, obs.matrix())
+
+    ideal_executor_qiskit = Executor(ideal_execute_qiskit)
+    ideal_values = np.array(
+        [ideal_executor_qiskit.evaluate(t) for t in qiskit_training_circuits]
+    )
+
+    epsilon = 0.1
+
+    def noisy_execute_qiskit(circ: qiskit.QuantumCircuit) -> float:
+        noise_model = qiskit_utils.initialized_depolarizing_noise(epsilon)
+        return qiskit_utils.execute_with_noise(circ, obs.matrix(), noise_model)
+
+    noisy_executor_qiskit = Executor(noisy_execute_qiskit)
+
+    noisy_values = np.array(
+        [noisy_executor_qiskit.evaluate(t) for t in qiskit_training_circuits]
+    )
+
+    loss = biased_noise_loss_function(
+        params=[epsilon, 0],
+        operations_to_mitigate=operations,
+        training_circuits=qiskit_training_circuits,
+        ideal_values=ideal_values,
+        noisy_executor=noisy_executor_qiskit,
+        pec_kwargs=pec_kwargs,
+    )
+
+    assert loss <= np.mean(
+        abs(noisy_values.reshape(-1, 1) - ideal_values.reshape(-1, 1)) ** 2
+    )
+
+
+@pytest.mark.parametrize("epsilon", [0.05, 0.1])
+@pytest.mark.parametrize("operations", [CNOT_ops[0]])
+# We assume the operation "op" appears just once in the circuit such
+# that it's enough to add a single noise channel after that operation.
+def test_learn_depolarizing_noise_parameter(epsilon, operations):
+    """Test the learning function with initial noise strength with a small
+    offset from the simulated noise model values"""
+
+    index = operations[0]
+    op = operations[1]
+    offset = 0.1
+    eta = 0
+
+    pec_kwargs_learning = {"num_samples": 300, "random_state": 1}
+
+    def noisy_execute(circ: Circuit) -> np.ndarray:
+        noisy_circ = circ.copy()
+        qubits = op.qubits
+        for q in qubits:
+            noisy_circ.insert(
+                index + 1,
+                biased_noise_channel(epsilon, eta)(q),
+                strategy=InsertStrategy.EARLIEST,
+            )
+        return ideal_execute(noisy_circ)
+
+    noisy_executor = Executor(noisy_execute)
+
+    epsilon0 = (1 - offset) * epsilon
+
+    operations_to_learn = [Circuit(operations[1])]
+
+    [success, epsilon_opt] = learn_depolarizing_noise_parameter(
+        operations_to_learn=operations_to_learn,
+        circuit=circuit,
+        ideal_executor=ideal_executor,
+        noisy_executor=noisy_executor,
+        pec_kwargs=pec_kwargs_learning,
+        num_training_circuits=5,
+        fraction_non_clifford=0.2,
+        training_random_state=np.random.RandomState(1),
+        epsilon0=epsilon0,
+        observable=observable,
+    )
+    assert success
+    assert abs(epsilon_opt - epsilon) < offset * epsilon
