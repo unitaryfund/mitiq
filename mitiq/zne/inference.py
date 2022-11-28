b535dcf261c7fd4aea434e9b79fefee13ef77b4f
Misty-W <82074193+Misty-W@users.noreply.github.com>
2022-11-28 12:12:23 -0500

    Update inference.py

--- mitiq/zne/inference.py
+++ mitiq/zne/inference.py
@@ -1363,7 +1363,15 @@ class PolyExpFactory(BatchedFactory):
 
         # Deduce "sign" parameter of the exponential ansatz
         linear_params, _ = mitiq_polyfit(scale_factors, exp_values, deg=1)
-        sign = np.sign(-linear_params[0])
+
+        if (
+            asymptote is not None
+            and np.sign(-linear_params[0] * (asymptote - linear_params[1]))
+            == 1
+        ):
+            sign = np.sign(linear_params[0])
+        else:
+            sign = np.sign(-linear_params[0])
 
         def _ansatz_unknown(x: float, *coeffs: float) -> float:
             """Ansatz of generic order with unknown asymptote."""
