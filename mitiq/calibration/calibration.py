64dbc48e5902805732ba7aac6fe83fbd0dd62faf
nate stemen <nate@unitary.fund>
2022-11-29 23:16:10 -0800

    add calibration skeleton/prototype
    hesitant to call it a prototype since it has such limited functionality

---
+++ mitiq/calibration/calibration.py
@@ -0,0 +1,98 @@
+# Copyright (C) 2021 Unitary Fund
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from mitiq import Executor
+from mitiq.calibration import Settings
+
+
+class Calibration:
+    """A calibration object which keeps track of, and aids in EM parameter tuning."""
+
+    def __init__(
+        self,
+        executor: Executor,
+        ideal_executor: Executor,
+        settings: Settings,
+    ):
+        self.executor = executor
+        self.ideal_executor = ideal_executor
+        self.settings = settings
+        self.circuits = self.settings.make_circuits()
+        self.results = []
+
+    def get_cost(self) -> dict[str, int]:
+        """Return expected number of noisy expectation values required for calibration.
+        If method requires simulation, returns number of ideal expectation values needed."""
+        num_circuits = len(self.circuits)
+        num_methods = len(self.settings.mitigation_methods)
+
+        noisy = num_circuits * self.settings.max_executions * num_methods
+        ideal = noisy if self.ideal_executor else 0
+        return {
+            "noisy_executions": noisy,
+            "ideal_executions": ideal,
+        }
+
+    def run_circuits(self) -> list[dict[str, float]]:
+        """run the calibration circuits and store the ideal and noisy expectation values for each circuit."""
+        expvals = []
+        for circuit_data in self.circuits:
+            circuit = circuit_data.circuit
+            expval = self.executor(circuit)
+            mitigated = {}
+            for (
+                method_key,
+                factory,
+                scale_factors,
+                scale_method,
+                mitiq_func,
+            ) in self.settings.mitigate_functions():
+                mitigated[
+                    f"{method_key}-{factory}-{scale_factors}-{scale_method}"
+                ] = mitiq_func(circuit, self.executor)
+            ideal = self.ideal_executor(circuit)
+            expvals.append(
+                {"unmitigated": expval, "mitigated": mitigated, "ideal": ideal}
+            )
+        self.results = expvals
+        # return expvals
+
+    def compute_improvements(self):
+        """compute improvement factors for each calibration result"""
+        best_val = 1.0
+        best_key = ""
+        circuit_index = 0
+        for i, result in enumerate(self.results):
+            ideal = result["ideal"]
+            unmitigated = result["unmitigated"]
+            unmitigated_error = abs((ideal - unmitigated) / ideal)
+            for em_key, expval in result["mitigated"].items():
+                diff = abs((ideal - expval) / ideal)
+                error_diff = abs(unmitigated_error - diff)
+                if error_diff < best_val:
+                    best_val = error_diff
+                    best_key = em_key
+                    circuit_index = i
+        print("circuit index:", circuit_index)
+        print("|ideal - best| / ideal =", best_val)
+        return best_key
+
+    def get_optimal_strategy(self):
+        """uses the improvement factors to propose optimal error mitigation strategy"""
+        pass
+
+    def run(self):
+        """make_circuits -> run_circuits -> compute_improvements"""
+        pass
---
+++ mitiq/calibration/settings.py
@@ -0,0 +1,122 @@
+# Copyright (C) 2021 Unitary Fund
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from mitiq.zne.scaling import (
+    fold_global,
+    fold_gates_at_random,
+    insert_id_layers,
+)
+from mitiq.zne.inference import RichardsonFactory, LinearFactory, ExpFactory
+from mitiq.zne import execute_with_zne
+from mitiq.pec import execute_with_pec
+from mitiq.benchmarks import (
+    generate_rb_circuits,
+    generate_mirror_circuit,
+    generate_ghz_circuit,
+    generate_quantum_volume_circuit,
+)
+from mitiq._typing import QPROGRAM
+
+from typing import Callable, Any
+import networkx as nx
+from collections import namedtuple
+from itertools import product
+from functools import partial
+
+
+CircuitData = namedtuple(
+    "CircuitData",
+    ["circuit", "metadata"],
+)
+
+
+class Settings:
+    def __init__(
+        self,
+        mitigation_methods: list[str],
+        max_executions: int = 1000,
+        circuit_types: list[str] = ["rb", "mirror"],
+        circuit_dimensions: tuple[int, int] = (5, 20),
+        method_params: dict[str, Any] = {
+            "scale_factors": [[1.0, 2.0, 3.0], [1.0, 3.0, 5.0]],
+            "scale_methods": [fold_global, fold_gates_at_random],
+            "factories": [RichardsonFactory, LinearFactory],
+        },
+    ):
+        self.mitigation_methods = mitigation_methods
+        self.method_params = method_params
+        self.max_executions = max_executions
+        self.circuit_types = circuit_types
+        self.circuit_dimensions = circuit_dimensions
+
+    def make_circuits(self) -> list[QPROGRAM]:
+        """Generate circuits specified by `circuit_types` and `circuit_dimensions`"""
+        circuits = []
+        nqubits, depth = self.circuit_dimensions
+        for circuit_type in self.circuit_types:
+            if circuit_type == "ghz":
+                circuit = generate_ghz_circuit(nqubits)
+            elif circuit_type == "rb":
+                circuit = generate_rb_circuits(nqubits, depth)[0]
+            elif circuit_type == "mirror":
+                circuit = generate_mirror_circuit(
+                    nlayers=depth,
+                    two_qubit_gate_prob=1.0,
+                    connectivity_graph=nx.complete_graph(nqubits),
+                )[0]
+            elif circuit_type == "qv":
+                circuit = generate_quantum_volume_circuit(nqubits, depth)[0]
+            else:
+                raise ValueError(
+                    f"invalid value passed for `circuit_types`. Must be one of `ghz`, `rb`, `mirror`, or `qv`, but got {circuit_type}"
+                )
+
+            circuits.append(CircuitData(circuit, circuit_type))
+        return circuits
+
+    def mitigate_functions(self) -> list[tuple[str, Callable]]:
+        """return a list of tuples containing a string identifier and an
+        error mitigation function"""
+        funcs = []
+        for method in self.mitigation_methods:
+            if method == "zne":
+                for factory, scale_factors, scale_method in product(
+                    self.method_params["factories"],
+                    self.method_params["scale_factors"],
+                    self.method_params["scale_methods"],
+                ):
+                    inference_func = factory(scale_factors)
+                    em_func = partial(
+                        execute_with_zne,
+                        factory=inference_func,
+                        scale_noise=scale_method,
+                    )
+                    funcs.append(
+                        ("zne", factory, scale_factors, scale_method, em_func)
+                    )
+            elif method == "pec":
+                funcs.append(("pec", execute_with_pec))
+            else:
+                raise ValueError(
+                    "invalid value passed for mitigation_methods. Must be one of `zne`, `pec`"
+                )
+        return funcs
+
+
+ZNESettings = Settings(
+    ["zne"],
+    circuit_types=["ghz", "rb", "mirror"],
+    circuit_dimensions=(2, 5),
+)
