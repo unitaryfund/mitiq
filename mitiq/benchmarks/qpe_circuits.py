4302de0fec28192fd5401600d5373ada41b35047
Purva Thakre <purva.athakre@gmail.com>
2023-04-05 12:36:33 -0500

    qpe circuit function

--- mitiq/benchmarks/qpe_circuits.py
+++ mitiq/benchmarks/qpe_circuits.py
@@ -12,3 +12,79 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+""" Functions to create a QPE circuit"""
+from typing import Optional
+import cirq
+from mitiq import QPROGRAM
+from mitiq.interface import convert_from_mitiq
+
+
+def generate_qpe_circuit(
+    evalue_reg: int,
+    input_gate: cirq.Gate,
+    return_type: Optional[str] = None,
+) -> QPROGRAM:
+    """Returns a circuit to create a Quantum Phase Estimation circuit as
+    defined in https://en.wikipedia.org/wiki/Quantum_phase_estimation_algorithm
+
+    The IQFT circuit defined in this method is taken from taken from Sec 7.7.4
+    of :cite:`Wong_2022`.
+    Args:
+        evalue_reg : Number of qubits in the eigenvalue register
+        input_gate: A quantum gate
+        return_type: Return type of the output circuit.
+    Returns:
+        A Quantum Phase Estimation circuit.
+    """
+    if evalue_reg <= 0:
+        raise ValueError(
+            f"Arg `evalue_reg` must be positive but was {evalue_reg}."
+        )
+    num_qubits_for_gate = input_gate.num_qubits()
+    if num_qubits_for_gate > 1:
+        raise ValueError(
+            "This QPE circuit generation method only works for 1-qubit gates."
+        )
+
+    if evalue_reg < num_qubits_for_gate:
+        raise ValueError(
+            "The eigenvalue register must be larger than the eigenstate reg."
+        )
+
+    total_num_qubits = evalue_reg + num_qubits_for_gate
+    qreg = cirq.LineQubit.range(total_num_qubits)
+    circuit = cirq.Circuit()
+
+    # QFT circuit
+    # apply hadamard and controlled unitary to the qubits in the eigenvalue reg
+    for i in range(evalue_reg):
+        circuit.append(cirq.H(qreg[i]))
+    for i in range(total_num_qubits - 1)[::-1]:
+        circuit.append(
+            [input_gate(qreg[-1]).controlled_by(qreg[i])]
+            * (2 ** (evalue_reg - 1 - i))
+        )
+
+    # IQFT of the eigenvalue register
+    # swap the qubits in the eigenvalue register
+    for i in range(int(evalue_reg / 2)):
+        circuit.append(
+            cirq.SWAP(qreg[i], qreg[evalue_reg - 1 - i]),
+            strategy=cirq.InsertStrategy.NEW,
+        )
+    # apply inverse of hadamard and controlled unitary
+    for i in range(evalue_reg):
+        circuit.append(
+            cirq.H(qreg[i]), strategy=cirq.InsertStrategy.NEW_THEN_INLINE
+        )
+    for i in range(1, evalue_reg):
+        for j in range(evalue_reg):
+            if j < i:
+                circuit.append(
+                    cirq.inverse(input_gate(qreg[i]).controlled_by(qreg[j])),
+                    strategy=cirq.InsertStrategy.NEW,
+                )
+    return_type = "cirq" if not return_type else return_type
+
+    return convert_from_mitiq(circuit, return_type)
