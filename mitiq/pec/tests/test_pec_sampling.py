3c1c3f53a53744cec44b0962f92acbc70e116066
nate stemen <nate@unitary.fund>
2023-04-07 13:21:49 -0700

    move copyright back to UF

--- mitiq/pec/tests/test_pec_sampling.py
+++ mitiq/pec/tests/test_pec_sampling.py
@@ -1,4 +1,4 @@
-# Copyright Contributors to the Mitiq project.
+# Copyright (C) Unitary Fund
 #
 # This source code is licensed under the GPL license (v3) found in the
 # LICENSE file in the root directory of this source tree.
