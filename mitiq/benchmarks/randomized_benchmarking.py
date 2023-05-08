6e872576b053b770e42045e38da99cd27d99fe83
nate stemen <nate@unitary.fund>
2023-05-08 11:53:50 -0700

    cast `cfd_mat_1q` to numpy array
    _random_single_q_clifford is supposed to take numpy array, so better to
    actually cast it as opposed to using `typing.cast`.

--- mitiq/benchmarks/randomized_benchmarking.py
+++ mitiq/benchmarks/randomized_benchmarking.py
@@ -53,10 +53,8 @@ def generate_rb_circuits(
 
     if n_qubits == 1:
         c1 = cliffords.c1_in_xy
-        cfd_mat_1q = cast(
-            npt.NDArray[np.complex64],
-            [_gate_seq_to_mats(gates) for gates in c1],
-        )
+        cfd_mat_1q = np.array([_gate_seq_to_mats(gates) for gates in c1])
+
         circuits = [
             _random_single_q_clifford(qubits[0], num_cliffords, c1, cfd_mat_1q)
             for _ in range(trials)
