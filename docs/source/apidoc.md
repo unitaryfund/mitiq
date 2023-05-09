c3ff7a967f3b32cd33a034ff1ac5484457e1fc3a
nate stemen <nate@unitary.fund>
2023-05-09 12:18:38 -0700

    add docstrings

--- docs/source/apidoc.md
+++ docs/source/apidoc.md
@@ -268,6 +268,12 @@ See Ref. :cite:`Czarnik_2021_Quantum` for more details on these methods.
    :members:
 ```
 
+## Pauli Twirling
+```{eval-rst}
+.. automodule:: mitiq.pt.pt
+   :members:
+```
+
 ## Calibration
 
 ```{eval-rst}
--- mitiq/pt/pt.py
+++ mitiq/pt/pt.py
@@ -59,6 +59,21 @@ def execute_with_pauli_twirling(
     *,
     num_circuits: int = 10,
 ) -> float:
+    """Estimates the expecation value by averaging pauli twirled circuits.
+
+    Args:
+        circuit: The input circuit to execute with twirling.
+        executor: A Mitiq executor that executes a circuit and returns the
+            unmitigated ``QuantumResult`` (e.g. an expectation value).
+        observable: Observable to compute the expectation value of. If
+            ``None``, the ``executor`` must return an expectation value
+            (float). Otherwise, the ``QuantumResult`` returned by ``executor``
+            is used to compute the expectation of the observable.
+        num_circuits: Number of circuits to be randomly twirled, and averaged.
+
+    Returns:
+        The expectation value estimated with Pauli twirling.
+    """
     executor = (
         executor if isinstance(executor, Executor) else Executor(executor)
     )
@@ -68,6 +83,12 @@ def execute_with_pauli_twirling(
 
 
 def twirl_CNOT_gates(circuit: QPROGRAM, num_circuits: int) -> List[QPROGRAM]:
+    """Generate a list of circuits using Pauli twirling on CNOT gates.
+
+    Args:
+        circuit: The circuit to generate twirled versions of
+        num_circuits: The number of sampled circuits to return
+    """
     return [_twirl_CNOT_qprogram(circuit) for _ in range(num_circuits)]
 
 
@@ -77,6 +98,12 @@ def _twirl_CNOT_qprogram(circuit: cirq.Circuit) -> List[cirq.Circuit]:
 
 
 def twirl_CZ_gates(circuit: QPROGRAM, num_circuits: int) -> List[QPROGRAM]:
+    """Generate a list of circuits using Pauli twirling on CZ gates.
+
+    Args:
+        circuit: The circuit to generate twirled versions of
+        num_circuits: The number of sampled circuits to return
+    """
     return [
         _twirl_CZ_qprogram(circuit, num_circuits) for _ in range(num_circuits)
     ]
@@ -88,6 +115,17 @@ def _twirl_CZ_qprogram(circuit: cirq.Circuit) -> List[cirq.Circuit]:
 
 
 def _twirl_single_CNOT_gate(op: cirq.Operation) -> cirq.OP_TREE:
+    """Function which converts a CNOT to a logical equivalent. That is, it
+    converts CNOT operations into the following, leaving all other cirq
+    operations alone.
+
+        --P---⏺---R--
+              |
+        --Q---⊕---S--
+
+    Where P, Q, R, and S are Pauli operators sampled so as to not effect
+    change on the underlying unitary.
+    """
     if op.gate != cirq.CNOT:
         return op
 
@@ -103,6 +141,17 @@ def _twirl_single_CNOT_gate(op: cirq.Operation) -> cirq.OP_TREE:
 
 
 def _twirl_single_CZ_gate(op: cirq.Operation) -> cirq.OP_TREE:
+    """Function which converts a CNOT to a logical equivalent. That is, it
+    converts CNOT operations into the following, leaving all other cirq
+    operations alone.
+
+        --P---⏺---R--
+              |
+        --Q---⏺---S--
+
+    Where P, Q, R, and S are Pauli operators sampled so as to not effect
+    change on the underlying unitary.
+    """
     if op.gate != cirq.CZ:
         return op
 
