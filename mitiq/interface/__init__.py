f2597bb73e6f60cbe570496708952a947252abc2
Aaron Robertson <58564008+Aaron-Robertson@users.noreply.github.com>
2023-06-07 13:20:18 -0400

    Clean up global, isolate tests, and fix mock module (#1864)
    * clean up global, isolate tests, and fix mock module
    
    * single registration function
    
    * fix type errors and simplify flow
    
    * missed type error fix in save, remove old test
    
    * missed formatting
    
    * feedback fixes
    
    * add test for unsupported circuit
    
    * format fix

--- mitiq/interface/__init__.py
+++ mitiq/interface/__init__.py
@@ -11,7 +11,7 @@ from mitiq.interface.conversions import (
     convert_to_mitiq,
     noise_scaling_converter,
     append_cirq_circuit_to_qprogram,
-    register_mitiq_converter,
+    register_mitiq_converters,
     CircuitConversionError,
     UnsupportedCircuitError,
 )
