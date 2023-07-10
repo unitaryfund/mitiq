f17c6aeba83ede2fbe33798157afb35d337491a6
Min Li <ggbsjsy@hotmail.com>
2023-07-09 21:36:01 -0700

    reformat

--- mitiq/shadows/shadows_utils.py
+++ mitiq/shadows/shadows_utils.py
@@ -4,9 +4,18 @@ import numpy as np
 from cirq.ops.pauli_string import PauliString
 
 
-# given error of the fidelity between the true state and the reconstructed state,
-# return the number of measurements needed
 def min_n_total_measurements(epsilon: float, num_qubits: int) -> int:
+    """
+    Calculate the number of measurements required to satisfy the shadow bound
+        for the Pauli measurement scheme.
+
+    Args:
+        epsilon (float): The error on the estimator.
+        num_qubits (int): The number of qubits in the system.
+
+    Returns:
+        An integer that gives the number of samples required to satisfy the shadow bound.
+    """
     return int(34 * (4**num_qubits) * epsilon ** (-2))
 
 
@@ -14,6 +23,19 @@ def min_n_total_measurements(epsilon: float, num_qubits: int) -> int:
 def calculate_shadow_bound(
     error: float, observables: List[PauliString], failure_rate: float
 ) -> Tuple[int, int]:
+    """
+    Calculate the shadow bound for the Pauli measurement scheme.
+
+    Args:
+        error (float): The error on the estimator.
+        observables (list) : List of cirq.PauliString corresponding to the observables we intend to
+            measure.
+        failure_rate (float): Rate of failure for the bound to hold.
+
+    Returns:
+        An integer that gives the number of samples required to satisfy the shadow bound and
+        the chunk size required attaining the specified failure rate.
+    """
     M = len(observables)
     K = 2 * np.log(2 * M / failure_rate)
 
@@ -31,14 +53,37 @@ def calculate_shadow_bound(
 
 
 def operator_2_norm(R: np.ndarray[np.complex128]) -> float:
+    """
+    Calculate the operator 2-norm.
+
+    Args:
+        R (array): The operator whose norm we want to calculate.
+
+    Returns:
+        Scalar corresponding to the norm.
+    """
     return float(
         np.sqrt(np.trace(R.conjugate().transpose() @ R)).reshape(-1).real
     )
 
 
+# given error of the fidelity between the true state and the reconstructed state,
+# return the number of measurements needed
 def fidelity(
     state_vector: np.ndarray[np.complex128], rho: np.ndarray[np.complex128]
 ) -> float:
+    r"""
+    Calculate the fidelity $$F(\rho,\sigma)=\mathrm{Tr}\sqrt{\rho^{1/2}\sigma\rho^{1/2}}$$,
+        when $$\rho=|v\rangle\langle v|$$ is a pure state
+        $$F(\rho,\sigma)=\langle v|\sigma|v\rangle$$.
+
+    Args:
+        state_vector (array): The vector whose norm we want to calculate.
+        rho (array): The operator whose norm we want to calculate.
+
+    Returns:
+        Scalar corresponding to the fidelity.
+    """
     return float(
         np.reshape(state_vector.conj().T @ rho @ state_vector, -1).real
     )
