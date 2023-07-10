b66883b8f2e1a4979dd4d5cf9e9353841413de7f
Min Li <ggbsjsy@hotmail.com>
2023-07-10 12:09:21 -0700

    modify test

--- mitiq/shadows/rotation_gates.py
+++ mitiq/shadows/rotation_gates.py
@@ -1,33 +1,41 @@
 from typing import List
-
 import cirq
 import numpy as np
 
 
 # generate N random Pauli strings for given number of qubits
+
 def generate_random_pauli_strings(
     num_qubits: int, num_strings: int
 ) -> List[str]:
-    # return "XXXYZYZX" where len string == num_qubits
+    """Generate a list of random Pauli strings.
+
+    Args:
+        num_qubits: The number of qubits in the Pauli strings.
+        num_strings: The number of Pauli strings to generate.
+
+    Returns:
+        A list of number of `num_strings` random Pauli strings
+          of length `num_qubits`.
+    """
 
     # Sample random Pauli operators uniformly from X, Y, Z
     unitary_ensemble = ["X", "Y", "Z"]
     paulis = np.random.choice(unitary_ensemble, (num_strings, num_qubits))
     return ["".join(pauli) for pauli in paulis]
 
-
 # attach random rotate gates to N copies of the circuit
 def get_rotated_circuits(
     circuit: cirq.Circuit, pauli_strings: List[str]
 ) -> List[cirq.Circuit]:
-    """Returns a list of circuits that are identical to the given circuit,
-    except that each one has a different Pauli gate applied to each qubit,
-    followed by a measurement.
+    """Returns a list of circuits that are identical to the given circuit, 
+        except that each one has a different Pauli gate applied to each qubit, 
+        followed by a measurement.
 
     Args:
         circuit: The circuit to measure.
-        pauli_strings: The Pauli strings to apply to each qubit, in order,
-        before measuring.
+        pauli_strings: The Pauli strings to apply to each qubit, in order, 
+            before measuring.
 
     Returns:
         A list of circuits, one for each Pauli string.
@@ -36,10 +44,9 @@ def get_rotated_circuits(
     num_qubits = len(qubits)
     rotated_circuits = []
     for pauli_string in pauli_strings:
-        assert len(pauli_string) == num_qubits, (
-            f"Pauli string must be same length as number of qubits, "
-            f"got {len(pauli_string)} and {num_qubits}"
-        )
+        assert (
+            len(pauli_string) == num_qubits
+        ), f"Pauli string must be same length as number of qubits, got {len(pauli_string)} and {num_qubits}"
         rotated_circuit = circuit.copy()
         for i, pauli in enumerate(pauli_string):
             qubit = qubits[i]
