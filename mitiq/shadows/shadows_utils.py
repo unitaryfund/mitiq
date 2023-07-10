af9b008b6b54378e9788094b093f90d008ddb26c
Min Li <ggbsjsy@hotmail.com>
2023-07-10 16:21:01 -0700

    format

--- mitiq/shadows/shadows_utils.py
+++ mitiq/shadows/shadows_utils.py
@@ -15,7 +15,8 @@ def min_n_total_measurements(epsilon: float, num_qubits: int) -> int:
         num_qubits (int): The number of qubits in the system.
 
     Returns:
-        An integer that gives the number of samples required to satisfy the shadow bound.
+        An integer that gives the number of samples required to satisfy the
+        shadow bound.
     """
     return int(34 * (4**num_qubits) * epsilon ** (-2))
 
@@ -31,13 +32,14 @@ def calculate_shadow_bound(
 
     Args:
         error (float): The error on the estimator.
-        observables (list) : List of cirq.PauliString corresponding to the observables we intend to
-            measure.
+        observables (list) : List of cirq.PauliString corresponding to the
+        observables we intend to measure.
         failure_rate (float): Rate of failure for the bound to hold.
 
     Returns:
-        An integer that gives the number of samples required to satisfy the shadow bound and
-        the chunk size required attaining the specified failure rate.
+        An integer that gives the number of samples required to satisfy
+        the shadow bound and the chunk size required attaining the specified
+        failure rate.
     """
     M = len(observables)
     K = 2 * np.log(2 * M / failure_rate)
@@ -70,14 +72,16 @@ def operator_2_norm(R: np.ndarray[Any, Any]) -> float:
     )
 
 
-# given error of the fidelity between the true state and the reconstructed state,
+# given error of the fidelity between the true state
+# and the reconstructed state,
 # return the number of measurements needed
 def fidelity(
     state_vector: np.ndarray[Any, Any],
     rho: np.ndarray[Any, Any],
 ) -> float:
     r"""
-    Calculate the fidelity $$F(\rho,\sigma)=\mathrm{Tr}\sqrt{\rho^{1/2}\sigma\rho^{1/2}}$$,
+    Calculate the fidelity
+    $$F(\rho,\sigma)=\mathrm{Tr}\sqrt{\rho^{1/2}\sigma\rho^{1/2}}$$,
         when $$\rho=|v\rangle\langle v|$$ is a pure state
         $$F(\rho,\sigma)=\langle v|\sigma|v\rangle$$.
 
