e4405c92d1b8b639dbb39246fa67f716527f5c42
Min Li <ggbsjsy@hotmail.com>
2023-07-09 21:08:50 -0700

    reformat

--- mitiq/shadows/computational_basis_measurement.py
+++ mitiq/shadows/computational_basis_measurement.py
@@ -4,16 +4,20 @@ import cirq
 import numpy as np
 
 from mitiq import Executor
-from mitiq.shadows.rotation_gates import generate_random_pauli_strings, get_rotated_circuits
+from mitiq.shadows.rotation_gates import (
+    generate_random_pauli_strings,
+    get_rotated_circuits,
+)
 
 
 # Stage 1 of Classical Shadows: Measurements
 def shadow_measure_with_executor(
-        circuit: cirq.Circuit,
-        executor: Executor,
-        n_total_measurements: int, ) -> Tuple[np.ndarray, np.ndarray]:
+    circuit: cirq.Circuit,
+    executor: Executor,
+    n_total_measurements: int,
+) -> Tuple[np.ndarray, np.ndarray]:
     r"""
-    
+
     Given a circuit, perform z-basis measurements on the circuit and return the outcomes in terms of a string,
      which represents for z-basis measurement outcomes $$1:=\{1,0\}$$, $$-1:=\{0,1\}$$.
 
@@ -21,7 +25,8 @@ def shadow_measure_with_executor(
               executor (Executor): measure with executor defined for measurements.
               n_total_measurements (int): number of snapshots.
 
-    Returns: outcomes (array): Tuple of two numpy arrays. The first array contains measurement outcomes (-1, 1)
+    Returns: outcomes (array): Tuple of two numpy arrays. The first array
+                        contains measurement outcomes (-1, 1)
                     while the second array contains the index for the sampled Pauli's (0,1,2=X,Y,Z).
                     Each row of the arrays corresponds to a distinct snapshot or sample while each
                     column corresponds to a different qubit.
@@ -30,7 +35,9 @@ def shadow_measure_with_executor(
     # Generate random Pauli unitaries
     qubits = list(circuit.all_qubits())
     num_qubits = len(qubits)
-    pauli_strings = generate_random_pauli_strings(num_qubits, n_total_measurements)
+    pauli_strings = generate_random_pauli_strings(
+        num_qubits, n_total_measurements
+    )
     # Attach measurement gates to the circuit
     rotated_circuits = get_rotated_circuits(circuit, pauli_strings)
 
@@ -43,8 +50,12 @@ def shadow_measure_with_executor(
         bitstring = list(result.get_counts().keys())[0]
         outcome = [1 - int(i) * 2 for i in bitstring]
         shadow_outcomes.append(outcome)
-    # Combine the computational basis outcomes $$|\mathbf{b}\rangle$$ and the unitaries sampled from $$CL_2^{\otimes n}$$.
+    # Combine the computational basis outcomes $$|\mathbf{b}\rangle$$
+    # and the unitaries sampled from $$CL_2^{\otimes n}$$.
     shadow_outcomes = np.array(shadow_outcomes, dtype=int)
-    assert shadow_outcomes.shape == (n_total_measurements, num_qubits), f"shape is {shadow_outcomes.shape}"
+    assert shadow_outcomes.shape == (
+        n_total_measurements,
+        num_qubits,
+    ), f"shape is {shadow_outcomes.shape}"
     pauli_strings = np.array(pauli_strings, dtype=str)
     return shadow_outcomes, pauli_strings
--- mitiq/shadows/test/test_computational_basis_measurement.py
+++ mitiq/shadows/test/test_computational_basis_measurement.py
@@ -4,10 +4,14 @@ import cirq
 import pytest
 
 from mitiq import Executor
-from mitiq.shadows.computational_basis_measurement import shadow_measure_with_executor
+from mitiq.shadows.computational_basis_measurement import (
+    shadow_measure_with_executor,
+)
 from mitiq.shadows.executor_functions import cirq_simulator_shadow_executor_fn
 
-executor = Executor(cirq_simulator_shadow_executor_fn, max_batch_size=int(1e10))
+executor = Executor(
+    cirq_simulator_shadow_executor_fn, max_batch_size=int(1e10)
+)
 n_total_measurements = 10
 
 
@@ -32,24 +36,34 @@ def test_shadow_measure_with_executor_no_errors(n_qubits: int):
 def test_shadow_measure_with_executor_output_dimensions(n_qubits: int):
     qubits = cirq.LineQubit.range(n_qubits)
     circuit = simple_test_circuit(qubits)
-    shadow_outcomes, pauli_strings = shadow_measure_with_executor(circuit, executor, n_total_measurements)
-    assert shadow_outcomes.shape == (n_total_measurements,
-                                     n_qubits), f"Shadow outcomes have incorrect shape, expected {(n_total_measurements, n_qubits)}, got {shadow_outcomes.shape}"
+    shadow_outcomes, pauli_strings = shadow_measure_with_executor(
+        circuit, executor, n_total_measurements
+    )
+    assert shadow_outcomes.shape == (
+        n_total_measurements,
+        n_qubits,
+    ), f"Shadow outcomes have incorrect shape, expected {(n_total_measurements, n_qubits)}, got {shadow_outcomes.shape}"
     assert pauli_strings.shape == (
-    n_total_measurements,), f"Pauli strings have incorrect shape, expected {(n_total_measurements, n_qubits)}, got {pauli_strings.shape}"
-    assert len(pauli_strings[
-                   0]) == n_qubits, f"Pauli strings have incorrect number of characters, expected {n_qubits}, got {len(pauli_strings[0])}"
+        n_total_measurements,
+    ), f"Pauli strings have incorrect shape, expected {(n_total_measurements, n_qubits)}, got {pauli_strings.shape}"
+    assert (
+        len(pauli_strings[0]) == n_qubits
+    ), f"Pauli strings have incorrect number of characters, expected {n_qubits}, got {len(pauli_strings[0])}"
 
 
 @pytest.mark.parametrize("n_qubits", [1, 2, 5])
 def test_shadow_measure_with_executor_output_types(n_qubits: int):
     qubits = cirq.LineQubit.range(n_qubits)
     circuit = simple_test_circuit(qubits)
-    shadow_outcomes, pauli_strings = shadow_measure_with_executor(circuit, executor, n_total_measurements)
-    assert shadow_outcomes[
-               0].dtype == int, f"Shadow outcomes have incorrect dtype, expected int, got {shadow_outcomes.dtype}"
-    assert isinstance(pauli_strings[0],
-                      str), f"Pauli strings have incorrect dtype, expected str, got {pauli_strings.dtype}"
+    shadow_outcomes, pauli_strings = shadow_measure_with_executor(
+        circuit, executor, n_total_measurements
+    )
+    assert (
+        shadow_outcomes[0].dtype == int
+    ), f"Shadow outcomes have incorrect dtype, expected int, got {shadow_outcomes.dtype}"
+    assert isinstance(
+        pauli_strings[0], str
+    ), f"Pauli strings have incorrect dtype, expected str, got {pauli_strings.dtype}"
 
 
 @pytest.mark.parametrize("n_qubits", [1, 2, 5])
@@ -63,4 +77,6 @@ def test_shadow_measure_with_executor_time_growth(n_qubits: int):
         shadow_measure_with_executor(circuit, executor, n)
         times.append(time.time() - start_time)
     for i in range(1, len(times)):
-        assert times[i] / times[i - 1] == pytest.approx(measurements[i] / measurements[i - 1], rel=0.2)
+        assert times[i] / times[i - 1] == pytest.approx(
+            measurements[i] / measurements[i - 1], rel=0.2
+        )
