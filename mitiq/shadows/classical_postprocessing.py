1b01d104b458e133696e425110eec1cea3921a54
Min Li <ggbsjsy@hotmail.com>
2023-07-12 17:45:56 -0700

    add doc and header.

--- mitiq/shadows/classical_postprocessing.py
+++ mitiq/shadows/classical_postprocessing.py
@@ -1,3 +1,10 @@
+# Copyright (C) Unitary Fund
+#
+# This source code is licensed under the GPL license (v3) found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Classical postprocessing process of classical shadows."""
+
 from typing import Tuple, List, Any
 from numpy.typing import NDArray
 import numpy as np
