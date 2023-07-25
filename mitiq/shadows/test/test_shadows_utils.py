f1837f6e8c47d7284916f34037cbcde47df895a6
Min Li <ggbsjsy@hotmail.com>
2023-07-25 00:06:40 -0700

    check format and add acknowlegement.

--- mitiq/shadows/test/test_shadows_utils.py
+++ mitiq/shadows/test/test_shadows_utils.py
@@ -1,3 +1,8 @@
+# Copyright (C) Unitary Fund
+#
+# This source code is licensed under the GPL license (v3) found in the
+# LICENSE file in the root directory of this source tree.
+"""Test functions for utility functions of classical shadows"""
 import cirq
 import numpy as np
 import mitiq
