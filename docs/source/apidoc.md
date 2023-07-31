9e8c6194b353dea03985976247fd2aca9d5c0167
Min Li <minl2@illinois.edu>
2023-07-30 19:03:08 -0700

    [Classical Shadows 1] utils for shadows and unit test (#1907)
    * utils for shadows and unit test
    
    * reformat
    
    * apidoc
    
    * format
    
    * rename numpy typing
    
    * Update docstring for shadow_utils
    
    * add doc and header.
    
    * fix test functions
    
    * Update mitiq/shadows/shadows_utils.py
    
    Co-authored-by: Andrea Mari <andreamari84@gmail.com>
    
    * Update mitiq/shadows/shadows_utils.py
    
    Co-authored-by: Andrea Mari <andreamari84@gmail.com>
    
    * reformat and apply Andrea's suggestion.
    
    * change docstring
    
    * modify API-doc; rename functions; pull out `shadow_norm` from `n_measurements_opts_expectation_bound` as a standalone function
    
    * Add util function `transform_to_cirq_paulistring` and test
    
    * resolve cirq PauliString typing issue
    
    * make utils could evaluate operators w/ coefficients
    
    * check format
    
    * check format and add acknowlegement.
    
    * Remove cirq.PauliString option and its transform function.
    
    * minor changes
    
    * update docstring
    
    ---------
    
    Co-authored-by: Andrea Mari <andreamari84@gmail.com>
    Co-authored-by: Nathan Shammah <nathan.shammah@gmail.com>
    Co-authored-by: nate stemen <nate@unitary.fund>

--- docs/source/apidoc.md
+++ docs/source/apidoc.md
@@ -134,11 +134,18 @@ See Ref. :cite:`Czarnik_2021_Quantum` for more details on these methods.
    :members:
 ```
 
+
 ## Classical Shadows
 
 ### Quantum Processing
 ```{eval-rst}
 .. automodule:: mitiq.shadows.quantum_processing
+   :members:
+```
+
+### Utility Functions
+```{eval-rst}
+.. automodule:: mitiq.shadows.shadows_utils
    :members:
 ```
 
---
+++ mitiq/shadows/shadows_utils.py
@@ -0,0 +1,103 @@
+# Copyright (C) Unitary Fund
+# Portions of this code have been adapted from PennyLane's tutorial
+# on Classical Shadows.
+# Original authors: PennyLane developers: Brian Doolittle, Roeland Wiersema
+# Tutorial link: https://pennylane.ai/qml/demos/tutorial_classical_shadows
+#
+# This source code is licensed under the GPL license (v3) found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Defines utility functions for classical shadows protocol."""
+from typing import List, Tuple
+
+import numpy as np
+from numpy.typing import NDArray
+
+import mitiq
+
+
+def n_measurements_tomography_bound(epsilon: float, num_qubits: int) -> int:
+    """
+    This function returns the minimum number of classical shadows required
+    for state reconstruction for achieving the desired accuracy.
+
+    Args:
+        epsilon: The error on the estimator.
+        num_qubits: The number of qubits in the system.
+
+    Returns:
+        An integer that gives the number of snapshots required to satisfy the
+        shadow bound.
+    """
+    return int(34 * (4**num_qubits) * epsilon ** (-2))
+
+
+def local_clifford_shadow_norm(obs: mitiq.PauliString) -> float:
+    """
+    Calculate shadow norm of an operator with random unitary sampled from local
+    Clifford group.
+
+    Args:
+        obs: An observable in terms of a ``mitiq.PauliString`` object.
+
+    Returns:
+        Shadow norm when unitary ensemble is local Clifford group.
+    """
+    opt = obs.matrix()
+    norm = (
+        np.linalg.norm(
+            opt - np.trace(opt) / 2 ** int(np.log2(opt.shape[0])),
+            ord=np.inf,
+        )
+        ** 2
+    )
+    return float(norm)
+
+
+def n_measurements_opts_expectation_bound(
+    error: float,
+    observables: List[mitiq.PauliString],
+    failure_rate: float,
+) -> Tuple[int, int]:
+    """
+    This function returns the minimum number of classical shadows required and
+    the number of groups "k" into which we need to split the shadows for
+    achieving the desired accuracy and failure rate in operator expectation
+    value estimation.
+
+    Args:
+        error: The error on the estimator.
+        observables: List of mitiq.PauliString corresponding to the
+            observables we intend to measure.
+        failure_rate: Rate of failure for the bound to hold.
+
+    Returns:
+        Integers quantifying the number of snapshots required to satisfy
+        the shadow bound and the chunk size required to attain the specified
+        failure rate.
+    """
+    M = len(observables)
+    K = 2 * np.log(2 * M / failure_rate)
+
+    N = (
+        34
+        * max(local_clifford_shadow_norm(o) for o in observables)
+        / error**2
+    )
+    return int(np.ceil(N * K)), int(K)
+
+
+def fidelity(
+    state_vector: NDArray[np.complex64],
+    rho: NDArray[np.complex64],
+) -> float:
+    """
+    Calculate the fidelity between a state vector and a density matrix.
+    Args:
+        state_vector: The vector whose norm we want to calculate.
+        rho: The operator whose norm we want to calculate.
+
+    Returns:
+        Scalar corresponding to the fidelity.
+    """
+    return np.reshape(state_vector.conj().T @ rho @ state_vector, -1).real[0]
