06f82af57095dc5b71dce8005755bd07b4501b4b
Min Li <minl2@illinois.edu>
2023-08-10 09:40:40 -0700

    [Robust Shadow 2]Add few functions for robust shadow estimation (#1940)
    * add few functions for robust shadow estimation
    
    * reformat
    
    * address comments
    
    * address comments
    
    * address comments
    
    * address comments
    
    * address comments
    
    * address comments
    
    * add doc strings
    
    * address Andrea's comments.

--- mitiq/shadows/test/test_shadows_utils.py
+++ mitiq/shadows/test/test_shadows_utils.py
@@ -4,14 +4,60 @@
 # LICENSE file in the root directory of this source tree.
 
 """Defines utility functions for classical shadows protocol."""
-import numpy as np
 
+import numpy as np
+import cirq
 import mitiq
 from mitiq.shadows.shadows_utils import (
-    fidelity,
-    n_measurements_opts_expectation_bound,
+    kronecker_product,
+    operator_ptm_vector_rep,
+    eigenvalues_to_bitstring,
+    bitstring_to_eigenvalues,
+    create_string,
     n_measurements_tomography_bound,
+    n_measurements_opts_expectation_bound,
+    fidelity,
 )
+
+
+# Tests start here
+def test_kronecker_product():
+    matrices = [np.array([[1, 2], [3, 4]]), np.array([[0, 1], [1, 0]])]
+    expected_result = np.array(
+        [[0, 1, 0, 2], [1, 0, 2, 0], [0, 3, 0, 4], [3, 0, 4, 0]]
+    )
+    np.testing.assert_array_equal(kronecker_product(matrices), expected_result)
+
+
+def test_operator_ptm_vector_rep():
+    opt = cirq.I._unitary_() / np.sqrt(2)
+    expected_result = np.array([1.0, 0.0, 0.0, 0.0])
+    np.testing.assert_array_almost_equal(
+        operator_ptm_vector_rep(opt), expected_result
+    )
+
+
+def test_eigenvalues_to_bitstring():
+    values = [-1, 1, 1]
+    assert eigenvalues_to_bitstring(values) == "100"
+    assert bitstring_to_eigenvalues(eigenvalues_to_bitstring(values)) == values
+
+
+def test_bitstring_to_eigenvalues():
+    bitstring = "100"
+    np.testing.assert_array_equal(
+        bitstring_to_eigenvalues(bitstring), np.array([-1, 1, 1])
+    )
+    assert (
+        eigenvalues_to_bitstring(bitstring_to_eigenvalues(bitstring))
+        == bitstring
+    )
+
+
+def test_create_string():
+    str_len = 5
+    loc_list = [1, 3]
+    assert create_string(str_len, loc_list) == "01010"
 
 
 def test_n_measurements_tomography_bound():
