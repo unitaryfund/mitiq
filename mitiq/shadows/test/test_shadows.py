51e14ca0b4ad470e5e97c2d5bc56ce3d69b2c8d3
Min Li <ggbsjsy@hotmail.com>
2023-08-12 22:21:56 -0700

    Update mitiq/shadows/test/test_shadows.py
    Co-authored-by: nate stemen <nate@stemen.email>

--- mitiq/shadows/test/test_shadows.py
+++ mitiq/shadows/test/test_shadows.py
@@ -23,7 +23,6 @@ num_qubits: int = 2
 qubits = cirq.LineQubit.range(num_qubits)
 circuit = cirq.Circuit([cirq.H(q) for q in qubits])
 circuit.append(cirq.CNOT(qubits[0], qubits[1]))
-# Define list of observables
 observables = [mitiq.PauliString("X", support=(i,)) for i in range(num_qubits)]
 
 
