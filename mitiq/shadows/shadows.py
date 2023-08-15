6cc85d7bf9bce34c10b46a71e85235fe71dbb12f
Min Li <ggbsjsy@hotmail.com>
2023-08-15 09:44:37 -0700

    reformat

--- mitiq/shadows/shadows.py
+++ mitiq/shadows/shadows.py
@@ -133,7 +133,6 @@ def shadow_quantum_processing(
         np.random.seed(random_seed)
     r"""
     Additional Information:
-    
     Shadow stage 1: Sample random unitary form
     :math:`\mathcal{g}\subset \mathrm{U}(2^n)` and perform computational
     basis measurement. In the current state, we have implemented
@@ -187,7 +186,6 @@ def classical_post_processing(
 
     """
     Additional Information:
-    
     Shadow stage 2: Estimate the expectation value of the observables OR
     reconstruct the state
     """
