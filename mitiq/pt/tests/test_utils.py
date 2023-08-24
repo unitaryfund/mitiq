75154a39cb2779d9d46b54c16ac8cbb1e873b35c
Purva Thakre <purva.athakre@gmail.com>
2023-08-24 00:30:07 -0500

    main function

--- mitiq/pt/tests/test_utils.py
+++ mitiq/pt/tests/test_utils.py
@@ -7,9 +7,16 @@
 
 import random
 
+import cirq
 import numpy as np
 
-from mitiq.pt.utils import _matrix_no_vec, _matrix_vec, _pauli_vectorized_list
+from mitiq.pt.utils import (
+    _get_n_qubit_paulis,
+    _matrix_no_vec,
+    _matrix_vec,
+    _pauli_vectorized_list,
+    ptm_matrix,
+)
 
 
 def test_matrix_to_vec():
@@ -34,8 +41,19 @@ def test_size_of_pauli_vec():
     Size of each matrix should be 2**num_qubits.
     """
     for i in range(1, 5):
+        assert len(_get_n_qubit_paulis(i)) == 4**i
         expected_output_pauli_vec_list = _pauli_vectorized_list(i)
         assert len(expected_output_pauli_vec_list) == 4**i
         # randomly check 1 entry in the output is a column vector
-        j = random.randrange(len(expected_output_pauli_vec_list) + 1)
+        j = random.randrange(len(expected_output_pauli_vec_list))
         assert expected_output_pauli_vec_list[j].shape[-1] == 1
+
+
+def test_ptm_matrix_size():
+    """Checks the output of the PTM matrix function."""
+    q0 = cirq.LineQubit(0)
+    circuit = cirq.Circuit()
+    circuit.append(cirq.Rz(rads=0.25 * np.pi)(q0))
+    expected_ptm_matrix = ptm_matrix(circuit, 1)
+    assert expected_ptm_matrix.shape == (4**1, 4**1)
+    assert np.isclose(expected_ptm_matrix[0, 1], 0.0, rtol=1e-05)
--- mitiq/pt/utils.py
+++ mitiq/pt/utils.py
@@ -29,13 +29,8 @@ def _matrix_no_vec(vec_matrix, num_qubits):
     return np.transpose(vec_to_matrix)
 
 
-def _pauli_vectorized_list(num_qubits):
-    """Define a function to create a list of vectorized matrices.
-
-    If the density matrix of interest has more than n>1 qubits, the
-    Pauli group is used to generate n-fold tensor products before
-    vectorizing the unitaries.
-    """
+def _get_n_qubit_paulis(num_qubits):
+    """Get list of n-qubit Pauli unitaries."""
     pauli_unitary_list = [
         cirq.unitary((cirq.I)),
         cirq.unitary((cirq.X)),
@@ -54,8 +49,44 @@ def _pauli_vectorized_list(num_qubits):
                 new_pauli = np.kron(j, k)
                 empty_pauli_list.append(new_pauli)
         n_qubit_paulis = empty_pauli_list
+    return n_qubit_paulis
 
+
+def _pauli_vectorized_list(num_qubits):
+    """Define a function to create a list of vectorized matrices.
+
+    If the density matrix of interest has more than n>1 qubits, the
+    Pauli group is used to generate n-fold tensor products before
+    vectorizing the unitaries.
+    """
+    n_qubit_paulis = _get_n_qubit_paulis(num_qubits)
     output_pauli_vec_list = []
     for i in n_qubit_paulis:
         output_pauli_vec_list.append(_matrix_vec(i))
     return output_pauli_vec_list
+
+
+def ptm_matrix(circuit, num_qubits):
+    """Find the Pauli Transfer Matrix (PTM) of a circuit."""
+    superop = cirq.kraus_to_superoperator(cirq.kraus(circuit))
+    ptm_matrix = np.zeros([4**num_qubits, 4**num_qubits], dtype=complex)
+    vec_pauli = _pauli_vectorized_list(num_qubits)
+
+    for i in range(4**num_qubits):
+        superop_on_pauli_vec = np.matmul(superop, vec_pauli[i])
+        superop_on_pauli_matrix = _matrix_no_vec(
+            superop_on_pauli_vec, num_qubits
+        )
+        ptm_matrix_row = []
+
+        n_qubit_paulis = _get_n_qubit_paulis(num_qubits)
+        for j in range(4**num_qubits):
+            pauli_superop_pauli = np.matmul(
+                n_qubit_paulis[j], superop_on_pauli_matrix
+            )
+            ptm_matrix_row.append(
+                (0.5**num_qubits) * np.trace(pauli_superop_pauli)
+            )
+
+        ptm_matrix[i] = ptm_matrix_row
+        return ptm_matrix
