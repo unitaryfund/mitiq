66564f10de7935d14383c0a5e92b3fd09e10fe36
Purva Thakre <purva.athakre@gmail.com>
2023-08-23 23:33:25 -0500

    tests for pirvate functions

--- mitiq/pt/tests/test_utils.py
+++ mitiq/pt/tests/test_utils.py
@@ -4,3 +4,38 @@
 # LICENSE file in the root directory of this source tree.
 
 """Tests for Pauli Twirling Utility functions."""
+
+import random
+
+import numpy as np
+
+from mitiq.pt.utils import _matrix_no_vec, _matrix_vec, _pauli_vectorized_list
+
+
+def test_matrix_to_vec():
+    """Check a square matrix is vectorized into a column vector."""
+    for i in range(1, 5):
+        func_output = _matrix_vec(np.random.rand(2**i, 2**i))
+        assert func_output.shape[-1] == 1
+
+
+def test_matrix_no_vec():
+    """Check a vec (column vector) is turned back into a square matrix."""
+    for i in range(1, 5):
+        vec_output = _matrix_vec(np.random.rand(2**i, 2**i))
+        vec_to_matrix = _matrix_no_vec(vec_output, i)
+        assert vec_to_matrix.shape[0] == vec_to_matrix.shape[-1]
+
+
+def test_size_of_pauli_vec():
+    """Check the output size of a list of n-qubit vec Paulis.
+
+    Number of n-qubit Paulis should be  4**num_qubits.
+    Size of each matrix should be 2**num_qubits.
+    """
+    for i in range(1, 5):
+        expected_output_pauli_vec_list = _pauli_vectorized_list(i)
+        assert len(expected_output_pauli_vec_list) == 4**i
+        # randomly check 1 entry in the output is a column vector
+        j = random.randrange(len(expected_output_pauli_vec_list) + 1)
+        assert expected_output_pauli_vec_list[j].shape[-1] == 1
--- mitiq/pt/utils.py
+++ mitiq/pt/utils.py
@@ -5,12 +5,8 @@
 
 """Utility functions for Pauli Twirling of CZ & CNOT gates."""
 
-import numpy as np
 import cirq
-
-
-pauli_unitary_list = [cirq.unitary((cirq.I)), cirq.unitary((cirq.X)),
-                      cirq.unitary((cirq.Y)), cirq.unitary((cirq.Z))]
+import numpy as np
 
 
 def _matrix_vec(matrix):
@@ -21,27 +17,34 @@ def _matrix_vec(matrix):
     vectorized form.
     """
     matrix_transpose = np.transpose(matrix)
-    return (np.reshape(matrix_transpose, (-1, 1)))
+    return np.reshape(matrix_transpose, (-1, 1))
 
 
-def _matrix_no_vec(vec_matrix):
+def _matrix_no_vec(vec_matrix, num_qubits):
     """Define the matrix form of a vectorized array.
 
     Converts a column vector (vec) to a square matrix.
     """
-    vec_to_matrix = np.reshape(vec_matrix, (2, 2))
-    return (np.transpose(vec_to_matrix))
+    vec_to_matrix = np.reshape(vec_matrix, (2**num_qubits, 2**num_qubits))
+    return np.transpose(vec_to_matrix)
 
 
-def _pauli_vectorized_list(num_qubits, pauli_unitary_list):
+def _pauli_vectorized_list(num_qubits):
     """Define a function to create a list of vectorized matrices.
 
     If the density matrix of interest has more than n>1 qubits, the
     Pauli group is used to generate n-fold tensor products before
     vectorizing the unitaries.
     """
+    pauli_unitary_list = [
+        cirq.unitary((cirq.I)),
+        cirq.unitary((cirq.X)),
+        cirq.unitary((cirq.Y)),
+        cirq.unitary((cirq.Z)),
+    ]
+
     # get the n-qubit paulis from the Pauli group
-    # diregard the n-qubit paulis with complex phase
+    # disregard the n-qubit paulis with complex phase
     n_qubit_paulis = pauli_unitary_list
     for i in range(num_qubits - 1):
         n_qubit_paulis = n_qubit_paulis
@@ -54,7 +57,5 @@ def _pauli_vectorized_list(num_qubits, pauli_unitary_list):
 
     output_pauli_vec_list = []
     for i in n_qubit_paulis:
-        output_pauli_vec_list.appen(_matrix_vec[i])
-    return (output_pauli_vec_list)
-
-
+        output_pauli_vec_list.append(_matrix_vec(i))
+    return output_pauli_vec_list
