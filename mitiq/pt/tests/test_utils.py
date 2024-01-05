5980d868783ef29d468a540696195dd77a5b0419
Purva Thakre <purva.athakre@gmail.com>
2024-01-05 06:37:25 -0600

    add ptm + tests for 1 Pauli gate, no mypy

---
+++ mitiq/pt/tests/test_utils.py
@@ -0,0 +1,126 @@
+# Copyright (C) Unitary Fund
+#
+# This source code is licensed under the GPL license (v3) found in the
+# LICENSE file in the root directory of this source tree.
+
+import cirq
+import numpy as np
+import pytest
+
+from mitiq.pt.utils import _n_qubit_paulis, _pauli_vectorized_list, ptm_matrix
+
+single_qubit_paulis = [
+    cirq.unitary((cirq.I)),
+    cirq.unitary((cirq.X)),
+    cirq.unitary((cirq.Y)),
+    cirq.unitary((cirq.Z)),
+]
+
+two_qubit_paulis = [
+    np.kron(cirq.unitary((cirq.I)), cirq.unitary((cirq.I))),
+    np.kron(cirq.unitary((cirq.I)), cirq.unitary((cirq.X))),
+    np.kron(cirq.unitary((cirq.I)), cirq.unitary((cirq.Y))),
+    np.kron(cirq.unitary((cirq.I)), cirq.unitary((cirq.Z))),
+    np.kron(cirq.unitary((cirq.X)), cirq.unitary((cirq.I))),
+    np.kron(cirq.unitary((cirq.X)), cirq.unitary((cirq.X))),
+    np.kron(cirq.unitary((cirq.X)), cirq.unitary((cirq.Y))),
+    np.kron(cirq.unitary((cirq.X)), cirq.unitary((cirq.Z))),
+    np.kron(cirq.unitary((cirq.Y)), cirq.unitary((cirq.I))),
+    np.kron(cirq.unitary((cirq.Y)), cirq.unitary((cirq.X))),
+    np.kron(cirq.unitary((cirq.Y)), cirq.unitary((cirq.Y))),
+    np.kron(cirq.unitary((cirq.Y)), cirq.unitary((cirq.Z))),
+    np.kron(cirq.unitary((cirq.Z)), cirq.unitary((cirq.I))),
+    np.kron(cirq.unitary((cirq.Z)), cirq.unitary((cirq.X))),
+    np.kron(cirq.unitary((cirq.Z)), cirq.unitary((cirq.Y))),
+    np.kron(cirq.unitary((cirq.Z)), cirq.unitary((cirq.Z))),
+]
+
+
+@pytest.mark.parametrize(
+    "num_qubits, expected_shape, expected_output",
+    [
+        (1, (2, 2), single_qubit_paulis),
+        (2, (4, 4), two_qubit_paulis),
+    ],
+)
+def test_size_n_qubit_paulis(num_qubits, expected_shape, expected_output):
+    """Check shape of n-qubits Paulis and size of the n-qubit Pauli list."""
+    calculated_n_qubit_paulis = _n_qubit_paulis(num_qubits)
+
+    assert len(calculated_n_qubit_paulis) == 4**num_qubits
+
+    for i in range(4**num_qubits):
+        item_n_qubit_paulis = calculated_n_qubit_paulis[i]
+        assert item_n_qubit_paulis.shape == expected_shape
+        assert (item_n_qubit_paulis == expected_output[i]).all()
+
+
+@pytest.mark.parametrize("num_qubits", (0, -1))
+def test_n_qubit_paulis_error(num_qubits):
+    """Check error is raised when the number of qubits is invalid."""
+    with pytest.raises(ValueError, match="Invalid number of qubits provided."):
+        _n_qubit_paulis(num_qubits)
+
+
+def test_pauli_vectorized_list():
+    """Checks n-qubit Paulis are stacked by column."""
+    calculated_output = _pauli_vectorized_list(1)
+    expected_output = [
+        np.array([1.0, 0.0, 0.0, 1.0]),
+        np.array([0.0 + 0.0j, 1.0 + 0.0j, 1.0 + 0.0j, 0.0 + 0.0j]),
+        np.array([0.0 + 0.0j, 0.0 + 1.0j, 0.0 - 1.0j, 0.0 + 0.0j]),
+        np.array([1.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, -1.0 + 0.0j]),
+    ]
+    for i in range(4):
+        assert (expected_output[i] == calculated_output[i]).all()
+
+
+@pytest.mark.parametrize(
+    "input_gate, expected_ptm",
+    [
+        # Pauli X
+        (
+            cirq.X,
+            np.array(
+                [
+                    [1.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 1.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, -1.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, -1.0 + 0.0j],
+                ]
+            ),
+        ),
+        # Pauli Y
+        (
+            cirq.Y,
+            np.array(
+                [
+                    [1.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, -1.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 1.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, -1.0 + 0.0j],
+                ]
+            ),
+        ),
+        # Pauli Z
+        (
+            cirq.Z,
+            np.array(
+                [
+                    [1.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, -1.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, -1.0 + 0.0j, 0.0 + 0.0j],
+                    [0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 1.0 + 0.0j],
+                ]
+            ),
+        ),
+        # Identity
+        (cirq.I, np.identity(4)),
+    ],
+)
+def test_ptm_single_ideal_gate(input_gate, expected_ptm):
+    q0 = cirq.LineQubit(0)
+    circuit = cirq.Circuit()
+    circuit.append(input_gate(q0))
+    calculated_ptm = ptm_matrix(circuit, 1)
+    np.testing.assert_array_equal(calculated_ptm, expected_ptm)
---
+++ mitiq/pt/utils.py
@@ -0,0 +1,80 @@
+# Copyright (C) Unitary Fund
+#
+# This source code is licensed under the GPL license (v3) found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Utility functions for Pauli Twirling."""
+import cirq
+import numpy as np
+
+from mitiq.utils import matrix_to_vector, vector_to_matrix
+
+pauli_unitary_list = [
+    cirq.unitary((cirq.I)),
+    cirq.unitary((cirq.X)),
+    cirq.unitary((cirq.Y)),
+    cirq.unitary((cirq.Z)),
+]
+
+
+def _n_qubit_paulis(num_qubits):
+    """Get a list of n-qubit Pauli unitaries."""
+    if not num_qubits >= 1:
+        raise ValueError("Invalid number of qubits provided.")
+
+    # get the n-qubit paulis from the Pauli group
+    # disregard the n-qubit paulis with complex phase
+    n_qubit_paulis = pauli_unitary_list
+    for i in range(num_qubits - 1):
+        n_qubit_paulis = n_qubit_paulis
+        empty_pauli_list = []
+        for j in pauli_unitary_list:
+            for k in n_qubit_paulis:
+                new_pauli = np.kron(j, k)
+                empty_pauli_list.append(new_pauli)
+        n_qubit_paulis = empty_pauli_list
+    return n_qubit_paulis
+
+
+def _pauli_vectorized_list(num_qubits):
+    """Define a function to create a list of vectorized matrices.
+
+    If the density matrix of interest has more than n>1 qubits, the
+    Pauli group is used to generate n-fold tensor products before
+    vectorizing the unitaries.
+    """
+    n_qubit_paulis = _n_qubit_paulis(num_qubits)
+    output_pauli_vec_list = []
+    for i in n_qubit_paulis:
+        # the matrix_to_vector function stacks rows in vec form
+        # transpose is used here to instead stack the columns
+        matrix_trans = np.transpose(i)
+        output_pauli_vec_list.append(matrix_to_vector(matrix_trans))
+    return output_pauli_vec_list
+
+
+def ptm_matrix(circuit, num_qubits):
+    """Find the Pauli Transfer Matrix (PTM) of a circuit."""
+    superop = cirq.kraus_to_superoperator(cirq.kraus(circuit))
+    vec_pauli = _pauli_vectorized_list(num_qubits)
+    n_qubit_paulis = _n_qubit_paulis(num_qubits)
+    ptm_matrix = np.zeros([4**num_qubits, 4**num_qubits], dtype=complex)
+
+    for i in range(len(vec_pauli)):
+        superop_on_pauli_vec = np.matmul(superop, vec_pauli[i])
+        superop_on_pauli_matrix_transpose = vector_to_matrix(
+            superop_on_pauli_vec
+        )
+        superop_on_pauli_matrix = np.transpose(
+            superop_on_pauli_matrix_transpose
+        )
+
+        ptm_matrix_row = []
+        for j in n_qubit_paulis:
+            pauli_superop_pauli = np.matmul(j, superop_on_pauli_matrix)
+            ptm_matrix_row.append(
+                (0.5**num_qubits) * np.trace(pauli_superop_pauli)
+            )
+        ptm_matrix[i] = ptm_matrix_row
+
+    return ptm_matrix
