3bae6f8bbc1f10a386ff335965e7f95dee3a78b0
bdg221 <brian.goldsmith@gmail.com>
2024-02-27 10:10:12 -0800

    Added warning filter to ignore warning

--- mitiq/rem/tests/test_inverse_confusion_matrix.py
+++ mitiq/rem/tests/test_inverse_confusion_matrix.py
@@ -183,7 +183,7 @@ def test_closest_positive_distribution():
             atol=1e-5,
         )
 
-
+@pytest.mark.filterwarnings("ignore:invalid value encountered in divide")
 def test_closest_positive_distribution_error():
     """Test unfeasible problem to trigger error."""
     with pytest.raises(ValueError, match="REM failed to determine"):
