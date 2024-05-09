11c52618f203dcb89d6c5386de5cdde84562789d
Purva Thakre <purva.athakre@gmail.com>
2024-05-09 10:35:51 -0500

    num layers without measurements

--- mitiq/lre/multivariate_scaling/layerwise_folding.py
+++ mitiq/lre/multivariate_scaling/layerwise_folding.py
@@ -5,3 +5,34 @@
 
 """Functions for layerwise folding of input circuits to allow for multivariate
 extrapolation."""
+
+from copy import deepcopy
+
+import cirq
+
+from mitiq.utils import _pop_measurements
+from mitiq.zne.scaling.folding import _check_foldable
+
+
+def _get_num_layers_without_measurements(input_circuit: cirq.Circuit) -> int:
+    """Checks if the circuit has non-terminal measurements and returns the
+    number of layers in the input circuit without the terminal measurements.
+
+        Args:
+        circuit: Checks whether this circuit is able to be folded.
+
+        Raises:
+            UnfoldableCircuitError:
+                * If the circuit has intermediate measurements.
+                * If the circuit has non-unitary channels which are not
+                terminal measurements.
+
+        Returns:
+            num_layers: the number of layers in the input circuit without the
+            terminal measurements.
+    """
+    circuit = deepcopy(input_circuit)
+    _check_foldable(circuit)
+    _pop_measurements(circuit)
+    num_layers = len(circuit)
+    return num_layers
--- mitiq/lre/tests/test_layerwise_folding.py
+++ mitiq/lre/tests/test_layerwise_folding.py
@@ -5,3 +5,34 @@
 
 """Unit tests for scaling noise by unitary folding of layers in the input
 circuit to allow for multivariate extrapolation."""
+
+from copy import deepcopy
+
+import cirq
+import pytest
+
+from mitiq.lre.multivariate_scaling.layerwise_folding import (
+    _get_num_layers_without_measurements,
+)
+
+qreg1 = cirq.LineQubit.range(3)
+test_circuit1 = cirq.Circuit(
+    [cirq.ops.H.on_each(*qreg1)],
+    [cirq.ops.CNOT.on(qreg1[0], qreg1[1])],
+    [cirq.ops.X.on(qreg1[2])],
+    [cirq.ops.TOFFOLI.on(*qreg1)],
+)
+
+test_circuit1_with_measurements = deepcopy(test_circuit1)
+test_circuit1_with_measurements.append(cirq.ops.measure_each(*qreg1))
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [(test_circuit1, 3), (test_circuit1_with_measurements, 3)],
+)
+def test_get_num_layers(test_input, expected):
+    """Verifies function works as expected."""
+    calculated_num_layers = _get_num_layers_without_measurements(test_input)
+
+    assert calculated_num_layers == expected
