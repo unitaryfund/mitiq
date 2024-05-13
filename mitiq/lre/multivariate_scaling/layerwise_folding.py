7b3e2fee5e58b12c5d7550c5ba869b9525d31fe9
Purva Thakre <purva.athakre@gmail.com>
2024-05-13 10:48:55 -0500

    add scale factor vectors required for multivariate extrapolation

--- mitiq/lre/multivariate_scaling/layerwise_folding.py
+++ mitiq/lre/multivariate_scaling/layerwise_folding.py
@@ -6,9 +6,11 @@
 """Functions for layerwise folding of input circuits to allow for multivariate
 extrapolation."""
 
+import itertools
 from copy import deepcopy
 
 import cirq
+import numpy as np
 
 from mitiq.utils import _pop_measurements
 from mitiq.zne.scaling.folding import _check_foldable
@@ -19,7 +21,7 @@ def _get_num_layers_without_measurements(input_circuit: cirq.Circuit) -> int:
     number of layers in the input circuit without the terminal measurements.
 
         Args:
-        circuit: Checks whether this circuit is able to be folded.
+            input_circuit: Checks whether this circuit is able to be folded.
 
         Raises:
             UnfoldableCircuitError:
@@ -36,3 +38,41 @@ def _get_num_layers_without_measurements(input_circuit: cirq.Circuit) -> int:
     _pop_measurements(circuit)
     num_layers = len(circuit)
     return num_layers
+
+
+def _get_scale_factor_vectors(
+    input_circuit: cirq.Circuit, degree: int, fold_multiplier: int
+) -> list[tuple[int]]:
+    """Returns the patterned scale factor vectors required for multivariate
+    extrapolation.
+
+        Args:
+            input_circuit: Circuit to be scaled
+            degree: Degree of the multivariate polynomial
+            fold_multiplier: Scaling gap required by unitary folding
+
+        Returns:
+            scale_factor_vectors: Multiple variations of scale factors for each
+            layer in the input circuit
+    """
+    num_layers = _get_num_layers_without_measurements(input_circuit)
+
+    # find the exponents of all the monomial terms required for the folding
+    # pattern
+    pattern_full = []
+    for i in range(degree + 1):
+        for j in itertools.combinations_with_replacement(range(num_layers), i):
+            pattern = [0] * num_layers
+            # get the monomial terms in graded lexicographic order
+            for index in j:
+                pattern[index] += 1
+            # use fold multiplier on the folding pattern to figure out which
+            # layers will be scaled
+            pattern_full.append(tuple(fold_multiplier * np.array(pattern)))
+
+    # get the scale factor vectors
+    # the layers are scaled as 2n+1 for unitary folding
+    return [
+        tuple(2 * num_folds + 1 for num_folds in pattern)
+        for pattern in pattern_full
+    ]
--- mitiq/lre/tests/test_layerwise_folding.py
+++ mitiq/lre/tests/test_layerwise_folding.py
@@ -13,6 +13,7 @@ import pytest
 
 from mitiq.lre.multivariate_scaling.layerwise_folding import (
     _get_num_layers_without_measurements,
+    _get_scale_factor_vectors,
 )
 
 qreg1 = cirq.LineQubit.range(3)
@@ -36,3 +37,134 @@ def test_get_num_layers(test_input, expected):
     calculated_num_layers = _get_num_layers_without_measurements(test_input)
 
     assert calculated_num_layers == expected
+
+
+@pytest.mark.parametrize(
+    "test_input, degree, test_fold_multiplier, expected_scale_factor_vectors",
+    [
+        (test_circuit1, 1, 0, [(1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1)]),
+        (test_circuit1, 1, 1, [(1, 1, 1), (3, 1, 1), (1, 3, 1), (1, 1, 3)]),
+        (
+            test_circuit1,
+            2,
+            1,
+            [
+                (1, 1, 1),
+                (3, 1, 1),
+                (1, 3, 1),
+                (1, 1, 3),
+                (5, 1, 1),
+                (3, 3, 1),
+                (3, 1, 3),
+                (1, 5, 1),
+                (1, 3, 3),
+                (1, 1, 5),
+            ],
+        ),
+        (
+            test_circuit1,
+            2,
+            2,
+            [
+                (1, 1, 1),
+                (5, 1, 1),
+                (1, 5, 1),
+                (1, 1, 5),
+                (9, 1, 1),
+                (5, 5, 1),
+                (5, 1, 5),
+                (1, 9, 1),
+                (1, 5, 5),
+                (1, 1, 9),
+            ],
+        ),
+        (
+            test_circuit1,
+            2,
+            3,
+            [
+                (1, 1, 1),
+                (7, 1, 1),
+                (1, 7, 1),
+                (1, 1, 7),
+                (13, 1, 1),
+                (7, 7, 1),
+                (7, 1, 7),
+                (1, 13, 1),
+                (1, 7, 7),
+                (1, 1, 13),
+            ],
+        ),
+        (
+            test_circuit1_with_measurements,
+            1,
+            0,
+            [(1, 1, 1), (1, 1, 1), (1, 1, 1), (1, 1, 1)],
+        ),(
+            test_circuit1_with_measurements,
+            1,
+            1,
+            [(1, 1, 1), (3, 1, 1), (1, 3, 1), (1, 1, 3)],
+        ),
+        (
+            test_circuit1_with_measurements,
+            2,
+            1,
+            [
+                (1, 1, 1),
+                (3, 1, 1),
+                (1, 3, 1),
+                (1, 1, 3),
+                (5, 1, 1),
+                (3, 3, 1),
+                (3, 1, 3),
+                (1, 5, 1),
+                (1, 3, 3),
+                (1, 1, 5),
+            ],
+        ),
+        (
+            test_circuit1_with_measurements,
+            2,
+            2,
+            [
+                (1, 1, 1),
+                (5, 1, 1),
+                (1, 5, 1),
+                (1, 1, 5),
+                (9, 1, 1),
+                (5, 5, 1),
+                (5, 1, 5),
+                (1, 9, 1),
+                (1, 5, 5),
+                (1, 1, 9),
+            ],
+        ),
+        (
+            test_circuit1_with_measurements,
+            2,
+            3,
+            [
+                (1, 1, 1),
+                (7, 1, 1),
+                (1, 7, 1),
+                (1, 1, 7),
+                (13, 1, 1),
+                (7, 7, 1),
+                (7, 1, 7),
+                (1, 13, 1),
+                (1, 7, 7),
+                (1, 1, 13),
+            ],
+        ),
+    ],
+)
+def test_get_scale_factor_vectors(
+    test_input, degree, test_fold_multiplier, expected_scale_factor_vectors
+):
+    """Verifies vectors of scale factors are calculated accurately."""
+    calculated_scale_factor_vectors = _get_scale_factor_vectors(
+        test_input, degree, test_fold_multiplier
+    )
+
+    assert calculated_scale_factor_vectors == expected_scale_factor_vectors
