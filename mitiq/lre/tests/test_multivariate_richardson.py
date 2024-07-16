634ea80fe5ff8fefa1aadf456420f3a6bb834409
Purva Thakre <purva.athakre@gmail.com>
2024-07-16 12:39:19 -0500

    sample matrix unit tests

--- mitiq/lre/tests/test_multivariate_richardson.py
+++ mitiq/lre/tests/test_multivariate_richardson.py
@@ -7,6 +7,7 @@
 
 from math import comb
 
+import numpy as np
 import pytest
 from cirq import Circuit, LineQubit, ops
 
@@ -14,6 +15,7 @@ from mitiq.lre.inference.multivariate_richardson import (
     _create_variable_combinations,
     _get_variables,
     full_monomial_basis,
+    sample_matrix,
 )
 
 
@@ -78,11 +80,55 @@ def test_full_monomial_basis(test_num_layers, test_degree, expected_basis):
     assert calculated_basis == expected_basis
 
 
-def test_sample_matrix():
-    qreg1 = LineQubit.range(3)
-    test_circuit1 = Circuit(
-        [ops.H.on_each(*qreg1)],
-        [ops.CNOT.on(qreg1[0], qreg1[1])],
-    )
+qreg1 = LineQubit.range(3)
+test_circuit1 = Circuit(
+    [ops.H.on_each(*qreg1)],
+    [ops.CNOT.on(qreg1[0], qreg1[1])],
+)
+test_circuit2 = Circuit(
+    [ops.H.on_each(*qreg1)],
+    [ops.CNOT.on(qreg1[0], qreg1[1])],
+    [ops.X.on(qreg1[2])],
+    [ops.TOFFOLI.on(*qreg1)],
+)
 
-    
+
+@pytest.mark.parametrize(
+    "test_circ, test_degree, expected_matrix",
+    [
+        (
+            test_circuit1,
+            2,
+            np.array(
+                [
+                    [1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                    [1.0, 1.0, 3.0, 1.0, 3.0, 9.0],
+                    [1.0, 3.0, 1.0, 9.0, 3.0, 1.0],
+                    [1.0, 1.0, 5.0, 1.0, 5.0, 25.0],
+                    [1.0, 3.0, 3.0, 9.0, 9.0, 9.0],
+                    [1.0, 5.0, 1.0, 25.0, 5.0, 1.0],
+                ]
+            ),
+        ),
+        (
+            test_circuit2,
+            2,
+            np.array(
+                [
+                    [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+                    [1.0, 1.0, 1.0, 3.0, 1.0, 1.0, 1.0, 3.0, 3.0, 9.0],
+                    [1.0, 1.0, 3.0, 1.0, 1.0, 3.0, 9.0, 1.0, 3.0, 1.0],
+                    [1.0, 3.0, 1.0, 1.0, 9.0, 3.0, 1.0, 3.0, 1.0, 1.0],
+                    [1.0, 1.0, 1.0, 5.0, 1.0, 1.0, 1.0, 5.0, 5.0, 25.0],
+                    [1.0, 1.0, 3.0, 3.0, 1.0, 3.0, 9.0, 3.0, 9.0, 9.0],
+                    [1.0, 3.0, 1.0, 3.0, 9.0, 3.0, 1.0, 9.0, 3.0, 9.0],
+                    [1.0, 1.0, 5.0, 1.0, 1.0, 5.0, 25.0, 1.0, 5.0, 1.0],
+                    [1.0, 3.0, 3.0, 1.0, 9.0, 9.0, 9.0, 3.0, 3.0, 1.0],
+                    [1.0, 5.0, 1.0, 1.0, 25.0, 5.0, 1.0, 5.0, 1.0, 1.0],
+                ]
+            ),
+        ),
+    ],
+)
+def test_sample_matrix(test_circ, test_degree, expected_matrix):
+    assert (expected_matrix == sample_matrix(test_circ, test_degree, 1)).all()
