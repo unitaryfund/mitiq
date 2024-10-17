6cd0de788852e828a94538b3dfec93e6b1e7d772
Purva Thakre <purva.athakre@gmail.com>
2024-10-17 07:26:47 -0500

    try changing import order

--- mitiq/lre/__init__.py
+++ mitiq/lre/__init__.py
@@ -5,11 +5,11 @@
 
 """Methods for scaling noise in circuits by layers and using multivariate extrapolation."""
 
+from mitiq.lre.lre import execute_with_lre, mitigate_executor, lre_decorator
+
 from mitiq.lre.multivariate_scaling.layerwise_folding import multivariate_layer_scaling
 
 from mitiq.lre.inference.multivariate_richardson import (
     multivariate_richardson_coefficients,
     sample_matrix,
 )
-
-from mitiq.lre.lre import execute_with_lre, mitigate_executor, lre_decorator
