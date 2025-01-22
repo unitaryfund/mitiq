af1ec5f08af1de8a68fbd3f361480fdc93da399a
Jegbrz <jari.egbers@gmail.com>
2025-01-22 19:52:27 +0100

    whoops otimisation is probaly bad

--- mitiq/vd/vd.py
+++ mitiq/vd/vd.py
@@ -95,7 +95,7 @@ def execute_with_vd(input_rho: cirq.Circuit, M: int=2, K: int=100, observable=Z)
     if not np.allclose(observable, Z):
         gate = cirq.MatrixGate(basis_change_unitary)
     else: 
-        gate = cirq.I()
+        gate = cirq.I
     
     for _ in range(K):
         
@@ -150,6 +150,106 @@ def execute_with_vd(input_rho: cirq.Circuit, M: int=2, K: int=100, observable=Z)
     
     # K must be odd so that D cannot accidentally be 0 and give an error.
     # [Forcing odd K is a workaround, we should look into this]
+    Z_i_corrected = [Ei[i] / D for i in range(N)]
+
+    return Z_i_corrected
+
+
+
+def old_execute_with_vd(input_rho: cirq.Circuit, M: int=2, K: int=100, observable=Z):
+    '''
+    Given a circuit rho that acts on N qubits, this function returns the expectation values of a given observable for each qubit i. 
+    The expectation values are corrected using the virtual distillation algorithm. 
+    '''
+
+    # input rho is an N qubit circuit
+    N = len(input_rho.all_qubits())
+    rho = M_copies_of_rho(input_rho, M)
+
+    # Coupling unitary corresponding to the diagonalization of the SWAP (as seen in the paper) for M = 2:
+    Bi_gate = np.array([
+            [1, 0, 0, 0],
+            [0, np.sqrt(2)/2, np.sqrt(2)/2, 0],
+            [0, np.sqrt(2)/2, -np.sqrt(2)/2, 0],
+            [0, 0, 0, 1]
+        ])
+
+    Ei = [0 for _ in range(N)]
+    D = 0
+    
+    # Forcing odd K, this is a workaround so that D (see end of the function) cannot be 0 accidentally
+    if K%2 == 0:
+        K += 1
+
+    for _ in range(K):
+        
+        circuit = rho.copy()
+
+        # 1) apply basis change unitary
+        # for example observable Z -> apply I
+        # for example observable X -> apply H
+        basis_change_unitary = diagonalize(observable)[0]
+        
+        # apply to every single qubit
+        if not np.allclose(observable, Z):
+            gate = cirq.MatrixGate(basis_change_unitary)
+            for i in range(M*N):
+                circuit.append(gate(cirq.LineQubit(i)))
+
+        # 2) apply the diagonalization gate B
+        B_gate = cirq.MatrixGate(Bi_gate)
+        for i in range(N):
+            circuit.append(B_gate(cirq.LineQubit(i), cirq.LineQubit(i+N)))
+
+        
+        # 3) apply measurements
+        # the measurement keys are applied in accordance with the SWAPS that are applied in the pseudo code in the paper.
+        # The SWAP operations are omitted here since they are hardware specific.
+        # once again this specific code is for M = 2
+        for i in range(N):
+            circuit.append(cirq.measure(cirq.LineQubit(i), key=f"{2*i}"))
+        for i in range(N):
+            circuit.append(cirq.measure(cirq.LineQubit(i+N), key=f"{2*i+1}"))
+        
+        # run the circuit
+        simulator = cirq.Simulator()
+        result = simulator.run(circuit, repetitions=1)
+                
+        # post processing measurements
+        z1 = []
+        z2 = []
+        
+        for i in range(2*N):
+            if i % 2 == 0:
+                z1.append(np.squeeze(result.records[str(i)]))
+            else:
+                z2.append(np.squeeze(result.records[str(i)]))
+
+        # this one is for the pauli Z obvservable
+        def map_to_eigenvalues(measurement):
+            if measurement == 0:
+                return 1
+            else:
+                return -1
+            
+        z1 = [map_to_eigenvalues(i) for i in z1]
+        z2 = [map_to_eigenvalues(i) for i in z2]
+
+        for i in range(N):
+            
+            productE = 1
+            for j in range(N):
+                if i != j:
+                    productE *= ( 1 + z1[j] - z2[j] + z1[j]*z2[j] )
+
+            Ei[i] += 1/2**N * (z1[i] + z2[i]) * productE
+
+        productD = 1
+        for j in range(N):
+            productD *= ( 1 + z1[j] - z2[j] + z1[j]*z2[j] )
+
+        D += 1/2**N * productD 
+        
     Z_i_corrected = [Ei[i] / D for i in range(N)]
 
     return Z_i_corrected
