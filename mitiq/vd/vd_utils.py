2383b505a24fdba27a3b77130fb1d67eaece910a
chrispy-chicken <chrisdehart2003@gmail.com>
2025-01-24 11:05:53 +0100

    added more feedback and addressed mypy/formatting issues

--- mitiq/vd/vd_utils.py
+++ mitiq/vd/vd_utils.py
@@ -1,14 +1,16 @@
 import cirq
-import numpy as np
 
-def M_copies_of_rho(rho: cirq.Circuit, M: int=2) -> cirq.Circuit:
-    """ Copies a circuit M times in parallel.
 
-    Given a circuit rho that acts on N qubits, this function returns a circuit that copies rho M times in parallel.
+def _copy_circuit_parallel(circuit: cirq.Circuit,
+                           num_copies: int = 2) -> cirq.Circuit:
+    """Copies a circuit M times in parallel.
+
+    Given a circuit rho that acts on N qubits,
+    this function returns a circuit that copies rho M times in parallel.
     This means the resulting circuit has N * M qubits.
 
     Args:
-        rho: 
+        rho:
             The circuit to be copied.
         M:
             The number of copies of rho to be made.
@@ -16,19 +18,13 @@ def M_copies_of_rho(rho: cirq.Circuit, M: int=2) -> cirq.Circuit:
     Returns:
         A cirq circuit that is the parallel composition of M copies of rho.
     """
-    
-    # if M <= 1:
-    #     print("warning: M_copies_of_rho is not needed for M <= 1")
-    #     return rho
 
-    N = len(rho.all_qubits())
+    N = len(circuit.all_qubits())
+    new_circuit = cirq.Circuit()
+    qubits = cirq.LineQubit.range(N * num_copies)
+    for i in range(num_copies):
+        new_circuit += circuit.transform_qubits(
+            lambda q: qubits[q.x + N * i]
+        )  # TODO add compatibility for grid qubits
 
-    circuit = cirq.Circuit()
-    qubits = cirq.LineQubit.range(N*M)
-
-    unitary_gate = cirq.MatrixGate(cirq.unitary(rho))
-    for i in range(M):
-        qubit_subset = qubits[i * N : (i+1) * N]
-        circuit.append(unitary_gate(*qubit_subset))
-        
-    return circuit
+    return new_circuit
