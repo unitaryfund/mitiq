7e9641014f5e27a15942775c99067a94e88a5969
Kajetan Knopp <kajetan.knopp@teampicnic.com>
2025-03-21 16:33:05 +0100

    Auxilliary code for VD to apply a Bi matrix on a circuit (#2650)
    * Added Bi gate function implementation
    
    * Added typehints and better docstring
    
    * Added tests for apply_Bi gate
    
    * fix (some) tests
    
    * added func to generate B matrix and updated the function that applies the B gate
    
    * reformatted vd_utils
    
    * updated tests
    
    * ruff
    
    * typos
    
    * bug fixed test and code
    
    * clean up docstrings
    
    * cleanup test; add test showing vd fails on grid qubits
    
    ---------
    
    Co-authored-by: nate stemen <nate@unitary.foundation>
    Co-authored-by: chrispy-chicken <chrisdehart2003@gmail.com>

--- mitiq/vd/vd_utils.py
+++ mitiq/vd/vd_utils.py
@@ -1,23 +1,19 @@
 from typing import List, cast
 
 import cirq
+import numpy as np
 
 
 def _copy_circuit_parallel(
     circuit: cirq.Circuit, num_copies: int = 2
 ) -> cirq.Circuit:
-    """Copies a circuit num_copies times in parallel.
-
-    Given a circuit that acts on N qubits,
-    this function returns a circuit
-    that copies the circuit num_copies times in parallel.
-    This means the resulting circuit has N * num_copies qubits.
+    """Given a circuit that acts on N qubits, this function returns a circuit
+    that copies the circuit num_copies times in parallel. This means the
+    resulting circuit has N * num_copies qubits.
 
     Args:
-        circuit:
-            The circuit to be copied.
-        num_copies:
-            The number of copies of circuit to be made.
+        circuit: The circuit to be copied.
+        num_copies: The number of copies of circuit to be made.
 
     Returns:
         A cirq circuit that is the parallel composition of
@@ -51,3 +47,64 @@ def _copy_circuit_parallel(
             new_circuit += circuit.transform_qubits(map_for_grid_qubits)
 
     return new_circuit
+
+
+def _apply_diagonalizing_gate(
+    circuit: cirq.Circuit, num_copies: int
+) -> cirq.Circuit:
+    """Apply the diagonalizing gate to a circuit, assuming the circuit has been
+    copied in parallel ``num_copies`` times.
+
+    The diagonalizing gate is applied to the first qubit of all copies, to the
+    second qubit of all copies, etc.
+
+    Args:
+        circuit: The circuit to apply the diagonalizing gate to.
+        num_copies: The number of copies of the original circuit that the input
+            circuit consists of.
+
+    Returns:
+        The circuit with the diagonalizing gate applied.
+    """
+
+    new_circuit = circuit.copy()
+
+    # this is the number of qubits in the original circuit
+    N = len(circuit.all_qubits()) // num_copies
+
+    diag_gate = _generate_diagonalizing_gate(num_copies)
+
+    for i in range(N):
+        qubits = [
+            cirq.LineQubit(i + N * j) for j in range(num_copies)
+        ]  # select qubit i of each copy
+
+        new_circuit.append(diag_gate(*qubits))
+
+    return new_circuit
+
+
+def _generate_diagonalizing_gate(num_copies: int = 2) -> cirq.Gate:
+    """Generate the diagonalizing gate for the VD algorithm. Currently only
+    ``num_copies=2`` is supported.
+
+    Args:
+        num_qubits: The number of qubits that the gate acts on.
+
+    Returns: The diagonalizing gate.
+    """
+    if num_copies == 2:
+        diagonalizing_matrix = np.array(
+            [
+                [1, 0, 0, 0],
+                [0, np.sqrt(2) / 2, np.sqrt(2) / 2, 0],
+                [0, np.sqrt(2) / 2, -np.sqrt(2) / 2, 0],
+                [0, 0, 0, 1],
+            ]
+        )
+    else:
+        raise NotImplementedError(
+            "Only num_copies = 2 is currently supported."
+        )
+
+    return cirq.MatrixGate(diagonalizing_matrix)
