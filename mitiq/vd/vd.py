afe883525e3af8951ddd7b64f893f619088fe94f
nate stemen <nate@unitary.foundation>
2025-03-26 08:55:08 -0700

    cleanup

--- mitiq/vd/vd.py
+++ mitiq/vd/vd.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import Callable, List
 
 import cirq
@@ -39,8 +40,7 @@ def vd_executor(
 def execute_with_vd(
     circuit: cirq.Circuit,
     executor: Callable[[cirq.Circuit], MeasurementResult],
-    M: int = 2, # TODO: refactor with num_additional_qubits
-    num_shots: int = 1000,
+    M: int = 2,  # TODO: refactor with num_additional_qubits
     observable: Observable | None = None,
 ) -> list[float]:
     """Given a circuit that acts on N qubits, this function returns the
@@ -50,100 +50,70 @@ def execute_with_vd(
 
     Args:
         circuit: The input circuit of N qubits to execute with VD.
-        executor: A Mitiq executor that executes a circuit and returns the
-            unmitigated ``QuantumResult`` (e.g. an expectation value).
-            The executor must either return a ``MeasurementResult`` or a list
-            thereof ``list[MeasurementResult]``.
+        executor: An executor that executes a circuit and returns either a
+            density matrix, or a measurement result (bitstring).
         M: The number of copies of rho. Only M=2 is implemented at this moment.
-        num_shots: The number of iterations of the algorithm. Only used if the
-            executor returns a outcomes from a single experiment (i.e. a
-            measurement result).
         observable: The one qubit observable for which the expectation values
             are computed. The default observable is the Pauli Z matrix. At the
             moment using different observables is not supported.
 
     Returns:
-        A list of VD-estimated expectation values.
+        A list of VD-estimated expectation values for <Z_i>.
     """
 
-    # input rho is an N qubit circuit
-    N = len(circuit.all_qubits())
-    new_circuit = _copy_circuit_parallel(circuit, M)
+    num_qubits = len(circuit.all_qubits())
+    parallel_copied_circuit = _copy_circuit_parallel(circuit, M)
 
-    Ei = np.array(list(0 for _ in range(N)))
-    D = 0
-
-    # Forcing odd num_shots, this is a workaround so that D (see end of func)
-    # cannot be 0 accidentally
-    if num_shots % 2 == 0:
-        num_shots -= 1
 
     executor_obj = Executor(executor)  # type: ignore[arg-type]
 
     # Density matrix return type
     if executor_obj._executor_return_type in DensityMatrixLike:
-        circuit_dm = executor_obj.run(new_circuit)
-        circuit_swaps = _apply_cyclic_system_permutation(circuit_dm, N)
-        resulting_dm = _apply_symmetric_observable(
-            circuit_swaps, N, observable
+        circuit_dm = executor_obj.run(parallel_copied_circuit)
+        circuit_swaps = _apply_cyclic_system_permutation(
+            circuit_dm, num_qubits
         )
-        exp_values = np.trace(resulting_dm, axis1=1, axis2=2) / np.trace(
+        resulting_dm = _apply_symmetric_observable(
+            circuit_swaps, num_qubits, observable
+        )
+        expectation_values = np.trace(resulting_dm, axis1=1, axis2=2) / np.trace(
             resulting_dm, axis1=1, axis2=2
         )
 
-    # Measurement result return type
     elif executor_obj._executor_return_type in MeasurementResultLike:
-        new_circuit = _apply_diagonalizing_gate(circuit, M)
+        # TODO: why no cyclic permutation here?
+        entangled_circuits = _apply_diagonalizing_gate(
+            parallel_copied_circuit, M
+        )
 
-        # apply measurements
-        for i in range(M * N):
-            new_circuit.append(cirq.measure(cirq.LineQubit(i), key=f"{i}"))
+        entangled_circuits.append(
+            cirq.measure(entangled_circuits.all_qubits())
+        )
 
-        res = executor_obj.run(
-            new_circuit, force_run_all=True, reps=num_shots
-        )  # TODO make this reps a **kwargs to allow any executor
+        meas_res = executor_obj.run(entangled_circuits, force_run_all=True)[0]
+        # TODO: drop one shot if not even?
 
-        self_packed = True
-        if isinstance(res, str):
-            res = [res]
-            self_packed = False
-
-        elif isinstance(res[0], int):
-            res = [res]
-            self_packed = False
-
-        if len(res) == 1:  # if the executor only returns a single measurement
-            for _ in range(num_shots - 1):  # then we repeat num_shots times
-                if not self_packed:
-                    res.append(
-                        executor_obj.run(new_circuit, force_run_all=True)
-                    )
-                else:
-                    res.append(
-                        executor_obj.run(new_circuit, force_run_all=True)[0]
-                    )
-
+        Ei = np.zeros(num_qubits)
+        D = 0
         # post processing measurements
-        for bitStr in res:
+        for bitstring in meas_res.asarray:
             # map 0/1 measurements to 1/-1 measurements (eigenvalues of Z)
-            Z_base_measurement = np.array(
-                1 if i == 0 else -1 for i in list(bitStr)
+            measurement_eigenvalues = np.array(
+                [1 if bit == 0 else -1 for bit in bitstring]
             )
-
-            # Separate the two systems
-            z1 = Z_base_measurement[:N]
-            z2 = Z_base_measurement[N:]
+            z1 = measurement_eigenvalues[:num_qubits]
+            z2 = measurement_eigenvalues[num_qubits:]
 
             # Implementing the sum and product from the paper
             # each factor in the product of the Ei sum will be either +/-1
             # only in case of pauli Z
             product_term = 1
-            for j in range(N):
+            for j in range(num_qubits):
                 product_term *= (1 + z1[j] - z2[j] + z1[j] * z2[j]) // 2
 
-            D += product_term
+            D += product_term # correct for M=2 case, but wrong otherwise
 
-            for i in range(N):
+            for i in range(num_qubits):
                 Ei[i] += (
                     (z1[i] + z2[i])
                     // 2
@@ -151,19 +121,13 @@ def execute_with_vd(
                     // ((1 + z1[i] - z2[i] + z1[i] * z2[i]) // 2)
                 )  # undo the j=i term in the product
 
-        exp_values = Ei / D
+        expectation_values = Ei / D
 
-    else:
-        raise ValueError(
-            "Executor must have a return type of DensityMatrixLike or \
-            MeasurementResultLike"
-        )
-
-    if not np.allclose(exp_values.real, exp_values, atol=1e-6):
-        print(
+    if not np.allclose(expectation_values.real, expectation_values, atol=1e-6):
+        warnings.warn(
             "Warning: The expectation value contains a significant \
             imaginary part. This should never happen."
         )
-        return exp_values
+        return expectation_values
     else:
-        return exp_values.real
+        return expectation_values.real
