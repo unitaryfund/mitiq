de2eb57a2dceac3bd9616de6f425a4a3eb510e3c
nate stemen <nate@unitary.foundation>
2025-03-28 07:29:07 -0700

    refactor into two step process

--- mitiq/vd/__init__.py
+++ mitiq/vd/__init__.py
@@ -3,4 +3,4 @@
 # This source code is licensed under the GPL license (v3) found in the
 # LICENSE file in the root directory of this source tree.
 
-from mitiq.vd.vd import execute_with_vd
+from mitiq.vd.vd import execute_with_vd, construct_circuits, combine_results
