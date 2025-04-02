e2b66a5d216d1a1b807d85b56e1b80faac4e6f95
nate stemen <nate@unitary.foundation>
2025-04-02 10:33:44 -0700

    add license to all vd files

--- mitiq/vd/vd.py
+++ mitiq/vd/vd.py
@@ -1,3 +1,8 @@
+# Copyright (C) Unitary Fund
+#
+# This source code is licensed under the GPL license (v3) found in the
+# LICENSE file in the root directory of this source tree.
+
 from typing import Callable
 
 import cirq
